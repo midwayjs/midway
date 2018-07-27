f96cac99f34cfb96be8b2cfd03b56db90f1c7fab
kurten <chinkurten@gmail.com>
2018-07-27 20:30:56 +0800

    add test case

--- packages/midway-web/test/fixtures/enhance/js-app-xml/lib/hello.js
+++ packages/midway-web/test/fixtures/enhance/js-app-xml/lib/hello.js
@@ -1,5 +1,13 @@
 'use strict';
 module.exports = class Hello {
+  constructor(options) {
+    this.options = options;
+  }
+
+  doPlugin2() {
+    return this.options.plugin2.text;
+  }
+
   say(name) {
     return `hello ${name}`;
   }
