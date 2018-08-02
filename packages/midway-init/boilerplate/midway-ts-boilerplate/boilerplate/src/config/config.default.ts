b7d23951089811fde2720d601474f806d6c4dc2b
Danielsss <better.sunjian@gmail.com>
2018-08-02 19:36:27 +0800

    changed: config.keys format

--- packages/midway-init/boilerplate/midway-ts-boilerplate/boilerplate/src/config/config.default.ts
+++ packages/midway-init/boilerplate/midway-ts-boilerplate/boilerplate/src/config/config.default.ts
@@ -1,12 +1,8 @@
 module.exports = (appInfo: any) => {
-  const config: {
-    keys?: any[],
-    middleware?: any[],
-    [propName: string]: any,
-  } = exports = {};
+  const config: any = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
-  config.keys = [appInfo.name + '_{{keys}}'];
+  config.keys = appInfo.name + '_{{keys}}';
 
   // add your config here
   config.middleware = [
