1af0f18a73b9c10e0c55e5b6219bc185cc0586ce
Harry Chen <czy88840616@gmail.com>
2018-08-02 18:38:54 +0800

    fix: init template

---
+++ packages/midway-init/boilerplate/midway-ts-boilerplate/boilerplate/procfile.js
@@ -0,0 +1,34 @@
+// you can use this file to deploy by pandora
+// please use 'npm i pandora --save' first
+
+'use strict';
+
+module.exports = pandora => {
+
+  /**
+   * default is fork mode
+   */
+  pandora
+    .fork('midway-sandbox', require('midway/server')({
+      typescript: true,
+    }));
+
+  /**
+   * you can use cluster mode to start application
+   */
+  // pandora
+  //   .cluster('./cluster.js');
+
+  /**
+   * you can create another process here
+   */
+  // pandora
+  //   .process('background')
+  //   .argv(['--expose-gc']);
+
+  /**
+   * more features please visit our document.
+   * https://github.com/midwayjs/pandora/
+   */
+
+};
