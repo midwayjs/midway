b1d452de35bc02afd9ad65522a31c28502ba886f
Harry Chen <czy88840616@gmail.com>
2018-09-07 15:39:17 +0800

    refactor: remove mm.container and use mm.app

--- packages/midway-mock/test/mock_container.test.ts
+++ packages/midway-mock/test/mock_container.test.ts
@@ -6,6 +6,12 @@ const assert = require('assert');
 const fixtures = path.join(__dirname, 'fixtures');
 
 describe('test/mock_container.test.ts', () => {
+
+  before(() => {
+    // midway dir
+    process.env.MIDWAY_FRAMEWORK_PATH = path.join(__dirname, '../../midway');
+  });
+
   describe('test base app decorator', () => {
     let container;
     before(() => {
@@ -14,7 +20,6 @@ describe('test/mock_container.test.ts', () => {
         typescript: true
       });
     });
-    after(() => container.stop());
     afterEach(mm.restore);
 
     it('should mock service success', async () => {
@@ -34,7 +39,7 @@ describe('test/mock_container.test.ts', () => {
     });
   });
 
-  describe('test js app xml', () => {
+  describe.only('test js app xml', () => {
     let container;
     before(() => {
       container = mm.container({
@@ -47,7 +52,6 @@ describe('test/mock_container.test.ts', () => {
 
       return container.ready();
     });
-    after(() => container.stop());
     afterEach(mm.restore);
 
     it('should test js app load success', async () => {
