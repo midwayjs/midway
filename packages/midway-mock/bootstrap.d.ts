f5df1de40e79c0d3575922c4319c6a5c29b73c5f
kurten <chinkurten@gmail.com>
2018-09-10 10:59:16 +0800

    fix: import dist 目录错误

--- packages/midway-mock/bootstrap.d.ts
+++ packages/midway-mock/bootstrap.d.ts
@@ -1,11 +1,9 @@
 import * as assert from 'power-assert';
 import { MockApplication, EggMock } from 'egg-mock';
-import { MidwayMock, MockContainer } from './dist';
 
 export {
-  assert,
-  MockContainer
+  assert
 };
 export declare const app: MockApplication;
 export declare const mock: EggMock;
-export declare const mm: MidwayMock;
+export declare const mm: EggMock;
