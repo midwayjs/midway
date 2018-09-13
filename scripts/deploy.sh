7f1d9d075a551d6156a8bfef50712944d8cf0741
Harry Chen <czy88840616@gmail.com>
2018-09-13 20:51:21 +0800

    chore: remove spec

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,7 @@ TARGET_BRANCH="gh-pages"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    npm run spec
+    echo "Skipping deploy; "
     exit 0
 fi
 
