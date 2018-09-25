f51395b5c75c7e0273269108e3a3baeb9fc27b67
Harry Chen <czy88840616@gmail.com>
2018-09-25 14:01:49 +0800

    refactor: different router and get same result

---
+++ packages/midway-web/test/fixtures/enhance/base-app-router/src/app/controller/home.ts
@@ -0,0 +1,15 @@
+import { provide } from 'injection';
+import { controller, get } from '../../../../../../../src/';
+
+@provide()
+@controller('/')
+export class HomeController {
+
+  @get('/')
+  @get('/home')
+  @get('/poster')
+  async index(ctx) {
+    ctx.body = 'hello';
+  }
+
+}
