a312d1244e925346c1f4062308f0c1cde25ea65c
Harry Chen <czy88840616@gmail.com>
2018-10-17 10:59:03 +0800

    chore: revert before force push

--- packages/midway-mock/bootstrap.d.ts
+++ packages/midway-mock/bootstrap.d.ts
@@ -1,9 +1,14 @@
 import * as assert from 'power-assert';
 import { MockApplication, EggMock } from 'egg-mock';
+import { IApplicationContext } from 'injection';
+
+interface MMockApplication extends MockApplication {
+  applicationContext: IApplicationContext;
+}
 
 export {
   assert
 };
-export declare const app: MockApplication;
+export declare const app: MMockApplication;
 export declare const mock: EggMock;
 export declare const mm: EggMock;
