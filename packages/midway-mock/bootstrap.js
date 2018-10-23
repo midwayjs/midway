f22f140d0233053ea86c1f0f4cadce6e0829da47
kurten <chinkurten@gmail.com>
2018-10-23 17:21:20 +0800

    fix mm not a function bug

--- packages/midway-mock/bootstrap.js
+++ packages/midway-mock/bootstrap.js
@@ -9,14 +9,17 @@ const app = mock.app(options);
 before(() => app.ready());
 afterEach(mock.restore);
 
+// 由于使用Object.assign，丢了默认的mm执行函数，所以使用default输出mm
+const mm = mock.default;
+
 module.exports = {
   assert,
   app,
   mock,
-  mm: mock,
+  mm,
 };
 
 exports.assert = assert;
 exports.app = app;
 exports.mock = mock;
-exports.mm = mock;
+exports.mm = mm;
