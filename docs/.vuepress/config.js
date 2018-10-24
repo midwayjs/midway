2e557c9e97e1ecb2d1d6ac34bce6b56c31ab4825
冰森 <lellansin@gmail.com>
2018-10-24 17:20:35 +0800

    chores: js file format

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,29 +20,29 @@ module.exports = {
       updatePopup: true
     },
     nav: [
-      {text: '首页', link: '/'},
-      {text: 'TS 新手指南', link: '/ts_start'},
-      {text: '使用指南', link: '/guide'},
-      {text: 'API', link: '/api-reference'},
+      { text: '首页', link: '/' },
+      { text: 'TS 新手指南', link: '/ts_start' },
+      { text: '使用指南', link: '/guide' },
+      { text: 'API', link: '/api-reference' },
       {
         text: 'MidwayJs 系列产品',
         items: [
           {
             text: '框架',
             items: [
-              {text: 'Midway - 面向未来的 Web 全栈框架', link: '/'},
+              { text: 'Midway - 面向未来的 Web 全栈框架', link: '/' },
             ]
           },
           {
             text: '应用管理',
             items: [
-              {text: 'Pandora.js - Node.js 应用管理器', link: 'http://midwayjs.org/pandora/'},
+              { text: 'Pandora.js - Node.js 应用管理器', link: 'http://midwayjs.org/pandora/' },
             ]
           },
           {
             text: '监控产品',
             items: [
-              {text: 'Sandbox - 私有化 Node.js 监控产品', link: '#'},
+              { text: 'Sandbox - 私有化 Node.js 监控产品', link: '#' },
             ]
           }
         ]
