75664a3b6b8c11128d42464f670304b83058839e
冰森 <lellansin@gmail.com>
2018-11-01 15:46:55 +0800

    chores: fit for redeclare block-scoped

--- packages/midway-web/test/utils.test.ts
+++ packages/midway-web/test/utils.test.ts
@@ -1,24 +1,24 @@
-const assert = require('assert');
-const utils = require('../src/utils');
+const { deepEqual } = require('assert');
+const { getParamNames, getMethodNames } = require('../src/utils');
 
 describe('test/utils.test.ts', () => {
   it('#getParamNames', () => {
-    const res = utils.getParamNames(function test(a, b) {
+    const res = getParamNames(function test(a, b) {
       console.log(a, b);
     });
-    assert.deepEqual(res, ['a', 'b']);
+    deepEqual(res, ['a', 'b']);
   });
 
   it('#getParamNames, with null', () => {
-    const res = utils.getParamNames(function test() {});
-    assert.deepEqual(res, []);
+    const res = getParamNames(function test() {});
+    deepEqual(res, []);
   });
 
   it('#getMethodNames', () => {
-    const res = utils.getMethodNames({
+    const res = getMethodNames({
       test: () => null,
       hello: () => null,
     });
-    assert.deepEqual(res, ['test', 'hello']);
+    deepEqual(res, ['test', 'hello']);
   });
 });
