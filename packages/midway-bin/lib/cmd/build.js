0a6676f2a62b398ff8d618e2d3b38f15e99ec911
ZQun <13304061222@qq.com>
2018-11-28 17:08:37 +0800

    feat: Add build specified suffix file
    - 添加npm run build 在 package.json -> midway-bin-build -> include 中指定后缀范围"lib/public/*.json"
    - 支持自定义后缀"lib/*.js" or "lib/*.txt"

--- packages/midway-bin/lib/cmd/build.js
+++ packages/midway-bin/lib/cmd/build.js
@@ -62,13 +62,38 @@ class BuildCommand extends Command {
           for (const file of pkg['midway-bin-build'].include) {
             const srcDir = path.join('src', file);
             const targetDir = path.join(outDir, file);
-            fse.copySync(path.join(cwd, srcDir), path.join(cwd, targetDir));
-            console.log(`[midway-bin] copy ${srcDir} to ${targetDir} success!`);
+            const isSrcDir = srcDir.indexOf('*');
+            if(isSrcDir !== -1) {
+              const srcPath = srcDir.substring(0, isSrcDir -1);
+              const fileExtensionName = srcDir.replace(/.+\./,"");// 拓展名
+
+              this.travel(path.join(cwd, srcPath), pathname => {
+                if(pathname.replace(/.+\./,"") === fileExtensionName) {
+                  const srcPathName = pathname.lastIndexOf(srcPath);
+                  const outFile = pathname.substr(srcPathName +4);// 去掉/src
+                  const targetDir = path.join(outDir, outFile);
+                  fse.copySync(path.join(pathname), path.join(cwd, targetDir));
+                }
+              })
+            } else {
+              fse.copySync(path.join(cwd, srcDir), path.join(cwd, targetDir));
+              console.log(`[midway-bin] copy ${srcDir} to ${targetDir} success!`);
+            }
           }
         }
       }
     }
-
+  }
+  
+  travel(dir, callback) {
+    fs.readdirSync(dir).forEach(file => {
+      let pathname = path.join(dir, file);
+      if (fs.statSync(pathname).isDirectory()) {
+        this.travel(pathname, callback);
+      } else {
+        callback(pathname);
+      }
+    });
   }
 }
 
