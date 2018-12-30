2610786f513a511f88115fa390fff26b880a9b4a
kurten <chinkurten@gmail.com>
2018-12-30 23:38:55 +0800

    add: compatible this.ctx.proxy.xxx feature

--- packages/midway-web/app/extend/context.js
+++ packages/midway-web/app/extend/context.js
@@ -11,4 +11,16 @@ module.exports = {
     }
     return this[rc];
   },
+
+  /**
+   * 代理egg的this.ctx.proxy
+   * 从当前requestContext获取，requestContext不存在则从applicationContext获取
+   */
+  get proxy() {
+    return new Proxy({}, {
+      get: (target, name) => {
+        return this[rc] ? this[rc].get(name) : this.app.applicationContext.get(name);
+      }
+    })
+  }
 };
