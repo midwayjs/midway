9879da58f908961134bfdd4353f26a57b493467a
Harry Chen <czy88840616@gmail.com>
2019-01-21 15:38:59 +0800

    chore: optimize cov

--- scripts/cov.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-cwd=`pwd`
-lerna run cov
-rm -rf "${cwd}/.nyc_output" || true
-mkdir "${cwd}/.nyc_output"
-cp -r ./packages/*/.nyc_output/* $cwd/.nyc_output/ || true
-cp -r ./packages/*/node_modules/.nyc_output/* $cwd/.nyc_output/ || true
-./node_modules/.bin/nyc report
