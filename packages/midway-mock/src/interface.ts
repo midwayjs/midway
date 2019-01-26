36c1090657f95af37013e8d388bd3d9e41b6eaf3
Harry Chen <czy88840616@gmail.com>
2019-01-27 00:07:11 +0800

    refactor: add midway-mock type support

--- packages/midway-mock/src/interface.ts
+++ packages/midway-mock/src/interface.ts
@@ -1,12 +1,15 @@
-import { MockApplication } from 'egg-mock';
+import { MockApplication, MockOption } from 'egg-mock';
+import { IApplicationContext } from 'injection';
 
-export interface MidwayApplicationOptions {
+export interface MidwayApplicationOptions extends MockOption {
   baseDir?: string;
   framework?: string;
+  plugin?: any;
   plugins?: any;
   container?: any;
   typescript?: boolean;
 }
 
 export interface MidwayMockApplication extends MockApplication {
+  applicationContext: IApplicationContext;
 }
