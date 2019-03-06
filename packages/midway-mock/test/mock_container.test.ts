fb620f7812a6e6775b4de6dc451319216c4de6de
Harry Chen <czy88840616@gmail.com>
2019-03-06 10:18:33 +0800

    chore: remove js test case in open version

--- packages/midway-mock/test/mock_container.test.ts
+++ packages/midway-mock/test/mock_container.test.ts
@@ -34,28 +34,4 @@ describe('test/mock_container.test.ts', () => {
     });
   });
 
-  describe('test js app xml', () => {
-    let container;
-    before(() => {
-      container = mm.container({
-        baseDir: path.join(fixtures, 'js-app-xml'),
-        typescript: false,
-        container: {
-          disableAutoLoad: true
-        }
-      });
-
-      return container.ready();
-    });
-    afterEach(mm.restore);
-
-    it('should test js app load success', async () => {
-      // use camelcase class name as provider id in js version
-      const my: any = await container.getAsync('myController');
-      assert(my);
-      assert(my.$$mytest);
-      assert(my.$$mytest === 'this is my test');
-      assert(my.$plugin2.text === 't');
-    });
-  });
 });
