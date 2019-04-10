c63505765ba46d4b2cfcdf30ff6c8ba0c4059d2e
skmdev <skmdev29@gmail.com>
2019-04-10 22:50:15 +0800

    feat: add project options in midway-bin

---
+++ packages/midway-bin/test/fixtures/ts-dir-with-another-tsconfig/a.ts
@@ -0,0 +1,2 @@
+import * as path from 'path';
+console.log(path.extname(__filename));
--- packages/midway-bin/test/lib/cmd/build.test.js
+++ packages/midway-bin/test/lib/cmd/build.test.js
@@ -52,5 +52,75 @@ describe('test/lib/cmd/build.test.js', () => {
     assert(fs.existsSync(path.join(cwd, 'dist/pattern/sub/sub_ignore.css')));
     yield rimraf(path.join(cwd, 'dist'));
   });
+});
 
+describe('test/lib/cmd/build.test.js - with another tsconfig', () => {
+  const midwayBin = require.resolve('../../../bin/midway-bin.js');
+
+  afterEach(mm.restore);
+
+  it('should warn message', function*() {
+    const cwd = path.join(__dirname, '../../fixtures/ts-dir-without-config');
+    const child = coffee
+      .fork(midwayBin, ['build', '-p', 'tsconfig.prod.json'], { cwd })
+      .expect('stdout', /tsconfig/);
+
+    yield child.expect('code', 0).end();
+  });
+
+  it('should build success with another tsconfig', function*() {
+    const cwd = path.join(
+      __dirname,
+      '../../fixtures/ts-dir-with-another-tsconfig'
+    );
+    yield rimraf(path.join(cwd, 'dist'));
+    const child = coffee.fork(
+      midwayBin,
+      ['build', '-p', 'tsconfig.prod.json'],
+      { cwd }
+    );
+    yield child.expect('code', 0).end();
+    assert(fs.existsSync(path.join(cwd, 'dist/a.js')));
+    yield rimraf(path.join(cwd, 'dist'));
+  });
+
+  it('should auto clean dir before build with another tsconfig', function*() {
+    const cwd = path.join(
+      __dirname,
+      '../../fixtures/ts-dir-with-another-tsconfig'
+    );
+    yield rimraf(path.join(cwd, 'dist'));
+    const child = coffee.fork(
+      midwayBin,
+      ['build', '-p', 'tsconfig.prod.json'],
+      { cwd }
+    );
+    yield child.expect('code', 0).end();
+    assert(fs.existsSync(path.join(cwd, 'dist/a.js')));
+    yield rimraf(path.join(cwd, 'dist'));
+  });
+
+  it('should copy assets file to dist dir with another tsconfig', function*() {
+    const cwd = path.join(
+      __dirname,
+      '../../fixtures/ts-dir-with-assets-and-another-tsconfig'
+    );
+
+    const child = coffee.fork(
+      midwayBin,
+      ['build', '-c', '-p', 'tsconfig.prod.json'],
+      { cwd }
+    );
+    yield child.expect('code', 0).end();
+    assert(fs.existsSync(path.join(cwd, 'dist/a.js')));
+    assert(fs.existsSync(path.join(cwd, 'dist/view/index.html')));
+    assert(fs.existsSync(path.join(cwd, 'dist/public/test.css')));
+    assert(fs.existsSync(path.join(cwd, 'dist/public/test.js')));
+    assert(fs.existsSync(path.join(cwd, 'dist/resource.json')));
+    assert(fs.existsSync(path.join(cwd, 'dist/lib/b.json')));
+    assert(fs.existsSync(path.join(cwd, 'dist/lib/a.text')));
+    assert(fs.existsSync(path.join(cwd, 'dist/pattern/ignore.css')));
+    assert(fs.existsSync(path.join(cwd, 'dist/pattern/sub/sub_ignore.css')));
+    yield rimraf(path.join(cwd, 'dist'));
+  });
 });
