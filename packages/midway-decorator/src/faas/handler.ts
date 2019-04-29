2e13d0ac6ef46c5cd29c3b1a11f38b9355ad5d61
Harry Chen <czy88840616@gmail.com>
2019-04-29 19:15:27 +0800

    refactor: add faas handler in @func

--- packages/midway-decorator/src/faas/handler.ts
+++ packages/midway-decorator/src/faas/handler.ts
@@ -1,11 +1,11 @@
 import { savePropertyDataToClass } from 'injection';
 import { HANDLER_KEY } from '../constant';
 
-export function handler(handlerName): MethodDecorator {
+export function handler(handlerMapping: string): MethodDecorator {
   return (target: object, propertykey: string, descriptor: PropertyDescriptor) => {
     savePropertyDataToClass(HANDLER_KEY, {
       method: propertykey,
-      data: handlerName,
+      data: handlerMapping,
     }, target, propertykey);
   };
 }
