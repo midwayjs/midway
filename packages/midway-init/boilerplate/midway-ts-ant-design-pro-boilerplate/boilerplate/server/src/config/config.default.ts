8ca6be302894006f6c65487acd9301516a1a489b
waiting <1661926154@qq.com>
2019-07-02 15:18:43 +0800

    chore(boilerplate): update types within configuration files

--- packages/midway-init/boilerplate/midway-ts-ant-design-pro-boilerplate/boilerplate/server/src/config/config.default.ts
+++ packages/midway-init/boilerplate/midway-ts-ant-design-pro-boilerplate/boilerplate/server/src/config/config.default.ts
@@ -1,7 +1,10 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'midway';
 import * as path from 'path';
 
-export default (appInfo: any) => {
-  const config: any = {};
+export type DefaultConfig = PowerPartial<EggAppConfig>
+
+export default (appInfo: : EggAppInfo) => {
+  const config = <DefaultConfig> {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1541510581780_3944';
--- packages/midway-init/boilerplate/midway-ts-boilerplate/boilerplate/src/config/config.default.ts
+++ packages/midway-init/boilerplate/midway-ts-boilerplate/boilerplate/src/config/config.default.ts
@@ -1,5 +1,9 @@
-export default (appInfo: any) => {
-  const config: any = {};
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'midway';
+
+export type DefaultConfig = PowerPartial<EggAppConfig>
+
+export default (appInfo: : EggAppInfo) => {
+  const config = <DefaultConfig> {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_{{keys}}';
