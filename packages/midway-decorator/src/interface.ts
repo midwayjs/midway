df4ca525d589ca4cbfac6f639fbeb44fa4f2ab00
Harry Chen <czy88840616@gmail.com>
2019-07-14 00:38:14 +0800

    chore: lint auto fix

--- packages/midway-decorator/src/interface.ts
+++ packages/midway-decorator/src/interface.ts
@@ -1,2 +1,2 @@
 export type KoaMiddleware <T = any> = (context: T, next: () => Promise<any>) => void;
-export type KoaMiddlewareParamArray <T = any> = Array<string | KoaMiddleware<T>>
+export type KoaMiddlewareParamArray <T = any> = Array<string | KoaMiddleware<T>>;
