bdcd2e27962667a015ba7bc4bcf0ddbc0b33b04f
Harry Chen <czy88840616@gmail.com>
2019-07-29 23:24:18 +0800

    test: add test case

---
+++ packages/midway-core/test/fixtures/complex_injection/dbAPI.ts
@@ -0,0 +1,34 @@
+import { inject, provide, scope, ScopeEnum } from 'injection';
+
+@provide()
+export class A {
+  config = {
+    c: 1
+  };
+}
+
+@provide()
+export class B {
+  config = {
+    c: 2
+  };
+}
+
+@scope(ScopeEnum.Singleton)
+@provide('newKey')
+export class DbAPI {
+
+  private config;
+
+  constructor(
+    @inject() a,
+    hello,
+    @inject() b,
+  ) {
+    this.config = a.config.c + b.config.c;
+  }
+
+  output() {
+    console.log(this.config);
+  }
+}
