52217358239eb8e892dddeefdf6ed34b59ed0cb5
waiting <1661926154@qq.com>
2019-09-05 21:17:09 +0800

    refactor(midway-bin): resolve module path with require.resolve.paths()

--- packages/midway-bin/lib/util.js
+++ packages/midway-bin/lib/util.js
@@ -1,6 +1,12 @@
 'use strict';
+const fs = require('fs');
 const path = require('path');
 
+/**
+ * Resolve module's full path
+ * @param {string} moduleName module name
+ * @return {string} full path
+ */
 function resolveModule(moduleName) {
   if (!moduleName) {
     console.log('[midway-bin] value of framework is blank and skipped.');
@@ -8,13 +14,33 @@ function resolveModule(moduleName) {
   }
 
   try {
-    require.resolve(moduleName);
-    const moduleDir = path.join(__dirname, '../../');
-    const moduleFullPath = path.join(moduleDir, moduleName);
+    const moduleFullPath = retrieveModulePath(moduleName);
     return moduleFullPath;
   } catch (err) {
     console.log(`[midway-bin] Not found framework ${moduleName} and skip.`);
   }
+}
+
+/**
+ * Retrieve correct module path
+ * @param {string} moduleName module name
+ * @return {string} path
+ */
+function retrieveModulePath(moduleName) {
+  const paths = require.resolve.paths(moduleName);
+
+  const moduleDir = paths.find(dir => {
+    const mpath = path.join(dir, moduleName);
+    try {
+      fs.accessSync(mpath, fs.constants.R_OK);
+      const stats = fs.statSync(mpath);
+      return stats && stats.isDirectory();
+    } catch (ex) {
+      return false;
+    }
+  });
+
+  return moduleDir ? path.join(moduleDir, moduleName) : '';
 }
 
 exports.resolveModule = resolveModule;
