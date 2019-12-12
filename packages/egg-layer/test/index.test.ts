2499452f38c463177ed876dba27ea9c3b1a8a9ff
Lxxyx <Lxxyxzj@gmail.com>
2019-12-12 13:38:01 +0800

    ✨ feat(egg-layer): 增加 scf 测试用例

--- packages/egg-layer/test/index.test.ts
+++ packages/egg-layer/test/index.test.ts
@@ -1,5 +1,6 @@
 import { createRuntime } from '@midwayjs/runtime-mock';
 import { HTTPTrigger } from '@midwayjs/serverless-fc-trigger';
+import { ApiGatewayTrigger } from '@midwayjs/serverless-scf-trigger';
 import { join } from 'path';
 import * as request from 'supertest';
 
@@ -21,6 +22,33 @@ describe('/test/index.test.ts', () => {
       });
       await runtime.start();
       const app = await runtime.delegate(new HTTPTrigger());
+      return new Promise(resolve => {
+        request(app)
+          .get('/user')
+          .expect('Content-Type', 'text/html; charset=utf-8')
+          .expect(/hello Alan/)
+          .expect(200, resolve);
+      });
+    });
+  });
+
+  describe('SCF should test apigateway trigger use app directly', () => {
+    let runtime;
+
+    afterEach(() => {
+      if (runtime) {
+        runtime.close();
+      }
+    });
+
+    it('should test with supertest', async () => {
+      const entryDir = join(__dirname, './fixtures/eaas');
+      process.env.ENTRY_DIR = entryDir;
+      runtime = createRuntime({
+        functionDir: entryDir,
+      });
+      await runtime.start();
+      const app = await runtime.delegate(new ApiGatewayTrigger());
       return new Promise(resolve => {
         request(app)
           .get('/user')
