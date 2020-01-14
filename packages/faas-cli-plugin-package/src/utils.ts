31ee6307e517709580e55b9b4942cf97c6896691
Gao Yang <echosoar@qq.com>
2020-01-14 19:44:27 +0800

    fix: layer format (#22)
    * fix: remove pkg unit test
    
    * fix: remove serverless-midway-plugin
    
    * fix: export all invoke function
    
    * fix: layer check

--- packages/faas-cli-plugin-package/src/utils.ts
+++ packages/faas-cli-plugin-package/src/utils.ts
@@ -5,9 +5,12 @@ interface Ilayer {
 }
 
 export function formatLayers(...multiLayers: Ilayer[]) {
-  const layerTypeList = { npm: {} };
+  const layerTypeList = { npm: {}, oss: {} };
   multiLayers.forEach((layer: Ilayer) => {
     Object.keys(layer || {}).forEach(layerName => {
+      if (!layer[layerName] || !layer[layerName].path) {
+        return;
+      }
       const [type, path] = layer[layerName].path.split(':');
       if (!layerTypeList[type]) {
         return;
