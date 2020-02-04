1e3051227346962c593eceedacd62461667bcd0f
Harry Chen <czy88840616@gmail.com>
2020-02-04 21:18:49 +0800

    chore: add middleware for faas

--- packages/midway-decorator/src/faas/fun.ts
+++ packages/midway-decorator/src/faas/fun.ts
@@ -1,11 +1,24 @@
 import { saveClassMetadata, saveModule, scope, ScopeEnum } from 'injection';
 import { FUNC_KEY } from '../constant';
+import { KoaMiddlewareParamArray } from '..';
 
-export function Func(funHandler: string): ClassDecorator {
+export function Func(
+  funHandler: string,
+  functionOptions: {
+    middleware?: KoaMiddlewareParamArray;
+  } = { middleware: [] }
+): ClassDecorator {
   return (target: any) => {
     // 保存到扫描列表
     saveModule(FUNC_KEY, target);
-    saveClassMetadata(FUNC_KEY, funHandler, target);
+    saveClassMetadata(
+      FUNC_KEY,
+      {
+        funHandler,
+        middleware: functionOptions.middleware,
+      },
+      target
+    );
     // 注册数据
     scope(ScopeEnum.Request)(target);
   };
