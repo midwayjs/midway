27779a50e5dc39693f77b2cab02510be779615bc
Harry Chen <czy88840616@gmail.com>
2020-02-21 13:12:59 +0800

    chore: add hooks

---
+++ .githooks/pre-commit/filter.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+EMAIL=$(git config user.email)
+if [[ $EMAIL == *"alibaba-inc"* ]] || [[ $EMAIL == *"taobao"* ]]
+then
+echo "email with *.alibaba-inc.com or *.taobao.com are not allowed";
+exit 1;
+else
+echo "";
+fi;
