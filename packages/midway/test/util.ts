027794a6757856e32213fd4f09715aeb9f0a6ab2
waiting <1661926154@qq.com>
2020-02-21 22:46:22 +0800

    chore(midway): eslint fix

--- packages/midway/test/util.ts
+++ packages/midway/test/util.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const { mm } = require('../../midway-mock/dist');
+
 
 export function cluster(name, options?) {
   options = Object.assign(
