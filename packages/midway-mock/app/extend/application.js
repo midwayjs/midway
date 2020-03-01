ba5afd356765557aeb87525cb05e33cb64fcb016
waiting <1661926154@qq.com>
2020-03-01 16:28:47 +0800

    refactor(midway-mock): application.js with ts

--- packages/midway-mock/app/extend/application.js
+++ packages/midway-mock/app/extend/application.js
@@ -1,17 +1,10 @@
-module.exports = {
-  mockClassFunction(className, methodName, fn) {
-    console.log('debug1: ', this)
-    console.log('debug1: ', this.applicationContext)
-    console.log('debug2: ', typeof this.applicationContext.registry)
-    const def = this.applicationContext.registry.getDefinition(className);
-    if (! def) {
-      throw new TypeError(`def undefined with className: "${className}", methodName: "${methodName}"`)
-    }
-    else {
-      const clazz = def.path;
-      if (clazz && typeof clazz === 'function') {
-        this._mockFn(clazz.prototype, methodName, fn);
-      }
-    }
-  }
-};
+"use strict";
+
+// exports = require('../../dist/app/extend/application')
+
+function __export(m) {
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+__export(require("../../dist/app/extend/application"));
+
