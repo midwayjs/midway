239d6cd040ad7f2cdd76edf548c1cddcc33f881e
waiting <1661926154@qq.com>
2020-03-01 17:47:23 +0800

    chore(midway-mock): eslint fix

--- packages/midway-mock/src/bootstrap.ts
+++ packages/midway-mock/src/bootstrap.ts
@@ -1,6 +1,8 @@
 import * as assert from 'power-assert';
+
 import { MidwayMockApplication } from './interface';
 import { MidwayMock, mm as mock } from './mock';
+
 
 // 由于使用Object.assign，丢了默认的mm执行函数，所以使用default输出mm
 export const mm: MidwayMock['default'] = mock.default;
