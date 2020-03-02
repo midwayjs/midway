5dfab29d567c6093ac86387c9e45b392f5ba8d46
waiting <1661926154@qq.com>
2020-03-02 18:24:06 +0800

    refactor: Midway mock refactor (#410)
    * refactor(midway-mock): application.js with ts
    
    * ci: add configuration for GitHub
    
    only nodejs v12
    
    * fix(midway-mock): package.json
    
    * ci: fix nodejs.yml
    
    * ci: dummy
    
    * ci: dummy
    
    * refactor(midway-mock): application.js
    
    Co-authored-by: Harry Chen <czy88840616@gmail.com>
    
    (cherry picked from commit df8a7b090c1777f6ec6d5419ad7a9b4c0c7a394c)

--- packages/midway-mock/app/extend/application.js
+++ packages/midway-mock/app/extend/application.js
@@ -1,9 +1,4 @@
-module.exports = {
-  mockClassFunction(className, methodName, fn) {
-    const def = this.applicationContext.registry.getDefinition(className);
-    const clazz = def.path;
-    if (clazz && typeof clazz === 'function') {
-      this._mockFn(clazz.prototype, methodName, fn);
-    }
-  }
-};
+"use strict";
+
+module.exports = require('../../dist/app/extend/application')
+
