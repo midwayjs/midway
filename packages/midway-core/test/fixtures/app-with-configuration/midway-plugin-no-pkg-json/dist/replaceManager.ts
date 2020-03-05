c8eb0dc1bfabf88514754d5fee42a83854bb4de1
kurten <chinkurten@gmail.com>
2020-03-05 15:21:47 +0800

    add fixtures

---
+++ packages/midway-core/test/fixtures/app-with-configuration/midway-plugin-no-pkg-json/dist/replaceManager.ts
@@ -0,0 +1,12 @@
+import { Config, Provide } from '@midwayjs/decorator';
+
+@Provide()
+export class ReplaceManager {
+
+  @Config('ok.text')
+  config;
+
+  async getOne() {
+    return this.config;
+  }
+}
