4c750a5ed1e3e95f84ad5ef2d2d9d41c80c9ae42
Harry Chen <czy88840616@gmail.com>
2020-03-13 19:31:54 +0800

    fix: fix export

--- packages/midway-decorator/src/rpc/hsf.ts
+++ packages/midway-decorator/src/rpc/hsf.ts
@@ -1,14 +1,14 @@
 import { ScopeEnum, saveClassMetadata, saveModule, HSF_KEY } from '../common';
 import { Scope } from '../annotation';
 
-export interface HsfOpts {
+export interface HSFOpts {
   interfaceName?: string;
   version?: string;
   group?: string;
   namespace?: string;
 }
 
-export function hsf(hsfOption: HsfOpts = {}): ClassDecorator {
+export function HSF(hsfOption: HSFOpts = {}): ClassDecorator {
   return (target: any) => {
     saveModule(HSF_KEY, target);
     saveClassMetadata(HSF_KEY, hsfOption, target);
