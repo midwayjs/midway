2576fd42696385baf49e0624c01b5bd48eaa64b3
stone-jin <1520006273@qq.com>
2020-03-14 02:51:15 +0800

    fix: can midway build when tsconfig.json has comments

--- packages/midway-bin/test/lib/util.test.js
+++ packages/midway-bin/test/lib/util.test.js
@@ -8,7 +8,7 @@ const util = require('../../lib/util.js');
 describe('test/lib/util.test.js', () => {
 
   it('should return full path of module', function() {
-    const moduleName = 'egg'
+    const moduleName = 'egg-bin'
     const path = util.resolveModule(moduleName)
     console.log('resolved path is:', path)
     assert(path && path.endsWith(`${pathx.sep}${moduleName}`));
