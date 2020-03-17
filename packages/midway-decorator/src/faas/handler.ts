b8c07e9bedb3bcd32be05701514aa64e09b38cc7
冰森 <dingheng.hdh@alibaba-inc.com>
2020-03-17 22:51:20 +0800

    feat: intecept support for @Handler

--- packages/midway-decorator/src/faas/handler.ts
+++ packages/midway-decorator/src/faas/handler.ts
@@ -1,7 +1,17 @@
 import { savePropertyDataToClass, HANDLER_KEY } from '../common';
 
+let inteceptor;
+
+export function HandlerIntercept(fn) {
+  inteceptor = fn;
+}
+
 export function Handler(handlerMapping: string): MethodDecorator {
   return (target: object, propertykey: string, descriptor: PropertyDescriptor) => {
+    if (typeof inteceptor === 'function') {
+      const method = descriptor.value;
+      descriptor.value = inteceptor(target, propertykey, method, handlerMapping);
+    }
     savePropertyDataToClass(HANDLER_KEY, {
       method: propertykey,
       data: handlerMapping,
