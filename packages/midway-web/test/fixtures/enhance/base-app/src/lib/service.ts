73df21c24333931010848c3d246a3297ebe38535
Harry Chen <czy88840616@gmail.com>
2020-03-19 20:06:38 +0800

    chroe: add ctx

--- packages/midway-web/test/fixtures/enhance/base-app/src/lib/service.ts
+++ packages/midway-web/test/fixtures/enhance/base-app/src/lib/service.ts
@@ -1,8 +1,11 @@
-import { Provide } from '@midwayjs/decorator';
+import { Provide, Inject } from '@midwayjs/decorator';
 
 @Provide()
 export class UserService {
+  @Inject()
+  ctx;
+
   async hello() {
-    return 'world';
+    return 'world,' + Object.keys(this.ctx.query).length;
   }
 }
