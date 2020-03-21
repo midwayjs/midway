e95021fc7c46247701e19b3b2de00ddaad396d67
Harry Chen <czy88840616@gmail.com>
2020-03-21 13:11:53 +0800

    fix: add interface export

--- packages/midway-mock/src/interface.ts
+++ packages/midway-mock/src/interface.ts
@@ -1,29 +1,27 @@
-import { MockOption, MockApplication  } from 'egg-mock';
-import { ApplicationContext, IApplicationContext } from 'injection';
-
+import { MockApplication, MockOption } from 'egg-mock';
+import { IApplicationContext } from 'injection';
 
 export interface MidwayApplicationOptions extends MockOption {
-  baseDir?: string
-  framework?: string
-  plugin?: any
-  plugins?: any
-  container?: any
-  typescript?: boolean
-  worker?: number
+  baseDir?: string;
+  framework?: string;
+  plugin?: any;
+  plugins?: any;
+  container?: any;
+  typescript?: boolean;
+  worker?: number;
 }
 
 export interface MidwayMockApplication extends MockApplication {
-  applicationContext: ApplicationContext
-  pluginContext: IApplicationContext
-  appDir: string
-  baseDir: string
-  enablePlugins: any
-  getApplicationContext(): IApplicationContext
-  getPluginContext(): IApplicationContext
-  getPlugin(pluginName: string): any
-  getLogger(name?: string): any
-  getConfig(key?: string): any
-
+  applicationContext: IApplicationContext;
+  pluginContext: IApplicationContext;
+  appDir: string;
+  baseDir: string;
+  enablePlugins: any;
+  getApplicationContext(): IApplicationContext;
+  getPluginContext(): IApplicationContext;
+  getPlugin(pluginName: string): any;
+  getLogger(name?: string): any;
+  getConfig(key?: string): any;
   /**
    * Mock class function
    */
@@ -34,4 +32,3 @@ export interface MidwayMockApplication extends MockApplication {
     fnOrData: any,
   ): any
 }
-
