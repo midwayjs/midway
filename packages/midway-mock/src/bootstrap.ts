f2806b6bca64e6dbc7ba940e13e2b87a1e9d9a27
Lxxyx <Lxxyxzj@gmail.com>
2020-04-09 03:48:16 +0000

    fix: jest support

--- packages/midway-mock/src/bootstrap.ts
+++ packages/midway-mock/src/bootstrap.ts
@@ -2,12 +2,19 @@ import * as assert from 'power-assert';
 import { MidwayMockApplication } from './interface';
 import { MidwayMock, mm as mock } from './mock';
 
+const isJest = process.env.MIDWAY_JEST_MODE === 'true';
+
 // 由于使用Object.assign，丢了默认的mm执行函数，所以使用default输出mm
 export const mm: MidwayMock['default'] = mock.default;
 
-export const app: MidwayMockApplication = mock.app({});
+export const app: MidwayMockApplication = mock.app({ typescript: isJest });
 
-before(app.ready);
+if (isJest) {
+  (global as any).beforeAll(app.ready)
+} else {
+  before(app.ready);
+}
+
 afterEach(mock.restore);
 
 export {
