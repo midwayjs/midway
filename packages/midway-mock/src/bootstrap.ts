173b44feb710247aaecc3e8bae585f25e872d9ee
Lxxyx <Lxxyxzj@gmail.com>
2020-04-09 04:22:22 +0000

    fix: midway-mock test

--- packages/midway-mock/src/bootstrap.ts
+++ packages/midway-mock/src/bootstrap.ts
@@ -7,7 +7,7 @@ const isJest = process.env.MIDWAY_JEST_MODE === 'true';
 // 由于使用Object.assign，丢了默认的mm执行函数，所以使用default输出mm
 export const mm: MidwayMock['default'] = mock.default;
 
-export const app: MidwayMockApplication = mock.app({ typescript: isJest });
+export const app: MidwayMockApplication = mock.app(isJest ? {typescript: true} : {});
 
 if (isJest) {
   (global as any).beforeAll(app.ready);
