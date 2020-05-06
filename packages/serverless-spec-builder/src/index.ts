53f40ef62b2b35ede3c9b667079c5126d41e6804
Harry Chen <czy88840616@gmail.com>
2020-05-06 13:14:51 +0800

    feat: support yaml with ${env.variable} (#157)

--- packages/serverless-spec-builder/src/index.ts
+++ packages/serverless-spec-builder/src/index.ts
@@ -7,13 +7,20 @@ export * from './interface';
 export * from './builder';
 export * from './wrapper';
 
+const pattern = /\$\{\s*(\w+\.\w+)\s*\}/g;
+
 export const transform = (sourcefilePathOrJson: any, builderCls?) => {
   let result: any = sourcefilePathOrJson;
   if (typeof sourcefilePathOrJson === 'string') {
     if (fs.existsSync(sourcefilePathOrJson)) {
       const content = fs.readFileSync(sourcefilePathOrJson, 'utf8');
+      const yamlContent = content.replace(pattern, (match, key, value) => {
+        if(key.startsWith('env.')) {
+          return process.env[key.replace('env.', '')] || match;
+        }
+      });
       // replace
-      result = parse(sourcefilePathOrJson, content);
+      result = parse(sourcefilePathOrJson, yamlContent);
     }
   }
   if (!result) {
--- packages/serverless-spec-builder/test/index.test.ts
+++ packages/serverless-spec-builder/test/index.test.ts
@@ -2,6 +2,7 @@ import { transform, SpecBuilder, generate } from '../src';
 import * as path from 'path';
 import * as assert from 'assert';
 import * as fs from 'fs';
+import * as mm from 'mm';
 
 describe('/test/index.test.ts', () => {
 
@@ -14,6 +15,19 @@ describe('/test/index.test.ts', () => {
     it('test transform yaml', () => {
       const result = transform(path.join(__dirname, './fixtures/fun.yaml'));
       assert(result['functions']['hello']['handler'] === 'index.handler');
+    });
+
+    it('test add variable for yml', () => {
+      mm(process.env, 'FAAS_PROVIDER', 'fc');
+      mm(process.env, 'FN_HANDLER', 'index.handler');
+      mm(process.env, 'FN_HTTP_PATH', '/foo');
+      mm(process.env, 'FN_HTTP_METHOD', 'get');
+      const result = transform(path.join(__dirname, './fixtures/fun-var.yml'));
+      assert(result['provider']['name'] === 'fc');
+      assert(result['provider']['runtime'] === '${ env.NODEJS_VERSION }');
+      assert(result['functions']['hello']['events'][0].http.path === '/foo');
+      assert(result['functions']['hello']['events'][0].http.method === 'get');
+      mm.restore();
     });
   });
 
