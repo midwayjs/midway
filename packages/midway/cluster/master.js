dc788a387d49af34687d664dcbb285419f12f4ae
Yorkie Liu <yorkiefixer@gmail.com>
2020-05-19 23:32:55 +0800

    chore(cluster): remove printing egg versions (#491)

--- packages/midway/cluster/master.js
+++ packages/midway/cluster/master.js
@@ -4,11 +4,7 @@ const formatOptions = require('./utils').formatOptions;
 class Master extends EggMaster {
 
   constructor(options) {
-    options = formatOptions(options);
-    super(options);
-    this.log('[master] egg version %s, egg-core version %s',
-      require('egg/package').version,
-      require('egg-core/package').version);
+    super(formatOptions(options));
   }
 
 }
