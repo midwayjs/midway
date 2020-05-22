c3899d03e08cceb22ad3cb9b6c3785517de30e5d
waiting <1661926154@qq.com>
2020-05-23 00:13:15 +0800

    feat: Eslint contrib (#496)

--- packages/eslint-midway-contrib/base.js
+++ packages/eslint-midway-contrib/base.js
@@ -6,7 +6,11 @@ module.exports = {
     mocha: true,
     node: true,
   },
-  extends: ['./plugins/import.yml'],
+  extends: [
+    './plugins/import.yml',
+    './plugins/node.yml',
+    // './plugins/unicorn.yml',
+  ],
   globals: {
     Atomics: 'readonly',
     document: 'readonly',
@@ -22,7 +26,7 @@ module.exports = {
       impliedStrict: true,
     },
     ecmaVersion: 2020,
-    // see: https://github.com/typescript-eslint/typescript-eslint/releases/tag/v2.0.0 
+    // see: https://github.com/typescript-eslint/typescript-eslint/releases/tag/v3.0.0
     project: 'tsconfig.json',
     sourceType: 'module',
   },
