3a3ac797171c4a78742ab74406ca63615e98a882
Harry Chen <czy88840616@gmail.com>
2020-07-03 19:24:16 +0800

    test: add logger case

--- packages/faas/test/index.test.ts
+++ packages/faas/test/index.test.ts
@@ -208,4 +208,32 @@ describe('test/index.test.ts', () => {
 
     assert(data.body === 'ahello123');
   });
+
+  it('test inject logger', async () => {
+    const { start } = require('../../serverless-scf-starter/src');
+    const runtime = await start();
+    const starter = new FaaSStarter({
+      baseDir: join(__dirname, './fixtures/base-app-inject-logger'),
+      applicationAdapter: runtime,
+    });
+    // set app
+    const app = runtime.getApplication();
+    app.mysql = {
+      model: '123',
+    };
+    await starter.start();
+    const data = await runtime.asyncEvent(
+      starter.handleInvokeWrapper('index.handler')
+    )(
+      {
+        text: 'hello',
+        httpMethod: 'GET',
+        headers: {},
+        requestContext: {},
+      },
+      { text: 'a' }
+    );
+
+    assert(data.body === 'hello world');
+  });
 });
