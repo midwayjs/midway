e5340265e46627c7df22ff8480bedc76c5b153d4
Harry Chen <czy88840616@gmail.com>
2020-07-17 17:37:49 +0800

    chore: add koa-router

--- packages/koa-layer/test/index.test.js
+++ packages/koa-layer/test/index.test.js
@@ -5,8 +5,19 @@ const { join } = require('path');
 const request = require('supertest');
 
 describe('/test/index.test.ts', () => {
-  describe.only('FC test', () => {
+  describe('FC test', () => {
     let runtime;
+    let app;
+
+    before(async () => {
+      const entryDir = join(__dirname, './fixtures/app');
+      process.env.ENTRY_DIR = entryDir;
+      runtime = createRuntime({
+        functionDir: entryDir,
+      });
+      await runtime.start();
+      app = await runtime.delegate(new HTTPTrigger());
+    });
 
     afterEach(() => {
       if (runtime) {
@@ -16,48 +27,20 @@ describe('/test/index.test.ts', () => {
       delete require.cache[require.resolve('./fixtures/app/index.js')];
     });
 
-    it('should test with get', async () => {
-      const entryDir = join(__dirname, './fixtures/app');
-      process.env.ENTRY_DIR = entryDir;
-      runtime = createRuntime({
-        functionDir: entryDir,
-      });
-      await runtime.start();
-      const app = await runtime.delegate(new HTTPTrigger());
-      return new Promise((resolve, reject) => {
-        request(app)
-          .get('/get')
-          .expect('Content-Type', 'text/plain; charset=utf-8')
-          .expect(/Hello World/)
-          .expect(200, err => {
-            if (err) {
-              reject(err);
-            }
-            resolve();
-          });
-      });
+    it('should test with get', done => {
+      request(app)
+        .get('/get')
+        .expect('Content-Type', 'text/plain; charset=utf-8')
+        .expect(/Hello World/)
+        .expect(200, done);
     });
 
-    it('should test with post', async () => {
-      const entryDir = join(__dirname, './fixtures/app');
-      process.env.ENTRY_DIR = entryDir;
-      runtime = createRuntime({
-        functionDir: entryDir,
-      });
-      await runtime.start();
-      const app = await runtime.delegate(new HTTPTrigger());
-      return new Promise((resolve, reject) => {
-        request(app)
-          .post('/post')
-          .expect('Content-Type', 'text/html; charset=utf-8')
-          .expect(/Hello World, post/)
-          .expect(200, err => {
-            if (err) {
-              reject(err);
-            }
-            resolve();
-          });
-      });
+    it('should test with post', done => {
+      request(app)
+        .post('/post')
+        .expect('Content-Type', 'text/html; charset=utf-8')
+        .expect(/Hello World, post/)
+        .expect(200, done);
     });
   });
 
