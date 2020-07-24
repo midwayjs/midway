109d7ec871b0014e35d74e32fe14c286bc38b232
Harry Chen <czy88840616@gmail.com>
2020-07-24 17:55:44 +0800

    chore: add rollback script

---
+++ scripts/rollback.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process');
+const { writeFileSync } = require('fs');
+const { join } = require('path');
+
+const originData = execSync('npx lerna ls --json').toString();
+const data = JSON.parse(originData);
+
+const arr = ['#!/bin/bash\n', `# timestamp: ${Date.now()}\n\n`];
+const diff = ['\n# Changes:\n\n'];
+
+for (const item of data) {
+  console.log('---->', item.name);
+  if (item.private === false) {
+    const version = execSync(
+      `npm show ${item.name} version`
+    ).toString().replace('\n', '');
+    arr.push(
+      `tnpm dist-tag add ${item.name}@${version} latest\n`
+    );
+    diff.push(`#  - ${item.name}: ${version} => 3.2.9\n`);
+  }
+}
+
+writeFileSync(join(__dirname, 'rollback.sh'), arr.join('') + diff.join(''));
---
+++ scripts/rollback.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# timestamp: 1595584149121
+
+tnpm dist-tag add @midwayjs/egg-layer@1.1.0 latest
+tnpm dist-tag add @midwayjs/express-layer@1.1.0 latest
+tnpm dist-tag add @midwayjs/fcli-command-core@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-aws@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-create@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-deploy@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-dev-pack@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-fc@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-invoke@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-package@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-scf@1.1.2 latest
+tnpm dist-tag add @midwayjs/fcli-plugin-test@1.1.2 latest
+tnpm dist-tag add @midwayjs/faas-cli@1.1.2 latest
+tnpm dist-tag add @midwayjs/faas-code-analysis@1.1.0 latest
+tnpm dist-tag add @midwayjs/faas-dev-pack@1.1.2 latest
+tnpm dist-tag add @midwayjs/faas-typings@1.1.0 latest
+tnpm dist-tag add @midwayjs/faas-util-ts-compile@1.0.11 latest
+tnpm dist-tag add @midwayjs/faas@1.1.1 latest
+tnpm dist-tag add @midwayjs/gateway-common-core@1.1.2 latest
+tnpm dist-tag add @midwayjs/gateway-common-http@1.1.2 latest
+tnpm dist-tag add @midwayjs/koa-layer@1.1.0 latest
+tnpm dist-tag add @midwayjs/runtime-engine@1.1.0 latest
+tnpm dist-tag add @midwayjs/runtime-mock@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-aws-starter@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-fc-starter@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-fc-trigger@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-http-parser@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-invoke@1.1.2 latest
+tnpm dist-tag add @midwayjs/serverless-meta-json@1.1.1 latest
+tnpm dist-tag add @midwayjs/serverless-scf-starter@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-scf-trigger@1.1.0 latest
+tnpm dist-tag add @midwayjs/serverless-spec-builder@1.1.2 latest
+tnpm dist-tag add @midwayjs/simple-lock@1.0.7 latest
+
+# Changes:
+
+#  - @midwayjs/egg-layer: 1.1.0 => 3.2.9
+#  - @midwayjs/express-layer: 1.1.0 => 3.2.9
+#  - @midwayjs/fcli-command-core: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-aws: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-create: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-deploy: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-dev-pack: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-fc: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-invoke: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-package: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-scf: 1.1.2 => 3.2.9
+#  - @midwayjs/fcli-plugin-test: 1.1.2 => 3.2.9
+#  - @midwayjs/faas-cli: 1.1.2 => 3.2.9
+#  - @midwayjs/faas-code-analysis: 1.1.0 => 3.2.9
+#  - @midwayjs/faas-dev-pack: 1.1.2 => 3.2.9
+#  - @midwayjs/faas-typings: 1.1.0 => 3.2.9
+#  - @midwayjs/faas-util-ts-compile: 1.0.11 => 3.2.9
+#  - @midwayjs/faas: 1.1.1 => 3.2.9
+#  - @midwayjs/gateway-common-core: 1.1.2 => 3.2.9
+#  - @midwayjs/gateway-common-http: 1.1.2 => 3.2.9
+#  - @midwayjs/koa-layer: 1.1.0 => 3.2.9
+#  - @midwayjs/runtime-engine: 1.1.0 => 3.2.9
+#  - @midwayjs/runtime-mock: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-aws-starter: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-fc-starter: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-fc-trigger: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-http-parser: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-invoke: 1.1.2 => 3.2.9
+#  - @midwayjs/serverless-meta-json: 1.1.1 => 3.2.9
+#  - @midwayjs/serverless-scf-starter: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-scf-trigger: 1.1.0 => 3.2.9
+#  - @midwayjs/serverless-spec-builder: 1.1.2 => 3.2.9
+#  - @midwayjs/simple-lock: 1.0.7 => 3.2.9
