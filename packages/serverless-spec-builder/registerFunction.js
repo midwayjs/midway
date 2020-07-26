106bfacff0f762fe561e026f883158f0bd7c3cdc
Lxxyx <Lxxyxzj@gmail.com>
2020-07-26 16:11:52 +0800

    fix: 参数获取不正确 & ctx 符合格式 (#556)

--- packages/serverless-spec-builder/registerFunction.js
+++ packages/serverless-spec-builder/registerFunction.js
@@ -6,30 +6,34 @@ const {
 } = require('@midwayjs/decorator');
 const { join } = require('path');
 const { existsSync } = require('fs');
-const registerFunctionToIoc = (container, funHandler, fun) => {
-  class Fun {
+const get = require('lodash.get');
+
+const registerFunctionToIoc = (container, functionName, func) => {
+  class FunctionContainer {
     async handler(event) {
-      const _this = {
-        context: this['REQUEST_OBJ_CTX_KEY'],
+      const bindCtx = {
+        ctx: this.ctx,
         event,
       };
-      const args =
-        (_this.context &&
-          _this.context.req &&
-          _this.context.req.body &&
-          _this.context.req.body.args) ||
-        [];
-      return fun.bind(_this)(...args);
+
+      /**
+       * HTTP Case
+       */
+      const args = get(this.ctx, 'request.body.args', []);
+
+      return func.bind(bindCtx)(...args);
     }
   }
-  const id = 'bind_func::' + funHandler;
-  saveProviderId(id, Fun, true);
-  container.bind(id, Fun);
-  saveModule(FUNC_KEY, Fun);
+
+  const id = 'bind_func::' + functionName;
+
+  saveProviderId(id, FunctionContainer, true);
+  container.bind(id, FunctionContainer);
+  saveModule(FUNC_KEY, FunctionContainer);
   attachClassMetadata(
     FUNC_KEY,
-    Object.assign({ funHandler, key: 'handler' }, {}),
-    Fun
+    { funHandler: functionName, key: 'handler' },
+    FunctionContainer
   );
 };
 
@@ -37,26 +41,32 @@ const registerFunctionToIocByConfig = (config, options) => {
   if (!config || !config.functionList) {
     return;
   }
+
   const { baseDir = process.cwd() } = options || {};
+
   config.functionList.forEach(functionInfo => {
     const { functionName, functionFilePath, functionHandler } = functionInfo;
 
     if (!functionFilePath) {
       return;
     }
+
     const functionPath = join(baseDir, functionFilePath);
+
     if (!existsSync(functionPath)) {
       return;
     }
+
     try {
-      const functionExport = require(functionPath);
-      if (!functionExport || !functionExport[functionName]) {
+      const exportMods = require(functionPath);
+      if (!exportMods || !exportMods[functionName]) {
         return;
       }
+
       registerFunctionToIoc(
         options.context,
         functionHandler || `${functionName}.handler`,
-        functionExport[functionName]
+        exportMods[functionName]
       );
     } catch {
       //
