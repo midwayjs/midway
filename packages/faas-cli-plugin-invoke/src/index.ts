94b400f21c744f97f0d055decbafacb9fc152026
Lxxyx <Lxxyxzj@gmail.com>
2020-07-26 15:20:16 +0000

    chore: JSON 输出时自动格式化，确保可读性

--- packages/faas-cli-plugin-invoke/src/index.ts
+++ packages/faas-cli-plugin-invoke/src/index.ts
@@ -288,7 +288,7 @@ export class FaaSInvokePlugin extends BasePlugin {
       this.setStore('functions', this.core.service.functions);
       writeFileSync(
         this.analysisCodeInfoPath,
-        JSON.stringify(newSpec.functions)
+        JSON.stringify(newSpec.functions, null, 2)
       );
     }
     if (this.core.pluginManager.options.stopLifecycle === 'invoke:compile') {
@@ -340,7 +340,7 @@ export class FaaSInvokePlugin extends BasePlugin {
       }
     });
     ensureFileSync(this.buildLockPath);
-    writeFileSync(this.buildLockPath, JSON.stringify(this.fileChanges));
+    writeFileSync(this.buildLockPath, JSON.stringify(this.fileChanges, null, 2));
   }
 
   async setFunctionList() {
@@ -349,7 +349,7 @@ export class FaaSInvokePlugin extends BasePlugin {
     // 将函数信息放入代码分析结果缓存中，便于下次跳过ts编译时使用
     writeFileSync(
       this.analysisCodeInfoPath,
-      JSON.stringify(this.core.service.functions)
+      JSON.stringify(this.core.service.functions, null, 2)
     );
   }
 
