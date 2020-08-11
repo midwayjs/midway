137dd036bde70a0f0c6a7ed98596da7c17b4a188
Harry Chen <czy88840616@gmail.com>
2020-08-11 14:11:36 +0800

    fix: fix global middleware load order (#594)

--- packages/faas/test/fixtures/base-app-middleware/src/configuration.ts
+++ packages/faas/test/fixtures/base-app-middleware/src/configuration.ts
@@ -3,4 +3,8 @@ import { Configuration } from '@midwayjs/decorator';
 @Configuration({
   importConfigs: ['./config.default'],
 })
-export class AutoConfiguraion {}
+export class AutoConfiguraion {
+  async onReady(container) {
+    container.registerObject('adb', { data: '123' });
+  }
+}
