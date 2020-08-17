dbde4760b4a85bc46658a2ef0d6348f0f508fa4f
Lxxyx <Lxxyxzj@gmail.com>
2020-08-17 11:02:57 +0800

    chore: faster sync script (#602)

--- scripts/sync_status.js
+++ scripts/sync_status.js
@@ -1,26 +1,35 @@
 const { execSync } = require('child_process');
+const execa = require('execa');
 
 const originData = execSync('npx lerna ls --json').toString();
 const data = JSON.parse(originData);
 
-const arr = [];
+const failed = [];
+const finished = [];
 
-for (const item of data) {
-  console.log('---->', item.name);
-  if (item.private === false) {
-    const npmVersion = execSync(`npm show ${item.name} version`)
-      .toString()
-      .replace('\n', '');
-    const tnpmVersion = execSync(`tnpm show ${item.name} version`)
-      .toString()
-      .replace('\n', '');
-    if (npmVersion !== tnpmVersion) {
-      console.log(`===> npm: ${npmVersion}, tnpm: ${tnpmVersion}`);
-      arr.push(item.name);
-    }
+async function checkSyncStatus(pkg) {
+  const npmVersion = await execa('npm', ['show', pkg, 'version']);
+  const tnpmVersion = await execa('tnpm', ['show', pkg, 'version']);
+
+  finished.push(pkg);
+  console.log(`[${finished.length}/${data.length}] ---->`, pkg);
+  if (npmVersion.stdout !== tnpmVersion.stdout) {
+    console.log(`===> npm: ${npmVersion.stdout}, tnpm: ${tnpmVersion.stdout}`);
+    failed.push(item.name);
   }
 }
 
-if (arr.length) {
-  console.log(`output command => tnpm sync ${arr.join(' ')}`);
+async function start() {
+  const packages = data
+    .filter((item) => item.private === false)
+    .map((item) => item.name);
+
+  const task = packages.map((pkg) => checkSyncStatus(pkg));
+  await Promise.all(task);
+
+  if (failed.length) {
+    console.log(`output command => tnpm sync ${failed.join(' ')}`);
+  }
 }
+
+start();
