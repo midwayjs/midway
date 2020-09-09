8547e5754593ded089d96b5bcf097bc3bc7f542e
Harry Chen <czy88840616@gmail.com>
2020-09-09 22:43:18 +0800

    feat: update midway-bin

--- packages/midway-bin/bin/midway-bin.js
+++ packages/midway-bin/bin/midway-bin.js
@@ -1,6 +1,33 @@
 #!/usr/bin/env node
 
 'use strict';
+const { debugWrapper } = require('@midwayjs/debugger');
+const cliFun = async argv => {
+  require('source-map-support/register');
+  const { CLI } = require('../dist');
+  const cli = new CLI(argv);
+  cli
+    .start()
+    .then(() => {
+      process.exit();
+    })
+    .catch(() => {
+      process.exitCode = 1;
+      process.exit(1);
+    });
+};
 
-const Command = require('../').MidwayBin;
-new Command().start();
+const cli = argv => {
+  const isDebug = argv.debug;
+  delete argv.debug;
+  debugWrapper({
+    file: __filename, // 要包裹的方法所在文件
+    export: 'cliFun', // 要包裹的方法的方法名
+    debug: isDebug,
+  })(argv);
+};
+
+module.exports = {
+  cliFun,
+  cli,
+};
