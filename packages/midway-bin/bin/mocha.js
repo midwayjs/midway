472db8fecae6ebf54c78372bbfef3a33e64cfcd6
Harry Chen <czy88840616@gmail.com>
2020-09-11 20:56:39 +0800

    revert midway-bin

---
+++ packages/midway-bin/bin/mocha.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+'use strict';
+
+require('egg-bin/bin/mocha');
---
+++ packages/midway-bin/lib/cmd/pkgfiles.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class PkgfilesCommand extends require('egg-bin').PkgfilesCommand {
+  constructor(rawArgv) {
+    super(rawArgv);
+    this.usage = 'Usage: midway-bin pkgfiles';
+  }
+}
+
+module.exports = PkgfilesCommand;
