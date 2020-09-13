e4c8b6021a311718196b9b9b303309e7e4179113
Harry Chen <czy88840616@gmail.com>
2020-09-13 14:30:30 +0800

    chore: ignore err test

---
+++ packages/mock/test/fixtures/base-app-new/src/controller/hone.ts
@@ -0,0 +1,11 @@
+import { Controller, Get, Provide, Query } from '@midwayjs/decorator';
+
+@Provide()
+@Controller('/')
+export class HomeController {
+
+  @Get()
+  async index(@Query('name') name) {
+    return `hello world, ${name}`;
+  }
+}
