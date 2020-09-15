71f9358b736b9e5f7f8c604be38ca53582863e1e
legendecas <legendecas@gmail.com>
2020-09-15 14:07:41 +0800

    feat: type extension bootstrap

---
+++ examples/hello-world/src/config/config.default.ts
@@ -0,0 +1,5 @@
+import { EggAppInfo } from '@midwayjs/web';
+
+export = (appInfo: EggAppInfo) => {
+  appInfo.appDir
+}
--- packages/web/src/type-extends/egg.ts
+++ packages/web/src/type-extends/egg.ts
@@ -1,6 +1,3 @@
-///<reference types="egg" />
-///<reference path="./dist/index.d.ts" />
-
 declare module 'egg' {
   interface EggAppInfo {
     appDir: string;
