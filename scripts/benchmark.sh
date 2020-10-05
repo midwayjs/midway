7e26507041223bcc7ddf13dfdf3f26427bec1077
Harry Chen <czy88840616@gmail.com>
2020-10-05 15:53:03 +0800

    chore: add yml

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -2,9 +2,11 @@
 
 npm install @midwayjs/cli autocannon
 ./node_modules/.bin/mw new midway_benchmark_app
+cp start.js ./midway_benchmark_app/start.js
+cp benchmark.js ./midway_benchmark_app/benchmark.js
+echo pwd
 cd midway_benchmark_app
 npm run build
 npm i heapdump
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator midway @midwayjs/mock @midwayjs/bootstrap
-mv ../benchmark.js ./benchmark.js
 node benchmark.js
