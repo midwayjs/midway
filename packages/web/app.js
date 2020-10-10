b541dc0a5437c172d835d215022096bbb2a4889e
Harry Chen <czy88840616@gmail.com>
2020-10-10 23:08:21 +0800

    fix: egg framework support ignore and match (#666)

--- packages/web/app.js
+++ packages/web/app.js
@@ -2,6 +2,7 @@
 
 const { Bootstrap } = require('@midwayjs/bootstrap');
 const { Framework } = require('./dist/index');
+const pathMatching = require('egg-path-matching');
 
 class AppBootHook {
   constructor(app) {
@@ -37,7 +38,23 @@ class AppBootHook {
         this.app.use(mwIns);
       } else {
         // egg
-        this.app.use(this.app.middlewares[name](this.app.config[name]));
+        const options = this.app.config[name];
+        if (options.enable === false) {
+          continue;
+        }
+        // support options.match and options.ignore
+        const mw = this.app.middlewares[name](options);
+        if (!options.match && !options.ignore) {
+          this.app.use(mw);
+        } else {
+          const match = pathMatching(options);
+          const fn = (ctx, next) => {
+            if (!match(ctx)) return next();
+            return mw(ctx, next);
+          };
+          fn._name = mw._name + 'middlewareWrapper';
+          this.app.use(fn);
+        }
       }
     }
   }
