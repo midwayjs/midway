ef46a2478b0a988cc2ae120ad79ba3057d081fbd
Harry Chen <czy88840616@gmail.com>
2020-10-22 18:36:10 +0800

    chore: downgrade node version for faas (#681)

--- packages/faas/jest.config.js
+++ packages/faas/jest.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
 };
--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
   preset: 'ts-jest',
+  testEnvironment: 'node',
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
   setupFilesAfterEnv: [path.join(__dirname, 'test/.setup.js')],
