a863d6eb8461a634b702eb123484820bc785fd54
Harry Chen <czy88840616@gmail.com>
2020-11-06 15:05:33 +0800

    test: add custom jest config

---
+++ packages-serverless/egg-layer/jest.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
+  coveragePathIgnorePatterns: ['<rootDir>/test/'],
+  setupFilesAfterEnv: [path.join(__dirname, 'test/.setup.js')],
+  forceExit: true,
+};
---
+++ packages-serverless/runtime-mock/jest.config.js
@@ -0,0 +1,8 @@
+const path = require('path');
+
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
+  coveragePathIgnorePatterns: ['<rootDir>/test/'],
+};
