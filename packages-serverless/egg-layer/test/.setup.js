dc7317a3b433b44198589dc13f941f1ab0b88dd9
Harry Chen <czy88840616@gmail.com>
2020-12-12 22:06:50 +0800

    fix: change cwd in test

--- packages-serverless/egg-layer/test/.setup.js
+++ packages-serverless/egg-layer/test/.setup.js
@@ -1,3 +1,4 @@
 jest.setTimeout(30000);
 const { join } = require('path');
 process.env.EGG_FRAMEWORK_DIR = join(__dirname, '../../../node_modules/egg');
+process.chdir(join(__dirname, '../../../'));
