354d03c0ecb5700d203053e5943727d3cdd25973
Harry Chen <czy88840616@gmail.com>
2021-01-23 16:36:39 +0800

    fix: empty args

--- packages/web/test/utils.test.ts
+++ packages/web/test/utils.test.ts
@@ -5,6 +5,7 @@ describe('test/utils.test.ts', function () {
   it('should test getCurrentDateString', function () {
     //  2021-01-21 00:30:00
     const format = getCurrentDateString(1611160200000);
-    expect(format).toEqual('2021-01-21');
+    const d = new Date(1611160200000);
+    expect(format).toEqual(`${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${(d.getDate()).toString().padStart(2, '0')}`);
   });
 });
