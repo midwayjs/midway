2014c649f5c692421412a362e8ddbf408646ca17
echosoar <82163514@qq.com>
2021-02-26 21:17:19 +0800

    test: serverless app coverage

--- packages-serverless/serverless-app/jest.config.js
+++ packages-serverless/serverless-app/jest.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  collectCoverage: true,
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
 };
