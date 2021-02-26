b4cf2ae72f7c6dc9191f07950f876006f2d997b9
echosoar <82163514@qq.com>
2021-02-26 21:34:09 +0800

    test: GENERATE_SOURCEMAP=false

--- packages-serverless/serverless-app/jest.config.js
+++ packages-serverless/serverless-app/jest.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  collectCoverage: true,
   testPathIgnorePatterns: ['<rootDir>/test/fixtures'],
   coveragePathIgnorePatterns: ['<rootDir>/test/'],
 };
