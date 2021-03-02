0dfa6f83e9e9fb42d73a4521dcfd05846c5c8643
echosoar <82163514@qq.com>
2021-03-02 15:32:33 +0800

    fix: isTsEnv

--- packages-serverless/serverless-app/src/utils.ts
+++ packages-serverless/serverless-app/src/utils.ts
@@ -74,3 +74,12 @@ export const analysisDecorator = async (cwd: string) => {
   }
   return allFunc;
 };
+
+export function isTypeScriptEnvironment() {
+  const TS_MODE_PROCESS_FLAG: string = process.env.MIDWAY_TS_MODE;
+  if ('false' === TS_MODE_PROCESS_FLAG) {
+    return false;
+  }
+  // eslint-disable-next-line node/no-deprecated-api
+  return TS_MODE_PROCESS_FLAG === 'true' || !!require.extensions['.ts'];
+}
