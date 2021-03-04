a360a0e645a9551cb9d90ceaf7871f3e0ab5b4d3
Harry Chen <czy88840616@gmail.com>
2021-03-04 16:31:07 +0800

    fix: exports missing (#884)

--- packages/mock/src/index.ts
+++ packages/mock/src/index.ts
@@ -1,4 +1,4 @@
-export { create, close, createApp } from './utils';
+export { create, close, createApp, createFunctionApp } from './utils';
 export * from './client/http';
 export * from './client/rabbitMQ';
 
