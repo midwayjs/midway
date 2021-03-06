594ca1c082b2e0c8a74096219d354b5e93a66150
echosoar <82163514@qq.com>
2021-03-06 11:27:21 +0800

    fix: benchmark autocannon

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -5,6 +5,7 @@ npm install @midwayjs/cli autocannon
 cp ./scripts/start.js ./midway_benchmark_app/start.js
 cp ./scripts/benchmark.js ./midway_benchmark_app/benchmark.js
 cd midway_benchmark_app
+npm install autocannon
 npm run build
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator midway @midwayjs/mock @midwayjs/bootstrap
 node benchmark.js
