ea2edfe86f19de983dc634c2e53d750feec906ee
金炳 <1520006273@qq.com>
2021-03-12 08:42:32 +0800

    fix: validator class extends (#896)

--- packages/decorator/test/annotation/check.test.ts
+++ packages/decorator/test/annotation/check.test.ts
@@ -2,19 +2,59 @@ import { Validate, Rule, RuleType } from '../../src';
 import * as assert from 'assert';
 describe('/test/annotation/check.test.ts', () => {
   it('check with check', () => {
-    class UserDTO {
+    class TO{
+
+    }
+
+    @Rule(TO)
+    class UserDTO extends TO{
       @Rule(RuleType.number().max(10))
+      age: number;
+    }
+
+    @Rule(UserDTO)
+    class HelloDTO extends UserDTO{
+    }
+
+    class Hello {
+      @Validate()
+      school(a, data: HelloDTO) {
+        return data;
+      }
+    }
+    const user = {
+      age: 8
+    };
+    const result = new Hello().school(1, user);
+    assert.deepEqual(result, user);
+  });
+
+  it('check with check with extends', () => {
+    class TO{
+
+    }
+
+    @Rule(TO)
+    class UserDTO extends TO{
+      @Rule(RuleType.number().max(10))
+      age: number;
+    }
+
+    @Rule(UserDTO)
+    class HelloDTO extends UserDTO{
+
+      @Rule(RuleType.number().min(4))
       age: number;
     }
 
     class Hello {
       @Validate()
-      school(a, data: UserDTO) {
+      school(a, data: HelloDTO) {
         return data;
       }
     }
     const user = {
-      age: 8,
+      age: 11
     };
     const result = new Hello().school(1, user);
     assert.deepEqual(result, user);
