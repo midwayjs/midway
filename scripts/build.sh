351d5c9eff8fd99b079fcd59d313094e7f5cc056
Harry Chen <czy88840616@gmail.com>
2021-03-14 15:45:22 +0800

    chore: update version script (#914)

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,3 +4,4 @@ set -e
 # cp ./README.md ./packages/midway/README.md
 lerna run build
 lerna exec -- rm -rf ./dist/.mwcc-cache
+echo "console.log('$(date)');" > ./packages/version/index.js
--- scripts/generate_version.js
+++ scripts/generate_version.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
-const { writeFileSync } = require('fs');
+const { writeFileSync, existsSync } = require('fs');
 const { join } = require('path');
 
 const originData = execSync('npx lerna ls --json').toString();
@@ -10,4 +10,28 @@ for (const info of data) {
   result[info.name] = info.version;
 }
 
-writeFileSync(join(__dirname, '../packages/decorator/version.json'), JSON.stringify(result, null, 2));
+const key = result['@midwayjs/decorator'].replace(/\./g, '_') + '-' + result['@midwayjs/core'].replace(/\./g, '_');
+
+const versionFile = join(__dirname, '../packages/version/versions', `${key}.json`);
+
+if (existsSync(versionFile)) {
+  const originData = require(versionFile);
+  for (const pkgName in result) {
+    if (!originData[pkgName]) {
+      originData[pkgName] = result[pkgName];
+    } else if (typeof originData[pkgName] === 'string') {
+      // 去重
+      if (originData[pkgName] !== result[pkgName]) {
+        originData[pkgName] = [originData[pkgName], result[pkgName]];
+      }
+    } else {
+      // array
+      if (!originData[pkgName].includes(result[pkgName])) {
+        originData[pkgName].push(result[pkgName]);
+      }
+    }
+  }
+  writeFileSync(versionFile, JSON.stringify(originData, null, 2));
+} else {
+  writeFileSync(versionFile, JSON.stringify(result, null, 2));
+}
