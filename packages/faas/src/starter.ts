d9bb501edd71e48b70ff2c2fc0d1115a5f674ab9
Harry Chen <czy88840616@gmail.com>
2021-03-23 09:22:44 +0800

    fix:  faas npe

--- packages/faas/src/starter.ts
+++ packages/faas/src/starter.ts
@@ -132,12 +132,12 @@ export class FaaSStarter implements IFaaSStarter {
       return this.webApplication;
     });
     this.loader.registerHook(PLUGIN_KEY, (key, target) => {
-      return target[REQUEST_OBJ_CTX_KEY]?.[key] || this.webApplication[key];
+      return target?.[REQUEST_OBJ_CTX_KEY]?.[key] || this.webApplication[key];
     });
 
     this.loader.registerHook(LOGGER_KEY, (key, target) => {
       return (
-        target[REQUEST_OBJ_CTX_KEY]?.['logger'] ||
+        target?.[REQUEST_OBJ_CTX_KEY]?.['logger'] ||
         this.webApplication.getLogger()
       );
     });
