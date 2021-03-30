25b4302b16431031aa2c20304848db9a14a8babb
Harry Chen <czy88840616@gmail.com>
2021-03-30 11:16:56 +0800

    fix: set error logger key (#953)

--- packages/faas/test/fixtures/base-app-inject-logger/src/index.ts
+++ packages/faas/test/fixtures/base-app-inject-logger/src/index.ts
@@ -1,5 +1,5 @@
 import { FunctionHandler } from '../../../../src';
-import { Func, Provide, App, Inject } from '@midwayjs/decorator';
+import { Func, Provide, App, Inject, Logger } from '@midwayjs/decorator';
 import * as assert from 'assert';
 
 @Provide()
@@ -17,11 +17,23 @@ export class HelloService implements FunctionHandler {
   @Inject()
   ctx;
 
+  @Logger('logger')
+  anotherLogger;
+
+  @Logger('appLogger')
+  anotherAppLogger;
+
+  @Logger()
+  appLogger;
+
   async handler() {
     assert(this.loggerService.getLogger());
     assert(this.loggerService.getLogger() === this.app.getLogger());
     assert(this.logger);
     assert(this.logger === this.ctx.logger);
+    assert(this.logger === this.appLogger);
+    assert(this.logger === this.anotherLogger);
+    assert(this.logger === this.anotherAppLogger);
     return 'hello world';
   }
 }
