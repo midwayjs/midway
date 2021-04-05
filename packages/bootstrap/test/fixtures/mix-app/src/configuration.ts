e734a5035e876c20d56f09adcd4b7ed1f736c16e
Harry Chen <czy88840616@gmail.com>
2021-04-05 22:38:01 +0800

    test: add singleton case

--- packages/bootstrap/test/fixtures/mix-app/src/configuration.ts
+++ packages/bootstrap/test/fixtures/mix-app/src/configuration.ts
@@ -1,6 +1,7 @@
-import { Configuration, Config, ALL } from '@midwayjs/decorator';
+import { Configuration, Config, ALL, Inject } from '@midwayjs/decorator';
 import { join } from 'path';
 import * as assert from 'assert';
+import { RemoteConfigService } from './service/remoteConfigService';
 
 @Configuration({
   importConfigs: [
@@ -11,8 +12,12 @@ export class AutoConfiguration {
   @Config(ALL)
   prepareConfig;
 
+  @Inject()
+  configService: RemoteConfigService;
+
   async onReady() {
     console.log('ready');
     assert(this.prepareConfig['prepare'] === 'remote data');
+    assert(this.prepareConfig['id'] === this.configService.innerData);
   }
 }
--- packages/bootstrap/test/fixtures/mix-app/src/service/remoteConfigService.ts
+++ packages/bootstrap/test/fixtures/mix-app/src/service/remoteConfigService.ts
@@ -2,10 +2,14 @@ import { Provide, sleep } from '@midwayjs/decorator';
 
 @Provide()
 export class RemoteConfigService {
+
+  innerData = Math.random();
+
   async getRemoteConfig() {
     await sleep(1000);
     return {
       prepare: 'remote data',
+      id: this.innerData,
     }
   }
 }
