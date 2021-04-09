d18244fa669fc1f353ca16ad9988571381fd74dc
Harry Chen <czy88840616@gmail.com>
2021-04-09 23:01:25 +0800

    chore: use concurrency

--- packages/midway/test/index.test.ts
+++ packages/midway/test/index.test.ts
@@ -1,16 +1,14 @@
-import { expect } from 'chai';
 import * as midway from '../src';
-
 import * as path from 'path';
 const _ROOT = path.join(__dirname, '../');
 
 describe('/test/index.test.ts', () => {
   it('should get framework version', function () {
-    expect(midway.VERSION).to.equal(require(_ROOT + '/package.json').version);
+    expect(midway.VERSION).toEqual(require(_ROOT + '/package.json').version);
   });
 
   it('should get framework release name', function () {
-    expect(midway.RELEASE).to.exist;
+    expect(midway.RELEASE).toBeDefined();
   });
 
 });
