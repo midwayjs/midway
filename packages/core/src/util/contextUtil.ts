3950f734b4f944d188773d8d901e8b119f52046e
Harry Chen <czy88840616@gmail.com>
2021-04-12 11:14:02 +0800

    fix: lint

--- packages/core/src/util/contextUtil.ts
+++ packages/core/src/util/contextUtil.ts
@@ -1,13 +1,21 @@
-import { IConfigurationOptions, IMidwayApplication, IMidwayContainer, IMidwayFramework } from '../interface';
+import {
+  IConfigurationOptions,
+  IMidwayApplication,
+  IMidwayContainer,
+  IMidwayFramework,
+} from '../interface';
 
 export const getCurrentApplicationContext = (): IMidwayContainer => {
   return getCurrentMainFramework().getApplicationContext();
-}
+};
 
-export const getCurrentMainFramework = <APP extends IMidwayApplication, T extends IConfigurationOptions>(): IMidwayFramework<APP, T> => {
+export const getCurrentMainFramework = <
+  APP extends IMidwayApplication,
+  T extends IConfigurationOptions
+>(): IMidwayFramework<APP, T> => {
   return global['MIDWAY_MAIN_FRAMEWORK'] as IMidwayFramework<APP, T>;
-}
+};
 
 export const getCurrentMainApp = <APP extends IMidwayApplication>(): APP => {
   return getCurrentMainFramework().getApplication() as APP;
-}
+};
