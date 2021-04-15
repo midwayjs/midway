ebc3bcdc5e70cde95e53be64bbead481c91fbd91
Harry Chen <czy88840616@gmail.com>
2021-04-15 20:35:22 +0800

    fix: case

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,5 +4,3 @@ set -e
 # cp ./README.md ./packages/midway/README.md
 lerna run build --concurrency=4
 lerna exec -- rm -rf ./dist/.mwcc-cache
-rm -rf ./node_modules/@midwayjs/decorator
-rm -rf ./node_modules/@midwayjs/core
