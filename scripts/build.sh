6f9e6b6976c2060520e0fcd63f2d1f0788ef1cbb
Harry Chen <czy88840616@gmail.com>
2021-04-15 20:20:50 +0800

    refactor: use same decorator

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # cp ./README.md ./packages/midway/README.md
-lerna run build
+lerna run build --concurrency=4
 lerna exec -- rm -rf ./dist/.mwcc-cache
 rm -rf ./node_modules/@midwayjs/decorator
 rm -rf ./node_modules/@midwayjs/core
