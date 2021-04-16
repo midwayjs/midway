81bcfc23524c7bc54e2e9e588807d51fcccdafb9
Harry Chen <czy88840616@gmail.com>
2021-04-17 00:57:01 +0800

    test: add skip

--- packages/logger/test/.setup.js
+++
@@ -1,1 +0,0 @@
-jest.setTimeout(3000000);
--- packages/midway-schedule/jest.setup.js
+++ packages/midway-schedule/jest.setup.js
@@ -1,2 +1,4 @@
+const path = require('path');
 process.env.MIDWAY_TS_MODE = 'true';
+process.env.MIDWAY_EGG_PLUGIN_PATH = path.join(__dirname, '../../');
 jest.setTimeout(30000);
