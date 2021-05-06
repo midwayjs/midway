6b623ed3e9577c9cb89b57d9580478a8b03e2d0a
Harry Chen <czy88840616@gmail.com>
2021-05-06 23:16:26 +0800

    fix: remove prepub

--- packages-serverless/serverless-scf-starter/src/runtime.ts
+++ packages-serverless/serverless-scf-starter/src/runtime.ts
@@ -9,10 +9,10 @@ import {
 const isOutputError = () => {
   return (
     process.env.SERVERLESS_OUTPUT_ERROR_STACK === 'true' ||
-    ['local', 'development', 'prepub'].includes(
+    ['local', 'development'].includes(
       process.env.MIDWAY_SERVER_ENV
     ) ||
-    ['local', 'development', 'prepub'].includes(process.env.NODE_ENV)
+    ['local', 'development'].includes(process.env.NODE_ENV)
   );
 };
 
