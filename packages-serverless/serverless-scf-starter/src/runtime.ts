b162b897812d1a1a5e981328fbbb43aa75eacf10
Harry Chen <czy88840616@gmail.com>
2021-05-07 11:13:28 +0800

    fix: output serverless error in some environment (#1030)

--- packages-serverless/serverless-scf-starter/src/runtime.ts
+++ packages-serverless/serverless-scf-starter/src/runtime.ts
@@ -6,10 +6,13 @@ import {
   HTTPResponse,
 } from '@midwayjs/serverless-http-parser';
 
-const isLocalEnv = () => {
+const isOutputError = () => {
   return (
-    process.env.MIDWAY_SERVER_ENV === 'local' ||
-    process.env.NODE_ENV === 'local'
+    process.env.SERVERLESS_OUTPUT_ERROR_STACK === 'true' ||
+    ['local', 'development'].includes(
+      process.env.MIDWAY_SERVER_ENV
+    ) ||
+    ['local', 'development'].includes(process.env.NODE_ENV)
   );
 };
 
@@ -114,7 +117,7 @@ export class SCFRuntime extends ServerlessLightRuntime {
               isBase64Encoded: false,
               statusCode: 500,
               headers: {},
-              body: isLocalEnv() ? err.stack : 'Internal Server Error',
+              body: isOutputError() ? err.stack : 'Internal Server Error',
             };
           });
       },
