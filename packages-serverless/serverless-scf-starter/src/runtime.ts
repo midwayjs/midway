0c6823673a6b4263a17b12c389e5f3221de64a6c
Harry Chen <czy88840616@gmail.com>
2021-05-08 18:07:20 +0800

    fix: lint

--- packages-serverless/serverless-scf-starter/src/runtime.ts
+++ packages-serverless/serverless-scf-starter/src/runtime.ts
@@ -9,9 +9,7 @@ import {
 const isOutputError = () => {
   return (
     process.env.SERVERLESS_OUTPUT_ERROR_STACK === 'true' ||
-    ['local', 'development'].includes(
-      process.env.MIDWAY_SERVER_ENV
-    ) ||
+    ['local', 'development'].includes(process.env.MIDWAY_SERVER_ENV) ||
     ['local', 'development'].includes(process.env.NODE_ENV)
   );
 };
