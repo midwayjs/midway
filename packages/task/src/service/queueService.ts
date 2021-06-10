2bfc95731871f0db12cfbdbc28140089e9f9a70c
金炳 <1520006273@qq.com>
2021-06-10 08:33:11 +0800

    chore: fix dev env remove old task (#1090)

--- packages/task/src/service/queueService.ts
+++ packages/task/src/service/queueService.ts
@@ -6,19 +6,26 @@ export class QueueService {
   @Inject('queueMap')
   queueMap;
 
+  @Inject('queueTaskMap')
+  queueTaskMap;
+
   /**
    * @deprecated please use execute method
    */
-  async excute(queueName: any, data: any, options: JobOptions) {
+  async excute(queueName: any, data: any, options: JobOptions = {}) {
     return this.execute(queueName, data, options);
   }
 
-  async execute(queueName: any, data: any, options: JobOptions) {
+  async execute(queueName: any, data: any, options: JobOptions = {}) {
     const queue = this.queueMap[`${queueName.name}:execute`] as Queue;
     return await queue.add(data, options);
   }
 
   getClassQueue(queueName: any): Queue {
     return this.queueMap[`${queueName.name}:execute`] as Queue;
+  }
+
+  getQueueTask(queueClass: string, queueName: string): Queue {
+    return this.queueTaskMap[`${queueClass}:${queueName}`] as Queue;
   }
 }
