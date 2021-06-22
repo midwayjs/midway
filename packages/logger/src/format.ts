f3dcb294fe71031df7f10f6f4c8fd431d87b288d
Harry Chen <czy88840616@gmail.com>
2021-06-22 14:50:08 +0800

    fix: lint

--- packages/logger/src/format.ts
+++ packages/logger/src/format.ts
@@ -80,7 +80,8 @@ export const displayCommonMessage = format(
         return el instanceof Error;
       });
       if (err) {
-        info.message = info.message.replace(err.message, '').trimEnd() + ' ' + err.stack;
+        info.message =
+          info.message.replace(err.message, '').trimEnd() + ' ' + err.stack;
         info[MESSAGE] = info[MESSAGE] || info.message + err.stack;
         info.originError = err;
         info.stack = err.stack;
