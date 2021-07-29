4ccce29615dd7ab4d4bc04465c68dc6a46f34597
tkvern <tkvern@qq.com>
2021-07-29 16:10:11 +0800

    style: prettier code

--- packages/web-express/src/framework.ts
+++ packages/web-express/src/framework.ts
@@ -92,9 +92,13 @@ export class MidwayExpressFramework extends BaseFramework<
   public async run(): Promise<void> {
     if (this.configurationOptions.port) {
       new Promise<void>(resolve => {
-        this.server.listen(this.configurationOptions.port, this.configurationOptions.hostname || '127.0.0.1', () => {
-          resolve();
-        });
+        this.server.listen(
+          this.configurationOptions.port,
+          this.configurationOptions.hostname || '127.0.0.1',
+          () => {
+            resolve();
+          }
+        );
       });
     }
   }
