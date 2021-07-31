2fe8a387ebb2fc0962d54f17d95084b24f07afc3
Harry Chen <czy88840616@gmail.com>
2021-07-31 16:21:33 +0800

    fix: server hostname args

--- packages/web-koa/src/framework.ts
+++ packages/web-koa/src/framework.ts
@@ -282,13 +282,14 @@ export class MidwayKoaFramework extends MidwayKoaBaseFramework<
   public async run(): Promise<void> {
     if (this.configurationOptions.port) {
       new Promise<void>(resolve => {
-        this.server.listen(
-          this.configurationOptions.port,
-          this.configurationOptions.hostname || '127.0.0.1',
-          () => {
-            resolve();
-          }
-        );
+        const args: any[] = [this.configurationOptions.port];
+        if (this.configurationOptions.hostname) {
+          args.push(this.configurationOptions.hostname);
+        }
+        args.push(() => {
+          resolve();
+        });
+        this.server.listen(...args);
       });
     }
   }
