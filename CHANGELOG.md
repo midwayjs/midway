d25eac8937c50c021e116b2a165861c7bc8cda17
Harry Chen <czy88840616@gmail.com>
2021-08-18 20:05:42 +0800

    v2.12.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,17 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
+## [2.12.6](https://github.com/midwayjs/midway/compare/v2.12.5...v2.12.6) (2021-08-18)
+
+
+### Bug Fixes
+
+* find egg plugin in cwd ([#1236](https://github.com/midwayjs/midway/issues/1236)) ([ac9a79f](https://github.com/midwayjs/midway/commit/ac9a79ffa90f3f9d7984b24084aa7ac9a307fc98))
+
+
+
+
+
 ## [2.12.5](https://github.com/midwayjs/midway/compare/v2.12.4...v2.12.5) (2021-08-18)
 
 
---
+++ scripts/rollback/rollback-2.12.6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# timestamp: 1629288253462
+
+npm dist-tag add @midwayjs/egg-layer@2.12.5 latest
+npm dist-tag add @midwayjs/egg-layer@2.12.6 beta
+tnpm dist-tag add @midwayjs/egg-layer@2.12.5 latest
+tnpm dist-tag add @midwayjs/egg-layer@2.12.6 latest
+npm dist-tag add midway@2.12.5 latest
+npm dist-tag add midway@2.12.6 beta
+tnpm dist-tag add midway@2.12.5 latest
+tnpm dist-tag add midway@2.12.6 latest
+npm dist-tag add @midwayjs/version@2.12.5 latest
+npm dist-tag add @midwayjs/version@2.12.6 beta
+tnpm dist-tag add @midwayjs/version@2.12.5 latest
+tnpm dist-tag add @midwayjs/version@2.12.6 latest
+npm dist-tag add @midwayjs/web@2.12.5 latest
+npm dist-tag add @midwayjs/web@2.12.6 beta
+tnpm dist-tag add @midwayjs/web@2.12.5 latest
+tnpm dist-tag add @midwayjs/web@2.12.6 latest
+
+# Changes:
+
+#  - @midwayjs/egg-layer: 2.12.5 => 2.12.6
+#  - midway: 2.12.5 => 2.12.6
+#  - @midwayjs/version: 2.12.5 => 2.12.6
+#  - @midwayjs/web: 2.12.5 => 2.12.6
