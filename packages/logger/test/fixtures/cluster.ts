5ff94a67b1b936e83857376f6a07e084182b7870
Harry Chen <czy88840616@gmail.com>
2021-09-04 23:11:51 +0800

    fix: node v16 typings

--- packages/logger/test/fixtures/cluster.ts
+++ packages/logger/test/fixtures/cluster.ts
@@ -2,18 +2,18 @@ import { MidwayBaseLogger } from '../../src';
 import { join } from 'path';
 import * as cluster from 'cluster';
 
-if (cluster.isMaster) {
+if (cluster['isMaster']) {
   console.log(`Master ${process.pid} is running`);
   const pidList = [];
 
   // Fork workers.
   for (let i = 0; i < 4; i++) {
-    const cp = cluster.fork();
+    const cp = (cluster as any).fork();
     pidList.push(cp.process.pid);
   }
   process.send(pidList);
 
-  cluster.on('exit', (worker, code, signal) => {
+  (cluster as any).on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
