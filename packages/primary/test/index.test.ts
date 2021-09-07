dc93febee7df46a012ebf10259eabbe6e2daed32
jinbing.jb <jinbing.jb@alibaba-inc.com>
2021-09-07 16:43:19 +0800

    chore: test error

--- packages/primary/test/index.test.ts
+++ packages/primary/test/index.test.ts
@@ -1,7 +1,6 @@
 import { createApp, close } from '@midwayjs/mock';
 import { Framework } from '@midwayjs/koa';
 import { join } from 'path';
-import * as assert from 'power-assert';
 
 describe('/test/index.test.ts', () => {
 
@@ -13,11 +12,10 @@ describe('/test/index.test.ts', () => {
   afterAll(async () => {
     await close(app);
   })
-  it('should get metrics', async (done) => {
+  it('should get metrics', async () => {
     let userService = await app.getApplicationContext().getAsync('userService');
     let res = await userService.getUser();
-    assert.equal(res, 3);
-    done();
+    expect(res).toBe(3);
   });
 
 });
