4a79ad2ab58db79b3489bb43fd8ea7b8331d9c4c
Harry Chen <czy88840616@gmail.com>
2021-10-21 14:31:34 +0800

    fix: remove connection key

--- packages/typegoose/src/decorator/injectEntityModel.ts
+++ packages/typegoose/src/decorator/injectEntityModel.ts
@@ -1,14 +1,13 @@
 import { attachClassMetadata } from '@midwayjs/decorator';
 import { ENTITY_MODEL_KEY } from '../interface';
 
-export function InjectEntityModel(modelKey: any, connectionName = 'default') {
+export function InjectEntityModel(modelKey: any) {
   return (target, propertyKey: string) => {
     attachClassMetadata(
       ENTITY_MODEL_KEY,
       {
         key: {
           modelKey,
-          connectionName,
         },
         propertyName: propertyKey,
       },
