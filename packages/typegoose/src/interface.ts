d3e9bf60edcddc64da61597bc4be971da5dc3a25
Harry Chen <czy88840616@gmail.com>
2021-10-21 12:54:19 +0800

    chore: upgrade mongoose to new version (#1336)

--- packages/typegoose/src/interface.ts
+++ packages/typegoose/src/interface.ts
@@ -1,5 +1,23 @@
+import * as mongoose from 'mongoose';
+
 export interface EntityOptions {
   connectionName: string;
 }
 
 export const ENTITY_MODEL_KEY  = 'TYPEGOOSE:MODEL';
+
+interface M5 {
+  ConnectionOptions: unknown;
+}
+interface M6 {
+  ConnectOptions: unknown;
+}
+
+// 不同的 mongoose 版本 options 字段定义不同
+type ExtractConnectionOptions<T> = T extends M5 ? T['ConnectionOptions']: T extends M6 ? T['ConnectOptions']: never;
+type ConnectionOptions = ExtractConnectionOptions<typeof mongoose>;
+
+export type DefaultConfig = {
+  uri: string;
+  options: ConnectionOptions
+};
