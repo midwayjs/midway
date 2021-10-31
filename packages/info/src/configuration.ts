1d3c7cba255280003c98fb00a35dd76c845754c4
Gao Yang <echosoar@qq.com>
2021-10-31 16:28:28 +0800

    feat: component info (#1347)

---
+++ packages/info/src/configuration.ts
@@ -0,0 +1,8 @@
+import { Configuration } from '@midwayjs/decorator';
+import { join } from 'path';
+
+@Configuration({
+  namespace: 'info',
+  importConfigs: [join(__dirname, './config.default')],
+})
+export class AutoConfiguration {}
