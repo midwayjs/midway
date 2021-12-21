653c7248a3aa501debe582b506b8a696b664ac0a
Harry Chen <czy88840616@gmail.com>
2021-12-21 16:14:01 +0800

    doc: update

--- packages/passport/index.d.ts
+++ packages/passport/index.d.ts
@@ -10,9 +10,48 @@ declare module '@midwayjs/core/dist/interface' {
 
 declare module '@midwayjs/koa/dist/interface' {
   interface Context {
+    state: {
+      user?: any;
+    };
     isAuthenticated(): boolean;
     isUnauthenticated(): boolean;
     login(): Promise<void>;
     logout(): void;
+  }
+}
+
+declare module '@midwayjs/web/dist/interface' {
+  interface Context {
+    state: {
+      user?: any;
+    };
+    isAuthenticated(): boolean;
+    isUnauthenticated(): boolean;
+    login(): Promise<void>;
+    logout(): void;
+  }
+}
+
+declare module '@midwayjs/faas/dist/interface' {
+  interface Context {
+    state: {
+      user?: any;
+    };
+    isAuthenticated(): boolean;
+    isUnauthenticated(): boolean;
+    login(): Promise<void>;
+    logout(): void;
+  }
+}
+
+declare module '@midwayjs/express/dist/interface' {
+  interface Context {
+    user?: any;
+    // These declarations are merged into express's Request type
+    login(user: any, done: (err: any) => void): void;
+    login(user: any, options: any, done: (err: any) => void): void;
+    logout(): void;
+    isAuthenticated(): boolean;
+    isUnauthenticated(): boolean;
   }
 }
