52e49b2fa4c833b525eeaba13756470cbb414ccc
echosoar <82163514@qq.com>
2021-12-30 19:25:20 +0800

    test: upload

--- packages/upload/test/faas.test.ts
+++ packages/upload/test/faas.test.ts
@@ -5,11 +5,18 @@ import * as ServerlessApp from '../../../packages-serverless/serverless-app/src'
 import { existsSync, statSync } from 'fs';
 
 describe('test/faas.test.ts', function () {
+  let app;
+  beforeAll(async () => {
+    const appDir = join(__dirname, 'fixtures/faas');
+    app = await createFunctionApp<ServerlessApp.Framework>(appDir, {}, ServerlessApp);
+  })
+
+  afterAll(async () => {
+    await close(app);
+  });
 
   it('upload file mode', async () => {
-    const appDir = join(__dirname, 'fixtures/faas');
     const imagePath = join(__dirname, 'fixtures/1.jpg');
-    const app = await createFunctionApp<ServerlessApp.Framework>(appDir, {}, ServerlessApp);
     const request = await createHttpRequest(app);
     const stat = statSync(imagePath);
     await request.post('/upload')
@@ -23,12 +30,10 @@ describe('test/faas.test.ts', function () {
         const file1Stat = statSync(response.body.files[0].data);
         assert(file1Stat.size && file1Stat.size === stat.size);
       });
-    await close(app);
+    
   });
   it('upload file mode and auto clean', async () => {
-    const appDir = join(__dirname, 'fixtures/faas');
     const imagePath = join(__dirname, 'fixtures/1.jpg');
-    const app = await createFunctionApp<ServerlessApp.Framework>(appDir, {}, ServerlessApp);
     const request = await createHttpRequest(app);
     await request.post('/uploadAutoClean')
       .field('name', 'form')
@@ -40,18 +45,13 @@ describe('test/faas.test.ts', function () {
         assert(response.body.fields.name === 'form');
         assert(!existsSync(response.body.files[0].data));
       });
-    await close(app);
   });
   it('upload unsupport ext file using file', async () => {
-
-    const appDir = join(__dirname, 'fixtures/faas');
     const testPath = join(__dirname, 'fixtures/1.test');
-    const app = await createFunctionApp<ServerlessApp.Framework>(appDir, {}, ServerlessApp);
     const request = await createHttpRequest(app);
     await request.post('/upload')
       .field('name', 'form')
       .attach('file', testPath)
       .expect(400);
-    await close(app);
   });
 });
