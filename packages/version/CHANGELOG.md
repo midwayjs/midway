d7ca169aec933e24070e494e530419781524369e
Harry Chen <czy88840616@gmail.com>
2022-01-12 16:14:37 +0800

    v2.14.3

--- packages/version/CHANGELOG.md
+++ packages/version/CHANGELOG.md
@@ -3,6 +3,14 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
+## [2.14.3](https://github.com/midwayjs/midway/compare/v2.14.2...v2.14.3) (2022-01-12)
+
+**Note:** Version bump only for package @midwayjs/version
+
+
+
+
+
 ## [2.14.2](https://github.com/midwayjs/midway/compare/v2.14.1...v2.14.2) (2021-12-28)
 
 **Note:** Version bump only for package @midwayjs/version
---
+++ scripts/rollback/rollback-2.14.3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# timestamp: 1641975167494
+
+npm dist-tag add @midwayjs/logger@2.14.0 latest
+npm dist-tag add @midwayjs/logger@2.13.0 beta
+npm dist-tag add @midwayjs/process-agent@2.14.2 latest
+npm dist-tag add @midwayjs/process-agent@2.14.3 beta
+npm dist-tag add @midwayjs/sequelize@2.14.2 latest
+npm dist-tag add @midwayjs/sequelize@2.14.3 beta
+npm dist-tag add @midwayjs/task@2.14.2 latest
+npm dist-tag add @midwayjs/task@2.14.3 beta
+npm dist-tag add @midwayjs/version@2.14.2 latest
+npm dist-tag add @midwayjs/version@2.14.3 beta
+tnpm dist-tag add @midwayjs/logger@2.14.0 latest
+tnpm dist-tag add @midwayjs/logger@2.13.0 beta
+tnpm dist-tag add @midwayjs/process-agent@2.14.2 latest
+tnpm dist-tag add @midwayjs/process-agent@2.14.3 beta
+tnpm dist-tag add @midwayjs/sequelize@2.14.2 latest
+tnpm dist-tag add @midwayjs/sequelize@2.14.3 beta
+tnpm dist-tag add @midwayjs/task@2.14.2 latest
+tnpm dist-tag add @midwayjs/task@2.14.3 beta
+tnpm dist-tag add @midwayjs/version@2.14.2 latest
+tnpm dist-tag add @midwayjs/version@2.14.3 beta
+
+# Changes:
+
+#  - @midwayjs/logger: 2.14.0 => 2.14.3
+#  - @midwayjs/process-agent: 2.14.2 => 2.14.3
+#  - @midwayjs/sequelize: 2.14.2 => 2.14.3
+#  - @midwayjs/task: 2.14.2 => 2.14.3
+#  - @midwayjs/version: 2.14.2 => 2.14.3
