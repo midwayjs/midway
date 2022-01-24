ca77247d229978a736e79bb208579c014ed226fc
Harry Chen <czy88840616@gmail.com>
2022-01-24 21:08:02 +0800

    v3.0.2

--- packages-serverless/serverless-vercel-starter/CHANGELOG.md
+++ packages-serverless/serverless-vercel-starter/CHANGELOG.md
@@ -3,6 +3,17 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
+## [3.0.2](https://github.com/midwayjs/midway/compare/v3.0.1...v3.0.2) (2022-01-24)
+
+
+### Bug Fixes
+
+* singleton invoke request scope not valid ([#1622](https://github.com/midwayjs/midway/issues/1622)) ([f97c063](https://github.com/midwayjs/midway/commit/f97c0632107b47cf357d17774a4e4bb5233bba57))
+
+
+
+
+
 ## [3.0.1](https://github.com/midwayjs/midway/compare/v3.0.0...v3.0.1) (2022-01-24)
 
 **Note:** Version bump only for package @midwayjs/serverless-vercel-starter
---
+++ scripts/rollback/rollback-3.0.2.sh
@@ -0,0 +1,301 @@
+#!/bin/bash
+# timestamp: 1643029517208
+
+npm dist-tag add @midwayjs/egg-layer@3.0.1 latest
+npm dist-tag add @midwayjs/egg-layer@3.0.2 beta
+npm dist-tag add @midwayjs/express-layer@3.0.1 latest
+npm dist-tag add @midwayjs/express-layer@3.0.2 beta
+npm dist-tag add @midwayjs/koa-layer@3.0.1 latest
+npm dist-tag add @midwayjs/koa-layer@3.0.2 beta
+npm dist-tag add @midwayjs/runtime-engine@3.0.0 latest
+npm dist-tag add @midwayjs/runtime-engine@3.0.2 beta
+npm dist-tag add @midwayjs/runtime-mock@3.0.0 latest
+npm dist-tag add @midwayjs/runtime-mock@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-app@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-app@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-aws-starter@3.0.0 latest
+npm dist-tag add @midwayjs/serverless-aws-starter@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-fc-starter@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-fc-starter@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-fc-trigger@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-fc-trigger@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-http-parser@3.0.0 latest
+npm dist-tag add @midwayjs/serverless-http-parser@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-scf-starter@3.0.0 latest
+npm dist-tag add @midwayjs/serverless-scf-starter@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-scf-trigger@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-scf-trigger@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-vercel-starter@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-vercel-starter@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-vercel-trigger@3.0.0 latest
+npm dist-tag add @midwayjs/serverless-vercel-trigger@3.0.2 beta
+npm dist-tag add @midwayjs/serverless-worker-starter@3.0.1 latest
+npm dist-tag add @midwayjs/serverless-worker-starter@3.0.2 beta
+npm dist-tag add @midwayjs/static-layer@3.0.1 latest
+npm dist-tag add @midwayjs/static-layer@3.0.2 beta
+npm dist-tag add @midwayjs/axios@2.14.0 latest
+npm dist-tag add @midwayjs/axios@3.0.2 beta
+npm dist-tag add @midwayjs/bootstrap@2.14.0 latest
+npm dist-tag add @midwayjs/bootstrap@3.0.2 beta
+npm dist-tag add @midwayjs/cache@2.14.0 latest
+npm dist-tag add @midwayjs/cache@3.0.2 beta
+npm dist-tag add @midwayjs/consul@2.14.2 latest
+npm dist-tag add @midwayjs/consul@3.0.2 beta
+npm dist-tag add @midwayjs/core@2.14.0 latest
+npm dist-tag add @midwayjs/core@3.0.2 beta
+npm dist-tag add @midwayjs/cos@2.14.0 latest
+npm dist-tag add @midwayjs/cos@3.0.2 beta
+npm dist-tag add @midwayjs/cross-domain@3.0.1 latest
+npm dist-tag add @midwayjs/cross-domain@3.0.2 beta
+npm dist-tag add @midwayjs/decorator@2.14.0 latest
+npm dist-tag add @midwayjs/decorator@3.0.2 beta
+npm dist-tag add @midwayjs/express-session@3.0.1 latest
+npm dist-tag add @midwayjs/express-session@3.0.2 beta
+npm dist-tag add @midwayjs/faas@2.14.4 latest
+npm dist-tag add @midwayjs/faas@3.0.2 beta
+npm dist-tag add @midwayjs/grpc@2.14.0 latest
+npm dist-tag add @midwayjs/grpc@3.0.2 beta
+npm dist-tag add @midwayjs/i18n@3.0.1 latest
+npm dist-tag add @midwayjs/i18n@3.0.2 beta
+npm dist-tag add @midwayjs/info@3.0.1 latest
+npm dist-tag add @midwayjs/info@3.0.2 beta
+npm dist-tag add @midwayjs/jwt@2.14.2 latest
+npm dist-tag add @midwayjs/jwt@3.0.2 beta
+npm dist-tag add @midwayjs/mock@2.14.0 latest
+npm dist-tag add @midwayjs/mock@3.0.2 beta
+npm dist-tag add @midwayjs/mongoose@2.14.2 latest
+npm dist-tag add @midwayjs/mongoose@3.0.2 beta
+npm dist-tag add @midwayjs/orm@2.14.0 latest
+npm dist-tag add @midwayjs/orm@3.0.2 beta
+npm dist-tag add @midwayjs/oss@2.14.0 latest
+npm dist-tag add @midwayjs/oss@3.0.2 beta
+npm dist-tag add @midwayjs/passport@2.14.4 latest
+npm dist-tag add @midwayjs/passport@3.0.2 beta
+npm dist-tag add @midwayjs/process-agent@2.14.3 latest
+npm dist-tag add @midwayjs/process-agent@3.0.2 beta
+npm dist-tag add @midwayjs/prometheus-socket-io@2.14.2 latest
+npm dist-tag add @midwayjs/prometheus-socket-io@3.0.2 beta
+npm dist-tag add @midwayjs/prometheus@2.14.2 latest
+npm dist-tag add @midwayjs/prometheus@3.0.2 beta
+npm dist-tag add @midwayjs/rabbitmq@2.14.0 latest
+npm dist-tag add @midwayjs/rabbitmq@3.0.2 beta
+npm dist-tag add @midwayjs/redis@2.14.0 latest
+npm dist-tag add @midwayjs/redis@3.0.2 beta
+npm dist-tag add @midwayjs/security@3.0.1 latest
+npm dist-tag add @midwayjs/security@3.0.2 beta
+npm dist-tag add @midwayjs/sequelize@2.14.3 latest
+npm dist-tag add @midwayjs/sequelize@3.0.2 beta
+npm dist-tag add @midwayjs/session@3.0.1 latest
+npm dist-tag add @midwayjs/session@3.0.2 beta
+npm dist-tag add @midwayjs/socketio@2.14.0 latest
+npm dist-tag add @midwayjs/socketio@3.0.2 beta
+npm dist-tag add @midwayjs/static-file@3.0.1 latest
+npm dist-tag add @midwayjs/static-file@3.0.2 beta
+npm dist-tag add @midwayjs/swagger@3.0.1 latest
+npm dist-tag add @midwayjs/swagger@3.0.2 beta
+npm dist-tag add @midwayjs/tablestore@2.14.0 latest
+npm dist-tag add @midwayjs/tablestore@3.0.2 beta
+npm dist-tag add @midwayjs/task@2.14.3 latest
+npm dist-tag add @midwayjs/task@3.0.2 beta
+npm dist-tag add @midwayjs/typegoose@2.14.2 latest
+npm dist-tag add @midwayjs/typegoose@3.0.2 beta
+npm dist-tag add @midwayjs/upload@3.0.1 latest
+npm dist-tag add @midwayjs/upload@3.0.2 beta
+npm dist-tag add @midwayjs/validate@3.0.1 latest
+npm dist-tag add @midwayjs/validate@3.0.2 beta
+npm dist-tag add @midwayjs/version@2.14.5 latest
+npm dist-tag add @midwayjs/version@3.0.2 beta
+npm dist-tag add @midwayjs/view-ejs@2.14.2 latest
+npm dist-tag add @midwayjs/view-ejs@3.0.2 beta
+npm dist-tag add @midwayjs/view-nunjucks@2.14.2 latest
+npm dist-tag add @midwayjs/view-nunjucks@3.0.2 beta
+npm dist-tag add @midwayjs/view@2.14.2 latest
+npm dist-tag add @midwayjs/view@3.0.2 beta
+npm dist-tag add @midwayjs/express@2.14.2 latest
+npm dist-tag add @midwayjs/express@3.0.2 beta
+npm dist-tag add @midwayjs/koa@2.14.2 latest
+npm dist-tag add @midwayjs/koa@3.0.2 beta
+npm dist-tag add @midwayjs/web@2.14.2 latest
+npm dist-tag add @midwayjs/web@3.0.2 beta
+npm dist-tag add @midwayjs/ws@2.14.0 latest
+npm dist-tag add @midwayjs/ws@3.0.2 beta
+tnpm dist-tag add @midwayjs/egg-layer@3.0.1 latest
+tnpm dist-tag add @midwayjs/egg-layer@3.0.2 beta
+tnpm dist-tag add @midwayjs/express-layer@3.0.1 latest
+tnpm dist-tag add @midwayjs/express-layer@3.0.2 beta
+tnpm dist-tag add @midwayjs/koa-layer@3.0.1 latest
+tnpm dist-tag add @midwayjs/koa-layer@3.0.2 beta
+tnpm dist-tag add @midwayjs/runtime-engine@3.0.0 latest
+tnpm dist-tag add @midwayjs/runtime-engine@3.0.2 beta
+tnpm dist-tag add @midwayjs/runtime-mock@3.0.0 latest
+tnpm dist-tag add @midwayjs/runtime-mock@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-app@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-app@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-aws-starter@3.0.0 latest
+tnpm dist-tag add @midwayjs/serverless-aws-starter@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-fc-starter@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-fc-starter@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-fc-trigger@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-fc-trigger@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-http-parser@3.0.0 latest
+tnpm dist-tag add @midwayjs/serverless-http-parser@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-scf-starter@3.0.0 latest
+tnpm dist-tag add @midwayjs/serverless-scf-starter@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-scf-trigger@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-scf-trigger@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-vercel-starter@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-vercel-starter@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-vercel-trigger@3.0.0 latest
+tnpm dist-tag add @midwayjs/serverless-vercel-trigger@3.0.2 beta
+tnpm dist-tag add @midwayjs/serverless-worker-starter@3.0.1 latest
+tnpm dist-tag add @midwayjs/serverless-worker-starter@3.0.2 beta
+tnpm dist-tag add @midwayjs/static-layer@3.0.1 latest
+tnpm dist-tag add @midwayjs/static-layer@3.0.2 beta
+tnpm dist-tag add @midwayjs/axios@2.14.0 latest
+tnpm dist-tag add @midwayjs/axios@3.0.2 beta
+tnpm dist-tag add @midwayjs/bootstrap@2.14.0 latest
+tnpm dist-tag add @midwayjs/bootstrap@3.0.2 beta
+tnpm dist-tag add @midwayjs/cache@2.14.0 latest
+tnpm dist-tag add @midwayjs/cache@3.0.2 beta
+tnpm dist-tag add @midwayjs/consul@2.14.2 latest
+tnpm dist-tag add @midwayjs/consul@3.0.2 beta
+tnpm dist-tag add @midwayjs/core@2.14.0 latest
+tnpm dist-tag add @midwayjs/core@3.0.2 beta
+tnpm dist-tag add @midwayjs/cos@2.14.0 latest
+tnpm dist-tag add @midwayjs/cos@3.0.2 beta
+tnpm dist-tag add @midwayjs/cross-domain@3.0.1 latest
+tnpm dist-tag add @midwayjs/cross-domain@3.0.2 beta
+tnpm dist-tag add @midwayjs/decorator@2.14.0 latest
+tnpm dist-tag add @midwayjs/decorator@3.0.2 beta
+tnpm dist-tag add @midwayjs/express-session@3.0.1 latest
+tnpm dist-tag add @midwayjs/express-session@3.0.2 beta
+tnpm dist-tag add @midwayjs/faas@2.14.4 latest
+tnpm dist-tag add @midwayjs/faas@3.0.2 beta
+tnpm dist-tag add @midwayjs/grpc@2.14.0 latest
+tnpm dist-tag add @midwayjs/grpc@3.0.2 beta
+tnpm dist-tag add @midwayjs/i18n@3.0.1 latest
+tnpm dist-tag add @midwayjs/i18n@3.0.2 beta
+tnpm dist-tag add @midwayjs/info@3.0.1 latest
+tnpm dist-tag add @midwayjs/info@3.0.2 beta
+tnpm dist-tag add @midwayjs/jwt@2.14.2 latest
+tnpm dist-tag add @midwayjs/jwt@3.0.2 beta
+tnpm dist-tag add @midwayjs/mock@2.14.0 latest
+tnpm dist-tag add @midwayjs/mock@3.0.2 beta
+tnpm dist-tag add @midwayjs/mongoose@2.14.2 latest
+tnpm dist-tag add @midwayjs/mongoose@3.0.2 beta
+tnpm dist-tag add @midwayjs/orm@2.14.0 latest
+tnpm dist-tag add @midwayjs/orm@3.0.2 beta
+tnpm dist-tag add @midwayjs/oss@2.14.0 latest
+tnpm dist-tag add @midwayjs/oss@3.0.2 beta
+tnpm dist-tag add @midwayjs/passport@2.14.4 latest
+tnpm dist-tag add @midwayjs/passport@3.0.2 beta
+tnpm dist-tag add @midwayjs/process-agent@2.14.3 latest
+tnpm dist-tag add @midwayjs/process-agent@3.0.2 beta
+tnpm dist-tag add @midwayjs/prometheus-socket-io@2.14.2 latest
+tnpm dist-tag add @midwayjs/prometheus-socket-io@3.0.2 beta
+tnpm dist-tag add @midwayjs/prometheus@2.14.2 latest
+tnpm dist-tag add @midwayjs/prometheus@3.0.2 beta
+tnpm dist-tag add @midwayjs/rabbitmq@2.14.0 latest
+tnpm dist-tag add @midwayjs/rabbitmq@3.0.2 beta
+tnpm dist-tag add @midwayjs/redis@2.14.0 latest
+tnpm dist-tag add @midwayjs/redis@3.0.2 beta
+tnpm dist-tag add @midwayjs/security@3.0.1 latest
+tnpm dist-tag add @midwayjs/security@3.0.2 beta
+tnpm dist-tag add @midwayjs/sequelize@2.14.3 latest
+tnpm dist-tag add @midwayjs/sequelize@3.0.2 beta
+tnpm dist-tag add @midwayjs/session@3.0.1 latest
+tnpm dist-tag add @midwayjs/session@3.0.2 beta
+tnpm dist-tag add @midwayjs/socketio@2.14.0 latest
+tnpm dist-tag add @midwayjs/socketio@3.0.2 beta
+tnpm dist-tag add @midwayjs/static-file@3.0.1 latest
+tnpm dist-tag add @midwayjs/static-file@3.0.2 beta
+tnpm dist-tag add @midwayjs/swagger@3.0.1 latest
+tnpm dist-tag add @midwayjs/swagger@3.0.2 beta
+tnpm dist-tag add @midwayjs/tablestore@2.14.0 latest
+tnpm dist-tag add @midwayjs/tablestore@3.0.2 beta
+tnpm dist-tag add @midwayjs/task@2.14.3 latest
+tnpm dist-tag add @midwayjs/task@3.0.2 beta
+tnpm dist-tag add @midwayjs/typegoose@2.14.2 latest
+tnpm dist-tag add @midwayjs/typegoose@3.0.2 beta
+tnpm dist-tag add @midwayjs/upload@3.0.1 latest
+tnpm dist-tag add @midwayjs/upload@3.0.2 beta
+tnpm dist-tag add @midwayjs/validate@3.0.1 latest
+tnpm dist-tag add @midwayjs/validate@3.0.2 beta
+tnpm dist-tag add @midwayjs/version@2.14.5 latest
+tnpm dist-tag add @midwayjs/version@3.0.2 beta
+tnpm dist-tag add @midwayjs/view-ejs@2.14.2 latest
+tnpm dist-tag add @midwayjs/view-ejs@3.0.2 beta
+tnpm dist-tag add @midwayjs/view-nunjucks@2.14.2 latest
+tnpm dist-tag add @midwayjs/view-nunjucks@3.0.2 beta
+tnpm dist-tag add @midwayjs/view@2.14.2 latest
+tnpm dist-tag add @midwayjs/view@3.0.2 beta
+tnpm dist-tag add @midwayjs/express@2.14.2 latest
+tnpm dist-tag add @midwayjs/express@3.0.2 beta
+tnpm dist-tag add @midwayjs/koa@2.14.2 latest
+tnpm dist-tag add @midwayjs/koa@3.0.2 beta
+tnpm dist-tag add @midwayjs/web@2.14.2 latest
+tnpm dist-tag add @midwayjs/web@3.0.2 beta
+tnpm dist-tag add @midwayjs/ws@2.14.0 latest
+tnpm dist-tag add @midwayjs/ws@3.0.2 beta
+
+# Changes:
+
+#  - @midwayjs/egg-layer: 3.0.1 => 3.0.2
+#  - @midwayjs/express-layer: 3.0.1 => 3.0.2
+#  - @midwayjs/koa-layer: 3.0.1 => 3.0.2
+#  - @midwayjs/runtime-engine: 3.0.0 => 3.0.2
+#  - @midwayjs/runtime-mock: 3.0.0 => 3.0.2
+#  - @midwayjs/serverless-app: 3.0.1 => 3.0.2
+#  - @midwayjs/serverless-aws-starter: 3.0.0 => 3.0.2
+#  - @midwayjs/serverless-fc-starter: 3.0.1 => 3.0.2
+#  - @midwayjs/serverless-fc-trigger: 3.0.1 => 3.0.2
+#  - @midwayjs/serverless-http-parser: 3.0.0 => 3.0.2
+#  - @midwayjs/serverless-scf-starter: 3.0.0 => 3.0.2
+#  - @midwayjs/serverless-scf-trigger: 3.0.1 => 3.0.2
+#  - @midwayjs/serverless-vercel-starter: 3.0.1 => 3.0.2
+#  - @midwayjs/serverless-vercel-trigger: 3.0.0 => 3.0.2
+#  - @midwayjs/serverless-worker-starter: 3.0.1 => 3.0.2
+#  - @midwayjs/static-layer: 3.0.1 => 3.0.2
+#  - @midwayjs/axios: 2.14.0 => 3.0.2
+#  - @midwayjs/bootstrap: 2.14.0 => 3.0.2
+#  - @midwayjs/cache: 2.14.0 => 3.0.2
+#  - @midwayjs/consul: 2.14.2 => 3.0.2
+#  - @midwayjs/core: 2.14.0 => 3.0.2
+#  - @midwayjs/cos: 2.14.0 => 3.0.2
+#  - @midwayjs/cross-domain: 3.0.1 => 3.0.2
+#  - @midwayjs/decorator: 2.14.0 => 3.0.2
+#  - @midwayjs/express-session: 3.0.1 => 3.0.2
+#  - @midwayjs/faas: 2.14.4 => 3.0.2
+#  - @midwayjs/grpc: 2.14.0 => 3.0.2
+#  - @midwayjs/i18n: 3.0.1 => 3.0.2
+#  - @midwayjs/info: 3.0.1 => 3.0.2
+#  - @midwayjs/jwt: 2.14.2 => 3.0.2
+#  - @midwayjs/mock: 2.14.0 => 3.0.2
+#  - @midwayjs/mongoose: 2.14.2 => 3.0.2
+#  - @midwayjs/orm: 2.14.0 => 3.0.2
+#  - @midwayjs/oss: 2.14.0 => 3.0.2
+#  - @midwayjs/passport: 2.14.4 => 3.0.2
+#  - @midwayjs/process-agent: 2.14.3 => 3.0.2
+#  - @midwayjs/prometheus-socket-io: 2.14.2 => 3.0.2
+#  - @midwayjs/prometheus: 2.14.2 => 3.0.2
+#  - @midwayjs/rabbitmq: 2.14.0 => 3.0.2
+#  - @midwayjs/redis: 2.14.0 => 3.0.2
+#  - @midwayjs/security: 3.0.1 => 3.0.2
+#  - @midwayjs/sequelize: 2.14.3 => 3.0.2
+#  - @midwayjs/session: 3.0.1 => 3.0.2
+#  - @midwayjs/socketio: 2.14.0 => 3.0.2
+#  - @midwayjs/static-file: 3.0.1 => 3.0.2
+#  - @midwayjs/swagger: 3.0.1 => 3.0.2
+#  - @midwayjs/tablestore: 2.14.0 => 3.0.2
+#  - @midwayjs/task: 2.14.3 => 3.0.2
+#  - @midwayjs/typegoose: 2.14.2 => 3.0.2
+#  - @midwayjs/upload: 3.0.1 => 3.0.2
+#  - @midwayjs/validate: 3.0.1 => 3.0.2
+#  - @midwayjs/version: 2.14.5 => 3.0.2
+#  - @midwayjs/view-ejs: 2.14.2 => 3.0.2
+#  - @midwayjs/view-nunjucks: 2.14.2 => 3.0.2
+#  - @midwayjs/view: 2.14.2 => 3.0.2
+#  - @midwayjs/express: 2.14.2 => 3.0.2
+#  - @midwayjs/koa: 2.14.2 => 3.0.2
+#  - @midwayjs/web: 2.14.2 => 3.0.2
+#  - @midwayjs/ws: 2.14.0 => 3.0.2
