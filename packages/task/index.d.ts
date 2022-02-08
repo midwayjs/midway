67d64c86ebdc4f7550a0610bb09691c772dd15af
Harry Chen <czy88840616@gmail.com>
2022-02-08 23:06:59 +0800

    fix: task typings

--- packages/task/index.d.ts
+++ packages/task/index.d.ts
@@ -1,8 +1,13 @@
-import * as DefaultConfig from './dist/config/config.default';
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
   // eslint-disable-next-line
-  interface MidwayConfig
-    extends Pick<FileConfigOption<typeof DefaultConfig>, 'task'> {}
+  interface MidwayConfig {
+    task?: {
+      redis?: any;
+      prefix?: string;
+      defaultJobOptions?: any;
+      concurrency?: number;
+    };
+  }
 }
