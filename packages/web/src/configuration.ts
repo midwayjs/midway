f69fb5a59dac41d4457a48a591dc15df8dce36e0
waiting <1661926154@qq.com>
2022-02-11 11:43:29 +0800

    fix(web): check type of variable this.app.middleware (#1688)

--- packages/web/src/configuration.ts
+++ packages/web/src/configuration.ts
@@ -101,7 +101,10 @@ export class EggConfiguration {
   async onReady() {
     // ps: 本想在这里清理掉egg添加的中间件，但是这里的数组已经只有一个 midway root middleware，其他的 getter 都是 egg 加进去的，但是不会被中间件执行，所以不需要清理
     // 去掉为了 egg-socket.io 报错扫进去的 session
-    this.app.middleware = this.app.middleware.slice(0);
+    this.app.middleware =
+      Array.isArray(this.app.middleware) && this.app.middleware.length
+        ? this.app.middleware.slice()
+        : [];
   }
 
   async onServerReady() {
