c277ac35495eef48413268d43fef96138c11e363
Harry Chen <czy88840616@gmail.com>
2022-05-20 17:52:50 +0800

    fix: benchmark

--- scripts/start.js
+++ scripts/start.js
@@ -1,16 +1,12 @@
 const { Bootstrap } = require('@midwayjs/bootstrap');
 
-process.on('message', (data) => {
+process.on('message', data => {
   if (data.action === 'collect_mem') {
     process.send({ action: 'collect_mem_result', data: process.memoryUsage()});
-  } else if(data.action === 'gc') {
+  } else if (data.action === 'gc') {
     console.log('invoke global.gc');
     global.gc();
   }
 });
 
-Bootstrap.load(
-  new (require('@midwayjs/web').Framework)().configure({
-    port: 7001,
-  })
-).run();
+Bootstrap.run();
