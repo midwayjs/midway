ff6f3d80a704ee835f0be42328aa0633003c1cef
Harry Chen <czy88840616@gmail.com>
2022-05-26 16:03:13 +0800

    v3.3.10

--- packages/version/CHANGELOG.md
+++ packages/version/CHANGELOG.md
@@ -3,6 +3,14 @@
 All notable changes to this project will be documented in this file.
 See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
+## [3.3.10](https://github.com/midwayjs/midway/compare/v3.3.9...v3.3.10) (2022-05-26)
+
+**Note:** Version bump only for package @midwayjs/version
+
+
+
+
+
 ## [3.3.9](https://github.com/midwayjs/midway/compare/v3.3.8...v3.3.9) (2022-05-25)
 
 **Note:** Version bump only for package @midwayjs/version
---
+++ scripts/rollback/rollback-3.3.10.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# timestamp: 1653552079318
+
+npm dist-tag add @midwayjs/grpc@3.3.5 latest
+npm dist-tag add @midwayjs/grpc@3.3.10 beta
+npm dist-tag add @midwayjs/version@3.3.9 latest
+npm dist-tag add @midwayjs/version@3.3.10 beta
+tnpm dist-tag add @midwayjs/grpc@3.3.5 latest
+tnpm dist-tag add @midwayjs/grpc@3.3.10 beta
+tnpm dist-tag add @midwayjs/version@3.3.9 latest
+tnpm dist-tag add @midwayjs/version@3.3.10 beta
+
+# Changes:
+
+#  - @midwayjs/grpc: 3.3.5 => 3.3.10
+#  - @midwayjs/version: 3.3.9 => 3.3.10
