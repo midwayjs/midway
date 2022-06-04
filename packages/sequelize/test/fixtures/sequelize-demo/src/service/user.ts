00ee15bdeeee59a418eabf1d6a0ce54a8d8d7c8c
dingdongzhao <ddzyan@163.com>
2022-06-05 00:25:34 +0800

    test(添加sequelize 事务相关测试): 🧪

--- packages/sequelize/test/fixtures/sequelize-demo/src/service/user.ts
+++ packages/sequelize/test/fixtures/sequelize-demo/src/service/user.ts
@@ -1,32 +1,56 @@
-import { Provide } from '@midwayjs/decorator';
+import { Inject, Provide } from '@midwayjs/decorator';
 import HelloModel from '../model/hello';
 import UserModel from '../model/user';
 
+import { SequelizeService } from '../../../../../src/index';
+
 @Provide()
-export class UserService{
+export class UserService {
+  @Inject()
+  sequelizeService: SequelizeService;
 
-  async list(){
-    let result = await UserModel.findAll();
+  async list() {
+    const result = await UserModel.findAll();
     return result;
   }
 
-  async listHello(){
-    let result = await HelloModel.findAll();
+  async listHello() {
+    const result = await HelloModel.findAll();
     return result;
   }
 
-  async add(){
-    let result = await UserModel.create({
-      name: '123'
-    })
+  async add() {
+    const result = await UserModel.create({
+      name: '123',
+    });
     return result;
   }
 
-  async delete(){
+  async delete() {
     await UserModel.destroy({
       where: {
-        name: '123'
-      }
-    })
+        name: '123',
+      },
+    });
+  }
+
+  async create() {
+    const t = await this.sequelizeService.transaction();
+    try {
+      const result = await UserModel.create(
+        {
+          name: '123',
+        },
+        {
+          transaction: t,
+        }
+      );
+      await t.commit();
+
+      return result;
+    } catch (error) {
+      await t.rollback();
+      throw error;
+    }
   }
 }
--- packages/sequelize/test/index.test.ts
+++ packages/sequelize/test/index.test.ts
@@ -1,36 +1,58 @@
 import { createApp, close } from '@midwayjs/mock';
 import { Framework } from '@midwayjs/koa';
 import { join } from 'path';
-import { UserService } from './fixtures/sequelize-demo/src/service/user'
+import { UserService } from './fixtures/sequelize-demo/src/service/user';
 
 describe('/test/index.test.ts', () => {
   let app = null;
   beforeAll(async () => {
-    app = await createApp(join(__dirname, 'fixtures', 'sequelize-demo'), {}, Framework);
+    app = await createApp(
+      join(__dirname, 'fixtures', 'sequelize-demo'),
+      {},
+      Framework
+    );
   });
 
   afterAll(async () => {
     await close(app);
-  })
+  });
   it('list user service ', async () => {
-    let userService: UserService = await app.getApplicationContext().getAsync(UserService);
-    let result = await userService.list();
-    expect(result.length).toBe(0)
+    const userService: UserService = await app
+      .getApplicationContext()
+      .getAsync(UserService);
+    const result = await userService.list();
+    expect(result.length).toBe(0);
   });
 
   it('list hello service ', async () => {
-    let userService: UserService = await app.getApplicationContext().getAsync(UserService);
-    let result = await userService.listHello();
+    const userService: UserService = await app
+      .getApplicationContext()
+      .getAsync(UserService);
+    const result = await userService.listHello();
     expect(result.length).toBe(0);
   });
 
   it('add and delete', async () => {
-    let userService: UserService = await app.getApplicationContext().getAsync(UserService);
+    const userService: UserService = await app
+      .getApplicationContext()
+      .getAsync(UserService);
     await userService.add();
     let result = await userService.list();
-    expect(result.length).toBe(1)
+    expect(result.length).toBe(1);
     await userService.delete();
     result = await userService.list();
-    expect(result.length).toBe(0)
+    expect(result.length).toBe(0);
+  });
+
+  it('test create transaction', async () => {
+    const userService: UserService = await app
+      .getApplicationContext()
+      .getAsync(UserService);
+    await userService.create();
+    let result = await userService.list();
+    expect(result.length).toBe(1);
+    await userService.delete();
+    result = await userService.list();
+    expect(result.length).toBe(0);
   });
 });
