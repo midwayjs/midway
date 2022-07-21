7b77a55751229d65ed65a9fa9a36f06ae610ff7c
dingdongzhao <ddzyan@163.com>
2022-07-21 18:41:33 +0800

    fix(修复最新版本sequlize声明配置不兼容老版本，导致编译错误): 🐞

--- packages/sequelize/index.d.ts
+++ packages/sequelize/index.d.ts
@@ -1,9 +1,16 @@
 import { SequelizeConfigOptions } from './dist';
 
+import { SequelizeOptions } from 'sequelize-typescript';
+
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    sequelize?: PowerPartial<SequelizeConfigOptions>;
+    sequelize?:
+      | PowerPartial<SequelizeConfigOptions>
+      | {
+          options?: SequelizeOptions;
+          sync?: boolean;
+        };
   }
 }
