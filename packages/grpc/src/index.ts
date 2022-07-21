b111357ab2e610a625a75bb999b97ebce9d29e4a
Harry Chen <czy88840616@gmail.com>
2022-07-21 13:26:09 +0800

    fix: export server credentials class

--- packages/grpc/src/index.ts
+++ packages/grpc/src/index.ts
@@ -3,3 +3,4 @@ export { MidwayGRPCFramework as Framework } from './provider/framework';
 export { GrpcConfiguration as Configuration } from './configuration';
 export { GRPCClients as Clients, createGRPCConsumer } from './comsumer/clients';
 export { loadProto } from './util';
+export { Metadata, ServerCredentials } from '@grpc/grpc-js';
