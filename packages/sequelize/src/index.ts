50dca8b5e7d7fda5a45ee3f3d0b4c882edb29171
Harry Chen <czy88840616@gmail.com>
2022-07-23 00:08:19 +0800

    refactor: try to lock dep version

--- packages/sequelize/src/index.ts
+++ packages/sequelize/src/index.ts
@@ -2,3 +2,5 @@ export { SequelizeConfiguration as Configuration } from './configuration';
 export { BaseTable, InjectRepository } from './decorator';
 export * from './interface';
 export * from './dataSourceManager';
+export * as Sequelize from 'sequelize';
+export * as Sequelize_TS from 'sequelize-typescript';
