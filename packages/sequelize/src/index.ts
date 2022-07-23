0f2c873f9135acbc7ac7af59d8deb688e0eaeed8
Harry Chen <czy88840616@gmail.com>
2022-07-23 10:54:13 +0800

    refactor: export sequelize-typescript and sequelize typings

--- packages/sequelize/src/index.ts
+++ packages/sequelize/src/index.ts
@@ -2,5 +2,6 @@ export { SequelizeConfiguration as Configuration } from './configuration';
 export { BaseTable, InjectRepository } from './decorator';
 export * from './interface';
 export * from './dataSourceManager';
-export * as Sequelize from 'sequelize';
-export * as Sequelize_TS from 'sequelize-typescript';
+export * from 'sequelize-typescript';
+export { Optional, Op } from 'sequelize';
+export * as sequelize from 'sequelize';
