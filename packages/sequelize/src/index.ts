ef70d2c1b3b622ed31e142cd16cb1d4e63f378f4
Harry Chen <czy88840616@gmail.com>
2022-07-23 11:19:42 +0800

    chore: export some type from sequelize

--- packages/sequelize/src/index.ts
+++ packages/sequelize/src/index.ts
@@ -3,5 +3,4 @@ export { BaseTable, InjectRepository } from './decorator';
 export * from './interface';
 export * from './dataSourceManager';
 export * from 'sequelize-typescript';
-export { Optional, Op } from 'sequelize';
-export * as sequelize from 'sequelize';
+export { QueryTypes, Optional, TableHints, IndexHints } from 'sequelize';
