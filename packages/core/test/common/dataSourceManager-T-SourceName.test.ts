5346e7e67e2c8608685ad934d216f4f1becdf049
waiting <1661926154@qq.com>
2022-07-29 22:27:46 +0800

    test(core): add "should test base data source without types"

--- packages/core/test/common/dataSourceManager-T-SourceName.test.ts
+++ packages/core/test/common/dataSourceManager-T-SourceName.test.ts
@@ -201,4 +201,60 @@ describe(filename, () => {
     await instance.stop();
     expect(instance.getDataSourceNames()).toEqual([]);
   });
+
+  it('should test base data source without types', async () => {
+    class EntityA {}
+
+    class EntityB {}
+
+    type Config = DataSourceConfig
+
+    const config: Config = {
+      dataSource: {
+        default: {
+          host: 'localhost',    //数据库地址,默认本机
+          port:'3306',
+          dialect: 'mysql',
+          pool: {   //连接池设置
+            max: 5, //最大连接数
+            min: 0, //最小连接数
+            idle: 10000
+          },
+          entities: [EntityA, EntityB]
+        },
+        test: {
+          host: 'localhost',    //数据库地址,默认本机
+          port:'3306',
+          dialect: 'mysql',
+          pool: {   //连接池设置
+            max: 5, //最大连接数
+            min: 0, //最小连接数
+            idle: 10000
+          },
+          dataSourceGroup: 'bb',
+          entities: [EntityA]
+        }
+      },
+    }
+
+    const instance = new CustomDataSourceFactory();
+    expect(instance.getName()).toEqual('test');
+
+    await instance.init(config)
+    expect(instance.getDataSourceNames()).toEqual(['default', 'test']);
+    expect(instance.hasDataSource('default')).toBeTruthy();
+    // expect(instance.getDataSource('default')).toMatchSnapshot();
+    expect(instance.getDataSource('default')?.entitiesLength).toEqual(2);
+    // expect(instance.getDataSource('test')).toMatchSnapshot();
+    expect(instance.getDataSource('test')?.entitiesLength).toEqual(1);
+    expect(instance.getDataSource('fff')).not.toBeDefined();
+
+    expect(await instance.isConnected('default')).toBeFalsy();
+
+    expect(instance.getDataSourceNameByModel(EntityB)).toEqual('default');
+    expect(instance.getDataSourceNameByModel(EntityA)).toEqual('test');
+
+    await instance.stop();
+    expect(instance.getDataSourceNames()).toEqual([]);
+  });
 });
