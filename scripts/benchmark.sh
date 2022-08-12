c2451788481e135a0577f9dec8c8079c7115c263
Yichen Yan <wenji.yyc@alibaba-inc.com>
2022-08-12 11:50:28 +0800

    update

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 npm install autocannon
-npm install create-midway
-# npm >= 8 drop parameters, so call directly.
+npm install -g create-midway
+# npm >= 8 drop parameters (?), so call directly.
 create-midway --template=@midwayjs-examples/application-web-v3 midway_benchmark_app
 cp ./scripts/start.js ./midway_benchmark_app/start.js
 cp ./scripts/benchmark.js ./midway_benchmark_app/benchmark.js
 cd midway_benchmark_app
 npm run build
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator @midwayjs/mock @midwayjs/bootstrap
+# failed here ⬆️ with following message:
+# npm ERR! Cannot read properties of null (reading 'package')
+
+# npm ERR! A complete log of this run can be found in:
+# npm ERR!     /home/yyc/.npm/_logs/2022-08-12T03_47_10_153Z-debug-0.log
 node benchmark.js
