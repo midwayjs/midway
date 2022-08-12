11d2fe4d7e3e90c18ff5b7f66ebd4fbcd6e15ace
Yichen Yan <wenji.yyc@alibaba-inc.com>
2022-08-12 15:09:16 +0800

    update

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -9,9 +9,5 @@ cp ./scripts/benchmark.js ./midway_benchmark_app/benchmark.js
 cd midway_benchmark_app
 npm run build
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator @midwayjs/mock @midwayjs/bootstrap
-# failed here ⬆️ with following message:
-# npm ERR! Cannot read properties of null (reading 'package')
-
-# npm ERR! A complete log of this run can be found in:
-# npm ERR!     /home/yyc/.npm/_logs/2022-08-12T03_47_10_153Z-debug-0.log
 node benchmark.js
+# failed here ⬆️:
