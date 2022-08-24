bbd2aa2565ec169dcaa90318fdd7e6dcf2e97479
Harry Chen <czy88840616@gmail.com>
2022-08-24 11:34:55 +0800

    chore: add echo

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-npm install autocannon
 npm init midway -- --template=@midwayjs-examples/application-web-v3 midway_benchmark_app
+echo '[benchmark] create template complete'
 cp ./scripts/start.js ./midway_benchmark_app/start.js
 cp ./scripts/benchmark.js ./midway_benchmark_app/benchmark.js
+echo '[benchmark] script complete'
 cd midway_benchmark_app
 npm run build
+echo '[benchmark] build example complete'
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator @midwayjs/mock @midwayjs/bootstrap
+echo '[benchmark] link package complete'
 node benchmark.js
