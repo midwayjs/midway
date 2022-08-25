6ab761c607476e8b6eaf2201e59e8347275bb9b9
chenzhaozheng <389428742@qq.com>
2022-08-25 20:14:31 +0800

    style(kafka): 优化loadSubscriber执行的逻辑

--- packages/kafka/src/framework.ts
+++ packages/kafka/src/framework.ts
@@ -9,7 +9,11 @@ import {
   MSListenerType,
   MS_CONSUMER_KEY,
 } from '@midwayjs/decorator';
-import { IMidwayKafkaApplication, IMidwayKafkaContext } from './interface';
+import {
+  IMidwayConsumerConfig,
+  IMidwayKafkaApplication,
+  IMidwayKafkaContext,
+} from './interface';
 import { KafkaConsumerServer } from './kafka';
 
 @Framework()
@@ -18,7 +22,7 @@ export class MidwayKafkaFramework extends BaseFramework<any, any, any> {
     return this.configService.getConfiguration('kafka');
   }
 
-  async applicationInitialize(options) {
+  async applicationInitialize() {
     // Create a connection manager
     this.app = new KafkaConsumerServer({
       logger: this.logger,
@@ -50,90 +54,87 @@ export class MidwayKafkaFramework extends BaseFramework<any, any, any> {
     });
     for (const module of subscriberModules) {
       const data = listPropertyDataFromClass(MS_CONSUMER_KEY, module);
-      const topics = [...new Set(data.map(e => e[0].topic))];
-      let tempTopics = [];
-      topics.forEach(e => {
-        tempTopics.push({ topic: e });
-      });
-      tempTopics = tempTopics.map(e => {
-        let subscription = {};
-        let runConfig = {};
-        data.forEach(e2 => {
-          if (e2[0].topic === e.topic) {
-            if (
-              typeof e2[0].subscription !== 'undefined' &&
-              Object.keys(e2[0].subscription).length > 0
-            ) {
-              subscription = e2[0].subscription;
-            }
-            if (
-              typeof e2[0].runConfig !== 'undefined' &&
-              Object.keys(e2[0].runConfig).length > 0
-            ) {
-              runConfig = e2[0].runConfig;
-            }
+      const topicTitles = [...new Set(data.map(e => e[0].topic))];
+      const midwayConsumerConfigs: IMidwayConsumerConfig[] = topicTitles.map(
+        e => {
+          const midwayConsumerConfig: IMidwayConsumerConfig = {
+            topic: e,
+            subscription: {},
+            runConfig: {},
+          };
+
+          const comsumerParams = data
+            .map(value => {
+              if (value[0].topic === midwayConsumerConfig.topic) {
+                return Object.assign(midwayConsumerConfig, value[0]);
+              }
+            })
+            .filter(e => e && true);
+
+          if (comsumerParams && Object.keys(comsumerParams[0]).length > 0) {
+            return comsumerParams[0];
           }
-        });
-        e.subscription = subscription;
-        e.runConfig = runConfig;
-        return e;
-      });
-      tempTopics.forEach(async e => {
-        await this.app.connection.subscribe(
-          Object.assign(
-            {
-              topics: topics,
-            },
-            e.subscription
-          )
-        );
-        await this.app.connection.run(
-          Object.assign(e.runConfig, {
-            eachMessage: async ({ topic, partition, message }) => {
-              let propertyKey;
-              for (const methodBindListeners of data) {
-                for (const listenerOptions of methodBindListeners) {
-                  if (topic === listenerOptions.topic) {
-                    propertyKey = listenerOptions.propertyKey;
-                    const ctx = {
-                      topic: topic,
-                      partition: partition,
-                      message: message,
-                      commitOffsets: (offset: any) => {
-                        return this.app.connection.commitOffsets([
-                          {
-                            topic: topic,
-                            partition: partition,
-                            offset,
-                          },
-                        ]);
-                      },
-                    } as unknown as IMidwayKafkaContext;
-                    this.app.createAnonymousContext(ctx);
-                    const ins = await ctx.requestContext.getAsync(module);
-                    const fn = await this.applyMiddleware(async ctx => {
-                      return await ins[propertyKey].call(ins, message);
-                    });
-                    try {
-                      const result = await fn(ctx);
-                      // 返回为undefined，下面永远不会执行
-                      if (result) {
-                        const res = await this.app.connection.commitOffsets(
-                          message.offset
-                        );
-                        return res;
+          return midwayConsumerConfig;
+        }
+      );
+      midwayConsumerConfigs.forEach(
+        async (e: { subscription: any; runConfig: any }) => {
+          await this.app.connection.subscribe(
+            Object.assign(
+              {
+                topics: topicTitles,
+              },
+              e.subscription
+            )
+          );
+          await this.app.connection.run(
+            Object.assign(e.runConfig, {
+              eachMessage: async ({ topic, partition, message }) => {
+                let propertyKey: string | number;
+                for (const methodBindListeners of data) {
+                  for (const listenerOptions of methodBindListeners) {
+                    if (topic === listenerOptions.topic) {
+                      propertyKey = listenerOptions.propertyKey;
+                      const ctx = {
+                        topic: topic,
+                        partition: partition,
+                        message: message,
+                        commitOffsets: (offset: any) => {
+                          return this.app.connection.commitOffsets([
+                            {
+                              topic: topic,
+                              partition: partition,
+                              offset,
+                            },
+                          ]);
+                        },
+                      } as unknown as IMidwayKafkaContext;
+                      this.app.createAnonymousContext(ctx);
+                      const ins = await ctx.requestContext.getAsync(module);
+                      const fn = await this.applyMiddleware(async () => {
+                        return await ins[propertyKey].call(ins, message);
+                      });
+                      try {
+                        const result = await fn(ctx);
+                        // 返回为undefined，下面永远不会执行
+                        if (result) {
+                          const res = await this.app.connection.commitOffsets(
+                            message.offset
+                          );
+                          return res;
+                        }
+                      } catch (error) {
+                        // 记录偏移量提交的异常情况
+                        this.logger.error(error);
                       }
-                    } catch (error) {
-                      // 记录偏移量提交的异常情况
-                      this.logger.error(error);
                     }
                   }
                 }
-              }
-            },
-          })
-        );
-      });
+              },
+            })
+          );
+        }
+      );
     }
   }
 
