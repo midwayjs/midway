b845070ee68bcbfe9aacd0bcb7f299fb2400825a
Harry Chen <czy88840616@gmail.com>
2022-08-27 15:09:35 +0800

    test: use nock replace proxy

--- packages/http-proxy/test/koa.test.ts
+++ packages/http-proxy/test/koa.test.ts
@@ -1,21 +1,56 @@
 import { createApp, createHttpRequest, close } from '@midwayjs/mock';
 import { join } from 'path';
 import * as assert from 'assert';
+import * as nock from 'nock';
 
 describe('test/koa.test.ts', function () {
   let app;
   beforeAll(async () => {
+    nock('https://gw.alicdn.com').get('/tfs/TB1.1EzoBBh1e4jSZFhXXcC9VXa-48-48.png?version=123').reply(200, '123', {'content-type': 'image/png'});
+    nock('https://www.baidu.com').get('/').reply(200, '<html>123</html>', {'content-type': 'text/html'});
+    nock('https://sm.bdimg.com').get('/static/wiseindex/amd_modules/@searchfe/assert_3ed54c3.js').reply(200, '123', {'content-type': 'application/x-javascript'});
+    nock('https://httpbin.org')
+      .persist()
+      .get('/get?name=midway').reply(200, {
+      "args": {
+        "name": "midway"
+      },
+      "headers": {
+        "Host": "httpbin.org",
+      },
+      "url": "https://httpbin.org/get?name=midway"
+    }, {'content-type': 'application/json'})
+      .post('/post').reply(200, function (uri, requestBody) {
+      const body = {
+        'headers': {
+          'Host': 'httpbin.org',
+          'Content-Type': this.req.headers['content-type'],
+          'url': 'https://httpbin.org/post'
+        },
+        'url': 'https://httpbin.org/post'
+      } as any;
+      if (this.req.headers['content-type'] === 'application/x-www-form-urlencoded') {
+        body.form = {
+          "name": "midway"
+        };
+      }
+      if (this.req.headers['content-type'] === 'application/json') {
+        body.data = JSON.stringify(requestBody);
+      }
+      return body;
+    }, {'content-type': 'application/json'});
     const appDir = join(__dirname, 'fixtures/koa');
     app = await createApp(appDir);
   })
 
   afterAll(async () => {
     await close(app);
+    nock.restore();
   });
 
   it('get image by host', async () => {
     const request = await createHttpRequest(app);
-    
+
     await request.get('/tfs/TB1.1EzoBBh1e4jSZFhXXcC9VXa-48-48.png?version=123')
       .expect(200)
       .then(async response => {
@@ -80,7 +115,7 @@ describe('test/koa.test.ts', function () {
       .set('Accept', 'application/json')
       .expect(200)
       .then(async response => {
-        
+
         assert(response.status === 200)
         assert(response.body.url === 'https://httpbin.org/post');
         assert(response.body.headers['Content-Type'] === 'application/x-www-form-urlencoded');
