a16923b0695e5c25ee9038cc05bd2ad9cb6b3109
Harry Chen <czy88840616@gmail.com>
2022-08-29 20:28:35 +0800

    fix: oss config typings

--- packages/oss/index.d.ts
+++ packages/oss/index.d.ts
@@ -1,9 +1,9 @@
-import * as OSS from 'ali-oss';
+import { OSSServiceFactoryCreateClientConfigType } from './dist';
 
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    oss?: ServiceFactoryConfigOption<OSS.STSOptions | OSS.Options>;
+    oss?: ServiceFactoryConfigOption<OSSServiceFactoryCreateClientConfigType>;
   }
 }
