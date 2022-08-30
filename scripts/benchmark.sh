3bb5e286e3bd5510461cd07b9f77f17429eafb2f
waiting <1661926154@qq.com>
2022-08-30 23:50:10 +0800

    ci: update benchmark scripts
    pretty output

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-npm init midway -- --template=@midwayjs-examples/application-web-v3 midway_benchmark_app
+export DIR=midway_benchmark_app
+
+npm init midway -- --template=@midwayjs-examples/application-web-v3 $DIR
 echo '[benchmark] create template complete'
-cp ./scripts/start.js ./midway_benchmark_app/start.js
-cp ./scripts/benchmark.js ./midway_benchmark_app/benchmark.js
+
+cp -f ./scripts/start.js ./$DIR/start.js
+cp -f ./scripts/benchmark.js ./$DIR/benchmark.js
 echo '[benchmark] script complete'
-cd midway_benchmark_app
+
+cd $DIR
 npm run build
 echo '[benchmark] build example complete'
+
 npm link @midwayjs/web @midwayjs/core @midwayjs/decorator @midwayjs/mock @midwayjs/bootstrap
 echo '[benchmark] link package complete'
+
 node benchmark.js
