951a1f217c18a338b032220da58cc1bc4a15ef84
ilyydy <ilyydy@gmail.com>
2022-10-04 12:06:44 +0800

    fix: add default generic type to MikroConfigOptions

--- packages/mikro/src/interface.ts
+++ packages/mikro/src/interface.ts
@@ -1,4 +1,4 @@
 import { Options, Configuration, IDatabaseDriver } from '@mikro-orm/core';
 import { DataSourceManagerConfigOption } from '@midwayjs/core';
 
-export type MikroConfigOptions<D extends IDatabaseDriver> = DataSourceManagerConfigOption<Options<D> | Configuration<D>>;
+export type MikroConfigOptions<D extends IDatabaseDriver = IDatabaseDriver> = DataSourceManagerConfigOption<Options<D> | Configuration<D>>;
