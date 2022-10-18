97fb994c612b534cb726f3da0ef881d613546852
Harry Chen <czy88840616@gmail.com>
2022-10-18 23:59:03 +0800

    fix: method

--- packages-serverless/midway-fc-starter/src/index.ts
+++ packages-serverless/midway-fc-starter/src/index.ts
@@ -154,7 +154,7 @@ export class BootstrapStarter extends AbstractBootstrapStarter {
     }
 
     try {
-      const result = await this.framework(ctx, handlerName, {
+      const result = await this.framework.getTriggerFunction(ctx, handlerName, {
         isHttpFunction: isHTTPMode || isApiGateway,
         originEvent: event,
         originContext: context,
