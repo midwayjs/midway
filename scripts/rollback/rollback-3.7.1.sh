d41be2f6082d035ea4fb109e32b0e240c2435f9a
Harry Chen <czy88840616@gmail.com>
2022-10-30 16:37:58 +0800

    v3.7.1

---
+++ scripts/rollback/rollback-3.7.1.sh
@@ -0,0 +1,266 @@
+#!/bin/bash
+# timestamp: 1667118922627
+
+npm dist-tag add @midwayjs/fc-starter@3.7.0 latest
+npm dist-tag add @midwayjs/fc-starter@3.7.1 beta
+npm dist-tag add @midwayjs/axios@3.7.0 latest
+npm dist-tag add @midwayjs/axios@3.7.1 beta
+npm dist-tag add @midwayjs/bootstrap@3.7.0 latest
+npm dist-tag add @midwayjs/bootstrap@3.7.1 beta
+npm dist-tag add @midwayjs/bull-board@3.7.0 latest
+npm dist-tag add @midwayjs/bull-board@3.7.1 beta
+npm dist-tag add @midwayjs/bull@3.7.0 latest
+npm dist-tag add @midwayjs/bull@3.7.1 beta
+npm dist-tag add @midwayjs/cache@3.7.0 latest
+npm dist-tag add @midwayjs/cache@3.7.1 beta
+npm dist-tag add @midwayjs/captcha@3.7.0 latest
+npm dist-tag add @midwayjs/captcha@3.7.1 beta
+npm dist-tag add @midwayjs/casbin-redis-adapter@3.7.0 latest
+npm dist-tag add @midwayjs/casbin-redis-adapter@3.7.1 beta
+npm dist-tag add @midwayjs/casbin-typeorm-adapter@3.7.0 latest
+npm dist-tag add @midwayjs/casbin-typeorm-adapter@3.7.1 beta
+npm dist-tag add @midwayjs/casbin@3.7.0 latest
+npm dist-tag add @midwayjs/casbin@3.7.1 beta
+npm dist-tag add @midwayjs/code-dye@3.7.0 latest
+npm dist-tag add @midwayjs/code-dye@3.7.1 beta
+npm dist-tag add @midwayjs/consul@3.7.0 latest
+npm dist-tag add @midwayjs/consul@3.7.1 beta
+npm dist-tag add @midwayjs/cos@3.7.0 latest
+npm dist-tag add @midwayjs/cos@3.7.1 beta
+npm dist-tag add @midwayjs/cross-domain@3.7.0 latest
+npm dist-tag add @midwayjs/cross-domain@3.7.1 beta
+npm dist-tag add @midwayjs/express-session@3.7.0 latest
+npm dist-tag add @midwayjs/express-session@3.7.1 beta
+npm dist-tag add @midwayjs/faas@3.7.0 latest
+npm dist-tag add @midwayjs/faas@3.7.1 beta
+npm dist-tag add @midwayjs/grpc@3.7.0 latest
+npm dist-tag add @midwayjs/grpc@3.7.1 beta
+npm dist-tag add @midwayjs/http-proxy@3.7.0 latest
+npm dist-tag add @midwayjs/http-proxy@3.7.1 beta
+npm dist-tag add @midwayjs/i18n@3.7.0 latest
+npm dist-tag add @midwayjs/i18n@3.7.1 beta
+npm dist-tag add @midwayjs/info@3.7.0 latest
+npm dist-tag add @midwayjs/info@3.7.1 beta
+npm dist-tag add @midwayjs/jwt@3.7.0 latest
+npm dist-tag add @midwayjs/jwt@3.7.1 beta
+npm dist-tag add @midwayjs/kafka@3.7.0 latest
+npm dist-tag add @midwayjs/kafka@3.7.1 beta
+npm dist-tag add @midwayjs/mikro@3.7.0 latest
+npm dist-tag add @midwayjs/mikro@3.7.1 beta
+npm dist-tag add @midwayjs/mock@3.7.0 latest
+npm dist-tag add @midwayjs/mock@3.7.1 beta
+npm dist-tag add @midwayjs/mongoose@3.7.0 latest
+npm dist-tag add @midwayjs/mongoose@3.7.1 beta
+npm dist-tag add @midwayjs/oss@3.7.0 latest
+npm dist-tag add @midwayjs/oss@3.7.1 beta
+npm dist-tag add @midwayjs/otel@3.7.0 latest
+npm dist-tag add @midwayjs/otel@3.7.1 beta
+npm dist-tag add @midwayjs/passport@3.7.0 latest
+npm dist-tag add @midwayjs/passport@3.7.1 beta
+npm dist-tag add @midwayjs/process-agent@3.7.0 latest
+npm dist-tag add @midwayjs/process-agent@3.7.1 beta
+npm dist-tag add @midwayjs/prometheus-socket-io@3.7.0 latest
+npm dist-tag add @midwayjs/prometheus-socket-io@3.7.1 beta
+npm dist-tag add @midwayjs/prometheus@3.7.0 latest
+npm dist-tag add @midwayjs/prometheus@3.7.1 beta
+npm dist-tag add @midwayjs/rabbitmq@3.7.0 latest
+npm dist-tag add @midwayjs/rabbitmq@3.7.1 beta
+npm dist-tag add @midwayjs/redis@3.7.0 latest
+npm dist-tag add @midwayjs/redis@3.7.1 beta
+npm dist-tag add @midwayjs/security@3.7.0 latest
+npm dist-tag add @midwayjs/security@3.7.1 beta
+npm dist-tag add @midwayjs/sequelize@3.7.0 latest
+npm dist-tag add @midwayjs/sequelize@3.7.1 beta
+npm dist-tag add @midwayjs/session@3.7.0 latest
+npm dist-tag add @midwayjs/session@3.7.1 beta
+npm dist-tag add @midwayjs/socketio@3.7.0 latest
+npm dist-tag add @midwayjs/socketio@3.7.1 beta
+npm dist-tag add @midwayjs/static-file@3.7.0 latest
+npm dist-tag add @midwayjs/static-file@3.7.1 beta
+npm dist-tag add @midwayjs/swagger@3.7.0 latest
+npm dist-tag add @midwayjs/swagger@3.7.1 beta
+npm dist-tag add @midwayjs/tablestore@3.7.0 latest
+npm dist-tag add @midwayjs/tablestore@3.7.1 beta
+npm dist-tag add @midwayjs/typegoose@3.7.0 latest
+npm dist-tag add @midwayjs/typegoose@3.7.1 beta
+npm dist-tag add @midwayjs/typeorm@3.7.0 latest
+npm dist-tag add @midwayjs/typeorm@3.7.1 beta
+npm dist-tag add @midwayjs/upload@3.7.0 latest
+npm dist-tag add @midwayjs/upload@3.7.1 beta
+npm dist-tag add @midwayjs/validate@3.7.0 latest
+npm dist-tag add @midwayjs/validate@3.7.1 beta
+npm dist-tag add @midwayjs/version@3.7.0 latest
+npm dist-tag add @midwayjs/version@3.7.1 beta
+npm dist-tag add @midwayjs/view-ejs@3.7.0 latest
+npm dist-tag add @midwayjs/view-ejs@3.7.1 beta
+npm dist-tag add @midwayjs/view-nunjucks@3.7.0 latest
+npm dist-tag add @midwayjs/view-nunjucks@3.7.1 beta
+npm dist-tag add @midwayjs/view@3.7.0 latest
+npm dist-tag add @midwayjs/view@3.7.1 beta
+npm dist-tag add @midwayjs/express@3.7.0 latest
+npm dist-tag add @midwayjs/express@3.7.1 beta
+npm dist-tag add @midwayjs/koa@3.7.0 latest
+npm dist-tag add @midwayjs/koa@3.7.1 beta
+npm dist-tag add @midwayjs/web@3.7.0 latest
+npm dist-tag add @midwayjs/web@3.7.1 beta
+npm dist-tag add @midwayjs/ws@3.7.0 latest
+npm dist-tag add @midwayjs/ws@3.7.1 beta
+tnpm dist-tag add @midwayjs/fc-starter@3.7.0 latest
+tnpm dist-tag add @midwayjs/fc-starter@3.7.1 beta
+tnpm dist-tag add @midwayjs/axios@3.7.0 latest
+tnpm dist-tag add @midwayjs/axios@3.7.1 beta
+tnpm dist-tag add @midwayjs/bootstrap@3.7.0 latest
+tnpm dist-tag add @midwayjs/bootstrap@3.7.1 beta
+tnpm dist-tag add @midwayjs/bull-board@3.7.0 latest
+tnpm dist-tag add @midwayjs/bull-board@3.7.1 beta
+tnpm dist-tag add @midwayjs/bull@3.7.0 latest
+tnpm dist-tag add @midwayjs/bull@3.7.1 beta
+tnpm dist-tag add @midwayjs/cache@3.7.0 latest
+tnpm dist-tag add @midwayjs/cache@3.7.1 beta
+tnpm dist-tag add @midwayjs/captcha@3.7.0 latest
+tnpm dist-tag add @midwayjs/captcha@3.7.1 beta
+tnpm dist-tag add @midwayjs/casbin-redis-adapter@3.7.0 latest
+tnpm dist-tag add @midwayjs/casbin-redis-adapter@3.7.1 beta
+tnpm dist-tag add @midwayjs/casbin-typeorm-adapter@3.7.0 latest
+tnpm dist-tag add @midwayjs/casbin-typeorm-adapter@3.7.1 beta
+tnpm dist-tag add @midwayjs/casbin@3.7.0 latest
+tnpm dist-tag add @midwayjs/casbin@3.7.1 beta
+tnpm dist-tag add @midwayjs/code-dye@3.7.0 latest
+tnpm dist-tag add @midwayjs/code-dye@3.7.1 beta
+tnpm dist-tag add @midwayjs/consul@3.7.0 latest
+tnpm dist-tag add @midwayjs/consul@3.7.1 beta
+tnpm dist-tag add @midwayjs/cos@3.7.0 latest
+tnpm dist-tag add @midwayjs/cos@3.7.1 beta
+tnpm dist-tag add @midwayjs/cross-domain@3.7.0 latest
+tnpm dist-tag add @midwayjs/cross-domain@3.7.1 beta
+tnpm dist-tag add @midwayjs/express-session@3.7.0 latest
+tnpm dist-tag add @midwayjs/express-session@3.7.1 beta
+tnpm dist-tag add @midwayjs/faas@3.7.0 latest
+tnpm dist-tag add @midwayjs/faas@3.7.1 beta
+tnpm dist-tag add @midwayjs/grpc@3.7.0 latest
+tnpm dist-tag add @midwayjs/grpc@3.7.1 beta
+tnpm dist-tag add @midwayjs/http-proxy@3.7.0 latest
+tnpm dist-tag add @midwayjs/http-proxy@3.7.1 beta
+tnpm dist-tag add @midwayjs/i18n@3.7.0 latest
+tnpm dist-tag add @midwayjs/i18n@3.7.1 beta
+tnpm dist-tag add @midwayjs/info@3.7.0 latest
+tnpm dist-tag add @midwayjs/info@3.7.1 beta
+tnpm dist-tag add @midwayjs/jwt@3.7.0 latest
+tnpm dist-tag add @midwayjs/jwt@3.7.1 beta
+tnpm dist-tag add @midwayjs/kafka@3.7.0 latest
+tnpm dist-tag add @midwayjs/kafka@3.7.1 beta
+tnpm dist-tag add @midwayjs/mikro@3.7.0 latest
+tnpm dist-tag add @midwayjs/mikro@3.7.1 beta
+tnpm dist-tag add @midwayjs/mock@3.7.0 latest
+tnpm dist-tag add @midwayjs/mock@3.7.1 beta
+tnpm dist-tag add @midwayjs/mongoose@3.7.0 latest
+tnpm dist-tag add @midwayjs/mongoose@3.7.1 beta
+tnpm dist-tag add @midwayjs/oss@3.7.0 latest
+tnpm dist-tag add @midwayjs/oss@3.7.1 beta
+tnpm dist-tag add @midwayjs/otel@3.7.0 latest
+tnpm dist-tag add @midwayjs/otel@3.7.1 beta
+tnpm dist-tag add @midwayjs/passport@3.7.0 latest
+tnpm dist-tag add @midwayjs/passport@3.7.1 beta
+tnpm dist-tag add @midwayjs/process-agent@3.7.0 latest
+tnpm dist-tag add @midwayjs/process-agent@3.7.1 beta
+tnpm dist-tag add @midwayjs/prometheus-socket-io@3.7.0 latest
+tnpm dist-tag add @midwayjs/prometheus-socket-io@3.7.1 beta
+tnpm dist-tag add @midwayjs/prometheus@3.7.0 latest
+tnpm dist-tag add @midwayjs/prometheus@3.7.1 beta
+tnpm dist-tag add @midwayjs/rabbitmq@3.7.0 latest
+tnpm dist-tag add @midwayjs/rabbitmq@3.7.1 beta
+tnpm dist-tag add @midwayjs/redis@3.7.0 latest
+tnpm dist-tag add @midwayjs/redis@3.7.1 beta
+tnpm dist-tag add @midwayjs/security@3.7.0 latest
+tnpm dist-tag add @midwayjs/security@3.7.1 beta
+tnpm dist-tag add @midwayjs/sequelize@3.7.0 latest
+tnpm dist-tag add @midwayjs/sequelize@3.7.1 beta
+tnpm dist-tag add @midwayjs/session@3.7.0 latest
+tnpm dist-tag add @midwayjs/session@3.7.1 beta
+tnpm dist-tag add @midwayjs/socketio@3.7.0 latest
+tnpm dist-tag add @midwayjs/socketio@3.7.1 beta
+tnpm dist-tag add @midwayjs/static-file@3.7.0 latest
+tnpm dist-tag add @midwayjs/static-file@3.7.1 beta
+tnpm dist-tag add @midwayjs/swagger@3.7.0 latest
+tnpm dist-tag add @midwayjs/swagger@3.7.1 beta
+tnpm dist-tag add @midwayjs/tablestore@3.7.0 latest
+tnpm dist-tag add @midwayjs/tablestore@3.7.1 beta
+tnpm dist-tag add @midwayjs/typegoose@3.7.0 latest
+tnpm dist-tag add @midwayjs/typegoose@3.7.1 beta
+tnpm dist-tag add @midwayjs/typeorm@3.7.0 latest
+tnpm dist-tag add @midwayjs/typeorm@3.7.1 beta
+tnpm dist-tag add @midwayjs/upload@3.7.0 latest
+tnpm dist-tag add @midwayjs/upload@3.7.1 beta
+tnpm dist-tag add @midwayjs/validate@3.7.0 latest
+tnpm dist-tag add @midwayjs/validate@3.7.1 beta
+tnpm dist-tag add @midwayjs/version@3.7.0 latest
+tnpm dist-tag add @midwayjs/version@3.7.1 beta
+tnpm dist-tag add @midwayjs/view-ejs@3.7.0 latest
+tnpm dist-tag add @midwayjs/view-ejs@3.7.1 beta
+tnpm dist-tag add @midwayjs/view-nunjucks@3.7.0 latest
+tnpm dist-tag add @midwayjs/view-nunjucks@3.7.1 beta
+tnpm dist-tag add @midwayjs/view@3.7.0 latest
+tnpm dist-tag add @midwayjs/view@3.7.1 beta
+tnpm dist-tag add @midwayjs/express@3.7.0 latest
+tnpm dist-tag add @midwayjs/express@3.7.1 beta
+tnpm dist-tag add @midwayjs/koa@3.7.0 latest
+tnpm dist-tag add @midwayjs/koa@3.7.1 beta
+tnpm dist-tag add @midwayjs/web@3.7.0 latest
+tnpm dist-tag add @midwayjs/web@3.7.1 beta
+tnpm dist-tag add @midwayjs/ws@3.7.0 latest
+tnpm dist-tag add @midwayjs/ws@3.7.1 beta
+
+# Changes:
+
+#  - @midwayjs/fc-starter: 3.7.0 => 3.7.1
+#  - @midwayjs/axios: 3.7.0 => 3.7.1
+#  - @midwayjs/bootstrap: 3.7.0 => 3.7.1
+#  - @midwayjs/bull-board: 3.7.0 => 3.7.1
+#  - @midwayjs/bull: 3.7.0 => 3.7.1
+#  - @midwayjs/cache: 3.7.0 => 3.7.1
+#  - @midwayjs/captcha: 3.7.0 => 3.7.1
+#  - @midwayjs/casbin-redis-adapter: 3.7.0 => 3.7.1
+#  - @midwayjs/casbin-typeorm-adapter: 3.7.0 => 3.7.1
+#  - @midwayjs/casbin: 3.7.0 => 3.7.1
+#  - @midwayjs/code-dye: 3.7.0 => 3.7.1
+#  - @midwayjs/consul: 3.7.0 => 3.7.1
+#  - @midwayjs/cos: 3.7.0 => 3.7.1
+#  - @midwayjs/cross-domain: 3.7.0 => 3.7.1
+#  - @midwayjs/express-session: 3.7.0 => 3.7.1
+#  - @midwayjs/faas: 3.7.0 => 3.7.1
+#  - @midwayjs/grpc: 3.7.0 => 3.7.1
+#  - @midwayjs/http-proxy: 3.7.0 => 3.7.1
+#  - @midwayjs/i18n: 3.7.0 => 3.7.1
+#  - @midwayjs/info: 3.7.0 => 3.7.1
+#  - @midwayjs/jwt: 3.7.0 => 3.7.1
+#  - @midwayjs/kafka: 3.7.0 => 3.7.1
+#  - @midwayjs/mikro: 3.7.0 => 3.7.1
+#  - @midwayjs/mock: 3.7.0 => 3.7.1
+#  - @midwayjs/mongoose: 3.7.0 => 3.7.1
+#  - @midwayjs/oss: 3.7.0 => 3.7.1
+#  - @midwayjs/otel: 3.7.0 => 3.7.1
+#  - @midwayjs/passport: 3.7.0 => 3.7.1
+#  - @midwayjs/process-agent: 3.7.0 => 3.7.1
+#  - @midwayjs/prometheus-socket-io: 3.7.0 => 3.7.1
+#  - @midwayjs/prometheus: 3.7.0 => 3.7.1
+#  - @midwayjs/rabbitmq: 3.7.0 => 3.7.1
+#  - @midwayjs/redis: 3.7.0 => 3.7.1
+#  - @midwayjs/security: 3.7.0 => 3.7.1
+#  - @midwayjs/sequelize: 3.7.0 => 3.7.1
+#  - @midwayjs/session: 3.7.0 => 3.7.1
+#  - @midwayjs/socketio: 3.7.0 => 3.7.1
+#  - @midwayjs/static-file: 3.7.0 => 3.7.1
+#  - @midwayjs/swagger: 3.7.0 => 3.7.1
+#  - @midwayjs/tablestore: 3.7.0 => 3.7.1
+#  - @midwayjs/typegoose: 3.7.0 => 3.7.1
+#  - @midwayjs/typeorm: 3.7.0 => 3.7.1
+#  - @midwayjs/upload: 3.7.0 => 3.7.1
+#  - @midwayjs/validate: 3.7.0 => 3.7.1
+#  - @midwayjs/version: 3.7.0 => 3.7.1
+#  - @midwayjs/view-ejs: 3.7.0 => 3.7.1
+#  - @midwayjs/view-nunjucks: 3.7.0 => 3.7.1
+#  - @midwayjs/view: 3.7.0 => 3.7.1
+#  - @midwayjs/express: 3.7.0 => 3.7.1
+#  - @midwayjs/koa: 3.7.0 => 3.7.1
+#  - @midwayjs/web: 3.7.0 => 3.7.1
+#  - @midwayjs/ws: 3.7.0 => 3.7.1
