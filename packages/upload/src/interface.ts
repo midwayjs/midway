ca33489d5b181e676c2a23fd741480ed37d95b37
soar.gy <soar.gy@alibaba-inc.com>
2022-12-07 11:52:42 +0800

    fix: upload whitelist support null type

--- packages/upload/src/interface.ts
+++ packages/upload/src/interface.ts
@@ -5,7 +5,7 @@ export type UploadMode = 'stream' | 'file';
 export interface UploadOptions {
   mode?: UploadMode,
   fileSize?: string;   // Max file size (in bytes), default is `10mb`
-  whitelist?: string[]; // The white ext file names, default is `null`
+  whitelist?: string[] | null; // The white ext file names, default is `null`
   tmpdir?: string; // 临时文件目录
   cleanTimeout?: number; // 临时文件自动清理时间
   base64?: boolean; // 上传的body是否为base64，例如腾讯云的apigw
