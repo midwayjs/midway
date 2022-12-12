6019c6b13a60c59cf59d9e53e115c278b2ca9729
Harry Chen <czy88840616@gmail.com>
2022-12-12 21:11:41 +0800

    chore: clean code

--- packages/web/test/child.ts
+++ packages/web/test/child.ts
@@ -13,7 +13,7 @@ process.chdir(dir);
 startCluster({
   baseDir: dir,
   workers: 1,
-  port: 8080,
+  port: 8081,
   framework: join(__dirname, '../src'),
   typescript: true,
   require: 'ts-node/register'
--- packages/web/test/cluster.test.ts
+++ packages/web/test/cluster.test.ts
@@ -12,7 +12,7 @@ describe('/test/cluster.test.ts', () => {
     });
 
     it('test setHeader decorator', async () => {
-      const result = await createHttpClient('http://127.0.0.1:8080/set_header', {
+      const result = await createHttpClient('http://127.0.0.1:8081/set_header', {
         method: 'get',
         params: { name: 'harry' },
         dataType: 'text',
@@ -24,14 +24,14 @@ describe('/test/cluster.test.ts', () => {
     });
 
     it('test get status 204', async () => {
-      const result = await createHttpClient('http://127.0.0.1:8080/204', {
+      const result = await createHttpClient('http://127.0.0.1:8081/204', {
         dataType: 'text',
       });
       expect(result.status).toEqual(204);
     });
 
     it('test get method with return value', async () => {
-      const result = await createHttpClient('http://127.0.0.1:8080/', {
+      const result = await createHttpClient('http://127.0.0.1:8081/', {
         method: 'get',
         data: { name: 'harry' },
         dataType: 'text',
@@ -41,14 +41,14 @@ describe('/test/cluster.test.ts', () => {
     });
 
     it('test get method with redirect', async () => {
-      const result = await createHttpClient('http://127.0.0.1:8080/login', {
+      const result = await createHttpClient('http://127.0.0.1:8081/login', {
         dataType: 'text',
       });
       expect(result.status).toEqual(302);
     });
 
     it('test get data with ctx.body', async () => {
-      const result = await createHttpClient('http://127.0.0.1:8080/ctx-body', {
+      const result = await createHttpClient('http://127.0.0.1:8081/ctx-body', {
         dataType: 'text',
       });
       expect(result.data).toEqual('ctx-body');
