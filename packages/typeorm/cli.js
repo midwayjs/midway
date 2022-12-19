d7ff9100c5c01a5b6a9a3e3a9182978bb233473a
Harry Chen <czy88840616@gmail.com>
2022-12-19 14:10:29 +0800

    feat: support typeorm migrate cli

---
+++ packages/typeorm/cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+'use strict';
+
+const { join } = require('path');
+// eslint-disable-next-line node/no-unpublished-require
+const { CommandUtils } = require('typeorm/commands/CommandUtils');
+// eslint-disable-next-line node/no-unpublished-require
+const ImportUtils = require('typeorm/util/ImportUtils');
+// eslint-disable-next-line node/no-unpublished-require
+const { DataSource } = require('typeorm');
+
+CommandUtils.loadDataSource = async function (dataSourceFilePath) {
+  try {
+    let dataSourceFileExports = await ImportUtils.importOrRequireFile(
+      dataSourceFilePath
+    );
+    if (dataSourceFileExports[0] && dataSourceFileExports[0].default) {
+      dataSourceFileExports = dataSourceFileExports[0].default;
+      if (typeof dataSourceFileExports === 'function') {
+        dataSourceFileExports = dataSourceFileExports({
+          appDir: process.cwd(),
+          baseDir: join(process.cwd(), 'src'),
+        });
+      }
+      if (dataSourceFileExports['typeorm']) {
+        dataSourceFileExports = dataSourceFileExports['typeorm'];
+        dataSourceFileExports =
+          dataSourceFileExports.dataSource[
+            dataSourceFileExports['defaultClientName'] || 'default'
+          ];
+      }
+      return new DataSource(dataSourceFileExports);
+    }
+  } catch (err) {
+    throw new Error(
+      `Unable to open file: "${dataSourceFilePath}". ${err.message}`
+    );
+  }
+};
+
+// eslint-disable-next-line node/no-unpublished-require
+require('typeorm/cli');
