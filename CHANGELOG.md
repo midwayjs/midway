0702c088ad34d182d916bb33d11c7f80bf4b24bd
Harry Chen <czy88840616@gmail.com>
2022-12-20 20:16:13 +0800

    v3.9.6

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,17 @@
 # Change Log
 
 
+## v3.9.6 (2022-12-20)
+
+#### :bug: Bug Fix
+* `cross-domain`, `http-proxy`, `static-file`
+  * [#2592](https://github.com/midwayjs/midway/pull/2592) fix: static with cors ([@czy88840616](https://github.com/czy88840616))
+
+#### Committers: 1
+- Harry Chen ([@czy88840616](https://github.com/czy88840616))
+
+
+
 ## v3.9.5 (2022-12-20)
 
 #### :bug: Bug Fix
---
+++ scripts/rollback/rollback-3.9.6.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# timestamp: 1671538461850
+
+npm dist-tag add @midwayjs/cross-domain@3.9.1 latest
+npm dist-tag add @midwayjs/cross-domain@3.9.6 beta
+npm dist-tag add @midwayjs/http-proxy@3.9.1 latest
+npm dist-tag add @midwayjs/http-proxy@3.9.6 beta
+npm dist-tag add @midwayjs/static-file@3.9.1 latest
+npm dist-tag add @midwayjs/static-file@3.9.6 beta
+npm dist-tag add @midwayjs/version@3.9.5 latest
+npm dist-tag add @midwayjs/version@3.9.6 beta
+tnpm dist-tag add @midwayjs/cross-domain@3.9.1 latest
+tnpm dist-tag add @midwayjs/cross-domain@3.9.6 beta
+tnpm dist-tag add @midwayjs/http-proxy@3.9.1 latest
+tnpm dist-tag add @midwayjs/http-proxy@3.9.6 beta
+tnpm dist-tag add @midwayjs/static-file@3.9.1 latest
+tnpm dist-tag add @midwayjs/static-file@3.9.6 beta
+tnpm dist-tag add @midwayjs/version@3.9.5 latest
+tnpm dist-tag add @midwayjs/version@3.9.6 beta
+
+# Changes:
+
+#  - @midwayjs/cross-domain: 3.9.1 => 3.9.6
+#  - @midwayjs/http-proxy: 3.9.1 => 3.9.6
+#  - @midwayjs/static-file: 3.9.1 => 3.9.6
+#  - @midwayjs/version: 3.9.5 => 3.9.6
