05fa579b3421d0733cf8d5afd702788d147d9ea4
Harry Chen <czy88840616@gmail.com>
2023-01-30 01:16:45 +0800

    feat: support app enable ssl in dev mode

--- packages/mock/src/interface.ts
+++ packages/mock/src/interface.ts
@@ -3,6 +3,7 @@ import { IMidwayBootstrapOptions } from '@midwayjs/core';
 export interface MockAppConfigurationOptions extends IMidwayBootstrapOptions {
   cleanLogsDir?: boolean;
   cleanTempDir?: boolean;
+  ssl?: boolean;
 }
 
 export type ComponentModule = {
