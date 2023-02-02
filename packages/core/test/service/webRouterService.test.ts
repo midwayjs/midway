801f5346c1fe30343451a1c2f3e8931a8acee6a7
下雨就像弹钢琴 <work.lukai@gmail.com>
2023-02-03 00:45:04 +0800

    fix: RouterService match router method all (#2697)
    * fix: RouterService match router method all
    
    * fix: RouterService match router method all
    
    ---------
    
    Co-authored-by: 陆凯(萧迹) <lk195625@alibaba-inc.com>

--- packages/core/test/service/webRouterService.test.ts
+++ packages/core/test/service/webRouterService.test.ts
@@ -25,16 +25,27 @@ describe('/test/service/webRouterService.test.ts', function () {
       requestMethod: 'GET',
     });
     collector.addRouter(async (ctx) => {
+      return 'test all method';
+    } ,{
+      url: '/test/all/method',
+      requestMethod: 'ALL',
+    });
+    collector.addRouter(async (ctx) => {
       return 'hello world';
     }, {
       url: '/abc/dddd/*',
       requestMethod: 'GET',
     });
+   
     let routeInfo = await collector.getMatchedRouterInfo('/api', 'get');
     expect(routeInfo).toBeUndefined();
 
     routeInfo = await collector.getMatchedRouterInfo('/abc/dddd/efg', 'GET');
-    expect(routeInfo.url).toEqual('/abc/dddd/*');
+    expect(routeInfo?.url).toEqual('/abc/dddd/*');
+
+    routeInfo = await collector.getMatchedRouterInfo('/test/all/method', 'POST');
+    expect(routeInfo?.url).toEqual('/test/all/method');
+    expect(routeInfo?.requestMethod.toUpperCase()).toEqual('ALL');
 
     collector.addRouter(async (ctx) => {
       return 'hello world';
