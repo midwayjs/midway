026b0225222ee72f5f32fb50f0b39dda098accf2
Harry Chen <czy88840616@gmail.com>
2023-02-06 21:41:12 +0800

    feat: add web param optional (#2708)
    * feat: add web param optional
    
    * feat: valid support pipes

--- packages/validate/src/decorator/valid.ts
+++ packages/validate/src/decorator/valid.ts
@@ -1,16 +1,25 @@
-import { createCustomParamDecorator } from '@midwayjs/core';
+import { createCustomParamDecorator, PipeUnionTransform } from '@midwayjs/core';
 import { AnySchema } from 'joi';
 import { VALID_KEY } from '../constants';
 import { DecoratorValidPipe } from '../pipe';
 
-export function Valid(schema?: AnySchema<any>) {
+export function Valid(
+  schemaOrPipes?: AnySchema<any> | PipeUnionTransform[],
+  pipes?: PipeUnionTransform[]
+) {
+  if (Array.isArray(schemaOrPipes)) {
+    pipes = schemaOrPipes;
+    schemaOrPipes = undefined;
+  } else {
+    pipes = pipes || [];
+  }
   return createCustomParamDecorator(
     VALID_KEY,
     {
-      schema,
+      schema: schemaOrPipes,
     },
     {
-      pipes: [DecoratorValidPipe],
+      pipes: [DecoratorValidPipe, ...pipes],
     }
   );
 }
