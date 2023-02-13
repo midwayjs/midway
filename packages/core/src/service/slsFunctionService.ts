7839b35f4d4e341d1d803c9344611b52a4b8c7d4
Harry Chen <czy88840616@gmail.com>
2023-02-13 15:38:40 +0800

    fix: func name set (#2732)

--- packages/core/src/service/slsFunctionService.ts
+++ packages/core/src/service/slsFunctionService.ts
@@ -131,14 +131,14 @@ export class MidwayServerlessFunctionService extends MidwayWebRouterService {
           ) || {};
         const functionName =
           functionMeta['functionName'] ??
-          webRouter['functionName'] ??
+          webRouter?.['metadata']?.['functionName'] ??
           createFunctionName(module, webRouter['methodName']);
         data.functionName = functionName;
         data.functionTriggerName = webRouter['type'];
         data.functionTriggerMetadata = webRouter['metadata'];
         data.functionMetadata = {
-          ...functionMeta,
           functionName,
+          ...functionMeta,
         };
         this.checkDuplicateAndPush(prefix, data);
       } else {
