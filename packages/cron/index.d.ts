34929b9c66c7cc4e3a7e810f10b5ab9a49cfea9c
Harry Chen <czy88840616@gmail.com>
2023-02-26 11:48:21 +0800

    feat: add cron

--- packages/cron/index.d.ts
+++ packages/cron/index.d.ts
@@ -1,8 +1,8 @@
-import { CORSOptions } from './dist/index';
+import { CronOptions } from './dist/index';
 export * from './dist/index';
 
 declare module '@midwayjs/core/dist/interface' {
   interface MidwayConfig {
-    cron?: Partial<CORSOptions>;
+    cron?: Partial<CronOptions>;
   }
 }
