6ba07e998b2cd5d7afe965be684fc17835a470e1
Harry Chen <czy88840616@gmail.com>
2023-03-25 21:54:15 +0800

    feat: add cron component (#2771)
    * feat: add cron
    
    * feat: add cron
    
    * feat: add cron
    
    * feat: add cron
    
    * fix: lint
    
    * fix: remove tick args

---
+++ packages/cron/test/fixtures/base-app/src/configuration.ts
@@ -0,0 +1,24 @@
+import { Configuration, Inject } from '@midwayjs/core';
+import * as cron from '../../../../src';
+import { HelloTask } from './task/hello.task';
+import { InjectJob, CronJob } from '../../../../src';
+import * as assert from 'assert';
+
+@Configuration({
+  imports: [
+    cron
+  ],
+})
+export class ContainerConfiguration {
+  @Inject()
+  framework: cron.Framework;
+
+  @InjectJob(HelloTask)
+  helloTask: CronJob;
+
+  async onServerReady() {
+    console.log('in server ready ' + Date.now());
+    this.framework.getJob(HelloTask).start();
+    assert(this.helloTask === this.framework.getJob(HelloTask));
+  }
+}
---
+++ packages/cron/test/fixtures/base-app/src/task/hello.task.ts
@@ -0,0 +1,34 @@
+import { App, Inject, sleep } from '@midwayjs/core';
+import { Job, IJob, Context, Application } from '../../../../../src/';
+let idx = 0;
+
+@Job('HelloTask', {
+  cronTime: '*/2 * * * * *',
+  // start: true,
+  // runOnInit: true,
+})
+export class HelloTask implements IJob {
+  @App()
+  app: Application;
+
+  @Inject()
+  ctx: Context;
+
+  async onTick() {
+    idx++;
+    console.log('task', idx);
+    this.app.setAttr(`task`, idx);
+
+    if (idx === 1) {
+      this.ctx.job.stop();
+    }
+
+    await sleep(2000);
+
+    console.log('task end');
+  }
+
+  async onComplete() {
+    console.log('task complete');
+  }
+}
