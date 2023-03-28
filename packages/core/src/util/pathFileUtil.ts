d405730d110049f4ec6926858accea381530309b
soar.gy <soar.gy@alibaba-inc.com>
2023-03-28 17:15:20 +0800

    chore: more debug info

--- packages/core/src/util/pathFileUtil.ts
+++ packages/core/src/util/pathFileUtil.ts
@@ -30,3 +30,30 @@ export const PathFileUtil = {
   isPathEqual,
   getFileContentSync,
 };
+
+export function getModuleRequirePathList(moduleName: string): string[] {
+  const moduleNameList = [moduleName, moduleName.replace(/\//g, '_')];
+  let moduleNameMap = {};
+  const modulePathList = [];
+  Object.keys(require.cache || {}).forEach(moduleName => {
+    let moduleIndex = -1;
+    for (const moduleName of moduleNameList) {
+      const index = moduleName.indexOf(moduleName);
+      if (index !== -1) {
+        moduleIndex = index;
+        break;
+      }
+    }
+    if (moduleIndex === -1) {
+      return;
+    }
+    const modulePath = moduleName.slice(0, moduleIndex);
+    if (moduleNameMap[modulePath]) {
+      return;
+    }
+    moduleNameMap[modulePath] = true;
+    modulePathList.push(modulePath);
+  });
+  moduleNameMap = undefined;
+  return modulePathList;
+}
