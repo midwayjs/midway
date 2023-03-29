9c7b3a7067b5b485dc0f7dc32b2b4d92e75337cd
waiting <1661926154@qq.com>
2023-03-29 23:47:26 +0800

    chore(core): remove value undefined from JoinPoint['proceedIsAsyncFunction']

--- packages/core/src/interface.ts
+++ packages/core/src/interface.ts
@@ -279,7 +279,7 @@ export interface JoinPoint {
   target: any;
   args: any[];
   proceed?(...args: any[]): any;
-  proceedIsAsyncFunction?: boolean | undefined;
+  proceedIsAsyncFunction?: boolean;
 }
 
 export interface AspectMetadata {
