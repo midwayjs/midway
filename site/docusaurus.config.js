f9670f136b04ecbcc876c1fcf5d784d4443bb699
Harry Chen <czy88840616@gmail.com>
2023-04-04 13:20:25 +0800

    chore: remove typings

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -4,7 +4,6 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-/** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Midway',
   tagline: 'Midway is a fullstack framework for web & Serverless',
