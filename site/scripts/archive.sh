6706dff75b032a8c15fb08f6360ec40161e5b91c
Harry Chen <czy88840616@gmail.com>
2023-04-04 15:20:41 +0800

    chore: use zip replace deploy zip

--- site/scripts/archive.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# 创建 build 目录
-mkdir -p build
-
-# 压缩 docs, i18n 和 blog 等目录，并将压缩文件放入 build 目录
-zip -r build/document_archive.zip docs i18n blog versioned_docs versioned_sidebars
-
-echo "All files have been compressed to $zip_name"
