f59e5dbbbc678851caebdda881b0fffc77d54fc4
sumy <sumyggsun@gmail.com>
2023-06-12 10:44:27 +0800

    feat(code-dye): outputType-html support show not end async call

--- packages/code-dye/src/html.ts
+++ packages/code-dye/src/html.ts
@@ -1,5 +1,25 @@
+function getInfoResult(info) {
+  if (info.end) {
+    return `<div class="infoBtn">返回值<div class="infoCard"><pre>${JSON.stringify(
+      info.end.result,
+      null,
+      2
+    )}</pre></div></div>`;
+  } else {
+    return '<div class="infoBtn">未结束</div>';
+  }
+}
+
+function getInfoTimeUse(info) {
+  if (info.end) {
+    return info.end.time - info.start.time;
+  } else {
+    return new Date().getTime() - info.start.time;
+  }
+}
+
 export const foreach = (info, start, pi, level) => {
-  const timeUse = info.end.time - info.start.time;
+  const timeUse = getInfoTimeUse(info);
   const timeDiff = info.start.time - start;
   const paths = info.paths.filter(path => {
     return !path.startsWith('/');
@@ -14,19 +34,15 @@ export const foreach = (info, start, pi, level) => {
             null,
             2
           )}</pre></div></div>
-          <div class="infoBtn">返回值<div class="infoCard"><pre>${JSON.stringify(
-            info.end.result,
-            null,
-            2
-          )}</pre></div></div>
+          ${getInfoResult(info)}
         </div>
       </div>
       <div class="timeContainer">
-        <div class="time" style="width:${timeUse / pi}px;left: ${
-    timeDiff / pi
-  }px"><div class="timeValue">${timeUse} ms</div></div>
+        <div class="time ${info.end ? 'end' : 'not-end'}" style="width:${
+    timeUse / pi
+  }px;left: ${timeDiff / pi}px"><div class="timeValue">${timeUse} ms</div></div>
       </div>
-      
+
     </div>
     <div class="child">
       <div class="childLine" style="left: ${(level - 1) * 24 + 12}px"></div>
@@ -39,12 +55,27 @@ export const foreach = (info, start, pi, level) => {
   `;
 };
 
-export const toHTML = info => {
-  const timeDiff = info.end.time - info.start.time;
+export const callChain = call => {
+  const timeDiff = call.end.time - call.start.time;
   let pi = timeDiff / 600;
   if (pi < 1) {
     pi = 1;
   }
+  return `
+  <div>
+    <div>调用链路总耗时 ${timeDiff}ms<div>
+    <div>调用结果:<br /><pre>${JSON.stringify(
+      call.end.result,
+      null,
+      2
+    )}</pre></div>
+    <hr />
+    <div style="padding: 12px;">${foreach(call, call.start.time, pi, 1)}</div>
+  </div>
+  `;
+};
+
+export const toHTML = info => {
   return `
   <title>Midway CodeDye</title>
   <meta charset="utf-8" />
@@ -89,7 +120,10 @@ export const toHTML = info => {
     background-color: #66c;
     border-radius: 3px;
   }
-  
+  .time.not-end {
+    background-color: #ddd;
+  }
+
   .timeValue {
     position: absolute;
     line-height: 16px;
@@ -131,15 +165,7 @@ export const toHTML = info => {
   }
   </style>
   <h1>Midway CodeDye</h1><hr />
-  <div>调用链路总耗时 ${timeDiff}ms<div>
-  <div>调用结果:<br /><pre>${JSON.stringify(
-    info.end.result,
-    null,
-    2
-  )}</pre></div>
-  <hr />
-  <div style="padding: 12px;">
-  ${foreach(info, info.start.time, pi, 1)}</div>
+  ${info.map(call => callChain(call)).join('')}
   <hr /><a href="https://www.midwayjs.org/">Powered by Midway.js</a>
   `;
 };
