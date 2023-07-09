29a762f0f26c05d7ef56d8bdcdd7e52bac0acbbc
Harry Chen <czy88840616@gmail.com>
2023-07-09 14:20:30 +0800

    fix: use npm8 under node14

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 set -e
 
-# 获取node版本
-NODE_VERSION=$(node -v | cut -dv -f2 | cut -d. -f1)
-
-# 如果 为 node 14，则重新安装高版本的 npm，并重新安装
-if [ "$NODE_VERSION" = "14" ]; then
-  echo 'npm versin is 14, reinstall npm'
-  npm install -g npm@latest
-
-  echo 'npm version'
-  npm -v
-
-  rm -rf node_modules
-  rm -rf package-lock.json
-  npm install
-fi
-
 # cp ./README.md ./packages/midway/README.md
 lerna exec -- rm -rf ./dist
 lerna run build --concurrency=4
