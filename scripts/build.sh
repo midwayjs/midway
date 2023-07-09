67aa3ea1d95165fc5adaf97d7439ae29bf8e98d9
Harry Chen <czy88840616@gmail.com>
2023-07-09 12:58:00 +0800

    chore: update

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,10 @@ NODE_VERSION=$(node -v | cut -dv -f2 | cut -d. -f1)
 
 # 如果 为 node 14，则重新安装高版本的 npm，并重新安装
 if [ "$NODE_VERSION" = "14" ]; then
+  echo 'npm versin is 14, reinstall npm'
   npm install -g npm@latest
+  rm -rf node_modules
+  rm -rf package-lock.json
   npm install
 fi
 
