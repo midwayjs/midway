baac34f967646e26664cde5b1a8e7a974d1fa69f
Harry Chen <czy88840616@gmail.com>
2023-07-09 12:51:45 +0800

    fix: node 14 support workspace

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# 获取node版本
+NODE_VERSION=$(node -v | cut -dv -f2 | cut -d. -f1)
+
+# 如果 为 node 14，则重新安装高版本的 npm，并重新安装
+if [ "$NODE_VERSION" = "14" ]; then
+  npm install -g npm@latest
+  npm install
+fi
+
 # cp ./README.md ./packages/midway/README.md
 lerna exec -- rm -rf ./dist
 lerna run build --concurrency=4
