0987493bd5648755009138edd24af8f6a787eab6
Cong Liu <cong.liu@dolearning.net>
2023-07-27 11:38:52 +0800

    feat: support all http methods

--- packages/core/src/util/httpclient.ts
+++ packages/core/src/util/httpclient.ts
@@ -7,7 +7,6 @@ import { MidwayUtilHttpClientTimeoutError } from '../error';
 const debug = debuglog('request-client');
 const URL = url.URL;
 
-type MethodType = 'GET' | 'POST';
 type MimeType = 'text' | 'json' | undefined;
 const mimeMap = {
   text: 'application/text',
@@ -16,7 +15,6 @@ const mimeMap = {
 };
 
 interface IOptions extends https.RequestOptions {
-  method?: MethodType;
   headers?: any;
   contentType?: MimeType;
   dataType?: MimeType;
@@ -44,7 +42,7 @@ export async function makeHttpRequest<ResType>(
   debug(`request '${url}'`);
   const whatwgUrl = new URL(url);
   const client = whatwgUrl.protocol === 'https:' ? https : http;
-  options.method = (options.method || 'GET').toUpperCase() as MethodType;
+  options.method = (options.method || 'GET').toUpperCase();
   const {
     contentType,
     dataType,
