b30b1886afd05f7d3f2e692fac41a4d62e15e14e
Cong Liu <ghostoy@users.noreply.github.com>
2023-08-05 08:40:51 +0800

    feat: expose native OSS from package (#3132)

--- packages/oss/src/interface.ts
+++ packages/oss/src/interface.ts
@@ -1,5 +1,7 @@
 import * as OSS from 'ali-oss';
 
+export { OSS };
+
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 export type XOR<T, U> = (Without<T, U> & U) | (Without<U, T> & T);
 
