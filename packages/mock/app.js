55695fca243e1d0fb77d407af5486384a0116754
flyingCrp <alexchen1875@gmail.com>
2024-01-05 10:52:37 +0800

    fix(Development environment under windows): After switching from cli to tsc, the development environment application under windows stops abnormally.

--- packages/mock/app.js
+++ packages/mock/app.js
@@ -2,6 +2,30 @@ const { createApp, close } = require('./dist');
 const { join } = require('path');
 
 (async () => {
+  function onSignal() {
+    close(app).then(() => {
+      process.exit(0);
+    });
+  }
+
+  if (process.platform === 'win32') {
+    const rl = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    let isShuttingDown = false;
+    rl.on('SIGINT', async () => {
+      rl.close();
+      if (isShuttingDown) {
+        console.error('Forcing shutdown, bye.');
+        process.exit();
+      } else {
+        isShuttingDown = true;
+        rl.close();
+        process.emit('SIGINT');
+      }
+    });
+  }
   process.env.MIDWAY_TS_MODE = 'false';
   // 查找 process.argv 中的 --port 参数
   const portIndex = process.argv.findIndex((item) => item === '--port');
@@ -24,10 +48,4 @@ const { join } = require('path');
     title: 'server-ready',
     port: process.env.MIDWAY_HTTP_PORT,
   });
-
-  function onSignal() {
-    close(app).then(() => {
-      process.exit(0);
-    });
-  }
 })();
