98011cd45f02f67d2ef0fdd2fcdb479523949420
Harry Chen <czy88840616@gmail.com>
2024-01-14 12:31:45 +0800

    fix: swagger-ui-dist require missing

--- packages/swagger/src/config/config.default.ts
+++ packages/swagger/src/config/config.default.ts
@@ -1,15 +1,16 @@
 import { SwaggerOptions } from '../interfaces';
 import { renderSwaggerUIRemote, renderSwaggerUIDist } from '../ui/render';
 import { safeRequire } from '@midwayjs/core';
-const { getAbsoluteFSPath } = safeRequire('swagger-ui-dist');
+const swaggerDist = safeRequire('swagger-ui-dist');
 
 export const swagger: SwaggerOptions = {
   title: 'My Project',
   description: 'This is a swagger-ui for midwayjs project',
   version: '1.0.0',
   swaggerPath: '/swagger-ui',
-  swaggerUIRender: getAbsoluteFSPath
-    ? renderSwaggerUIDist
-    : renderSwaggerUIRemote,
+  swaggerUIRender:
+    swaggerDist && swaggerDist.getAbsoluteFSPath
+      ? renderSwaggerUIDist
+      : renderSwaggerUIRemote,
   swaggerUIRenderOptions: {},
 };
