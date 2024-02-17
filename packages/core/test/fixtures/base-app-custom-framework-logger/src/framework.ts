df8e83a43d99ce468537623d1567557178711b17
Harry Chen <czy88840616@gmail.com>
2024-02-17 22:32:45 +0800

    fix: ctx get logger with custom logger

---
+++ packages/core/test/fixtures/base-app-custom-framework-logger/src/framework.ts
@@ -0,0 +1,17 @@
+import { BaseFramework, Framework, IMidwayBootstrapOptions } from '../../../../src';
+
+@Framework()
+export class CustomFramework extends BaseFramework<any, any, any> {
+  applicationInitialize(options: IMidwayBootstrapOptions) {
+    this.app = {};
+  }
+
+  configure(options: any) {
+    return this.configService.getConfiguration('custom');
+  }
+
+  run(): Promise<void> {
+    this.app.getCoreLogger().info('run custom framework');
+    return Promise.resolve(undefined);
+  }
+}
