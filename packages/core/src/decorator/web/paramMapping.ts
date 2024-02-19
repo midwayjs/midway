cf15290f4938b637d17694340e91583dbe6db756
142vip <fairy@2925.com>
2024-02-19 18:02:08 +0800

    feat(core):  createRequestParamDecorator default support throw error

--- packages/core/src/decorator/web/paramMapping.ts
+++ packages/core/src/decorator/web/paramMapping.ts
@@ -68,6 +68,9 @@ export const createRequestParamDecorator = function (
       pipes: pipesOrOptions as Array<PipeUnionTransform>,
     };
   }
+  if(pipesOrOptions.throwError==null)
+    pipesOrOptions.throwError=true
+
   return createParamMapping(RouteParamTypes.CUSTOM)(transform, pipesOrOptions);
 };
 
