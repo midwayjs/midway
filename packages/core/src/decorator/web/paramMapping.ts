656a7093977bb6bef12b3bb0787eb045dd4c78a5
142vip <fairy@2925.com>
2024-02-19 21:27:33 +0800

    chore: update

--- packages/core/src/decorator/web/paramMapping.ts
+++ packages/core/src/decorator/web/paramMapping.ts
@@ -68,7 +68,7 @@ export const createRequestParamDecorator = function (
       pipes: pipesOrOptions as Array<PipeUnionTransform>,
     };
   }
-  if(pipesOrOptions.throwError===undefined)
+  if(pipesOrOptions.throwError == null)
     pipesOrOptions.throwError=true
 
   return createParamMapping(RouteParamTypes.CUSTOM)(transform, pipesOrOptions);
