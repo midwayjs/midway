cfc2df1b824db84101b9d29f93e962466d379795
Harry Chen <czy88840616@gmail.com>
2024-03-01 23:19:18 +0800

    chore: use Axios for interface export

--- packages/axios/src/index.ts
+++ packages/axios/src/index.ts
@@ -1,3 +1,13 @@
+import axios from 'axios';
+import * as Axios from 'axios';
+
 export { AxiosConfiguration as Configuration } from './configuration';
 export * from './interface';
 export * from './serviceManager';
+export {
+  /**
+   * @deprecated Use `Axios` directly
+   */
+  axios,
+};
+export { Axios };
