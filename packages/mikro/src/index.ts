b0bdb23fac11c5b809f202ea5254545b4f346165
Harry Chen <czy88840616@gmail.com>
2024-03-16 19:11:52 +0800

    chore: update request content middleware and use default source name for context name of mirko (#3708)

--- packages/mikro/src/index.ts
+++ packages/mikro/src/index.ts
@@ -1,5 +1,4 @@
 export { MikroConfiguration as Configuration } from './configuration';
 export * from './decorator';
 export * from './interface';
-export * from './mikro.middleware';
 export * from './dataSourceManager';
