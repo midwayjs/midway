666442e3b736eb8d6ee9d1f2609346df8997ca7b
Harry Chen <czy88840616@gmail.com>
2024-03-25 00:11:51 +0800

    fix: enable console transport in serverless env

--- packages/faas/src/config.default.ts
+++ packages/faas/src/config.default.ts
@@ -1,15 +1,31 @@
-export const midwayLogger = {
-  default: {
-    enableFile: false,
-    enableError: false,
-    format: (info: any) => {
-      const requestId =
-        info.ctx?.['originContext']?.['requestId'] ??
-        info.ctx?.['originContext']?.['request_id'] ??
-        '';
-      return `${new Date().toISOString()} ${requestId} [${info.level}] ${
-        info.message
-      }`;
+import { MidwayAppInfo } from '@midwayjs/core';
+
+export const isDevelopmentEnvironment = env => {
+  return ['local', 'test', 'unittest'].includes(env);
+};
+
+export default (appInfo: MidwayAppInfo) => ({
+  midwayLogger: {
+    default: {
+      // for logger v2
+      enableConsole: true,
+      enableFile: false,
+      enableError: false,
+      transports: {
+        // for logger v3
+        console: {
+          autoColors: isDevelopmentEnvironment(appInfo.env),
+        },
+      },
+      format: (info: any) => {
+        const requestId =
+          info.ctx?.['originContext']?.['requestId'] ??
+          info.ctx?.['originContext']?.['request_id'] ??
+          '';
+        return `${new Date().toISOString()} ${requestId} [${info.level}] ${
+          info.message
+        }`;
+      },
     },
   },
-};
+});
