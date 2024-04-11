3299573f988700eb0bd844583cb155e8eee99e11
Harry Chen <czy88840616@gmail.com>
2024-04-12 00:39:52 +0800

    fix: socket with regexp namespace

---
+++ packages/socketio/test/fixtures/base-app-namespace-regexp/src/socket/api.ts
@@ -0,0 +1,37 @@
+import {
+  Inject,
+  OnWSConnection,
+  OnWSDisConnection,
+  OnWSMessage,
+  Provide,
+  WSController,
+  WSEmit,
+} from '@midwayjs/core';
+import { UserService } from '../service/user';
+import { Context } from '../../../../../src';
+
+@Provide()
+@WSController(/^\/abc.+/)
+export class APIController {
+  @Inject()
+  ctx: Context;
+
+  @Inject()
+  userService: UserService;
+
+  @OnWSConnection()
+  init() {
+    console.log(`namespace / got a connection ${this.ctx.id}`);
+  }
+
+  @OnWSMessage('my')
+  @WSEmit('ok')
+  async gotMyMessage(data1, data2, data3) {
+    return { name: 'harry', result: data1 + data2 + data3 };
+  }
+
+  @OnWSDisConnection()
+  disconnect(reason: string) {
+    console.log(this.ctx.id + ' disconnect ' + reason);
+  }
+}
