09fc11ee3df3f4cd09d7822227e06f7afb0a089f
Harry Chen <czy88840616@gmail.com>
2024-05-06 21:47:42 +0800

    feat: support dynamic create mqtt consumer (#3699)
    (cherry picked from commit 107539abb3157ad67454144a57d6f66f7d566034)

--- packages/mqtt/test/index.test.ts
+++ packages/mqtt/test/index.test.ts
@@ -1,7 +1,13 @@
-import { createApp, close } from '@midwayjs/mock';
-import { sleep } from '@midwayjs/core';
+import { close, createApp, createLightApp } from '@midwayjs/mock';
+import { IMidwayApplication, Provide, Scope, ScopeEnum, sleep } from '@midwayjs/core';
+import { Framework, IMqttSubscriber, Mqtt, MqttProducerFactory } from '../src';
 
 describe('/test/index.test.ts', () => {
+
+  it('should test Mqtt export ', () => {
+    expect(Mqtt).toBeDefined();
+  });
+
   it('should test subscribe topic and send message', async () => {
     // create app and got data
     const app = await createApp('base-app');
@@ -14,6 +20,50 @@ describe('/test/index.test.ts', () => {
     // create app and got data
     const app = await createApp('base-app-no-pub-sub');
     await sleep();
+    await close(app);
+  });
+
+  it('should test dynamic create mqtt subscribe case', async () => {
+    let app: IMidwayApplication;
+    @Provide()
+    @Scope(ScopeEnum.Request)
+    class TestSubscriber implements IMqttSubscriber {
+      async subscribe() {
+        app.setAttr('subscribe', true);
+      }
+    }
+    app = await createLightApp({
+      imports: [
+        require('../src')
+      ],
+      preloadModules: [
+        TestSubscriber
+      ]
+    });
+    await (app.getFramework() as Framework).createSubscriber({
+      host: 'test.mosquitto.org',
+      port: 1883,
+    }, {
+      topicObject: 'test_midway_dynamic',
+    }, TestSubscriber, 'test');
+
+    await sleep();
+
+    // send
+    const producerService = await app.getApplicationContext().getAsync(MqttProducerFactory);
+    const producer = await producerService.createInstance({
+      host: 'test.mosquitto.org',
+      port: 1883,
+    }, 'test');
+
+    producer.publish('test_midway_dynamic', 'hello world', {
+      qos: 2
+    });
+
+    await sleep();
+
+    expect(app.getAttr('subscribe')).toBe(true);
+
     await close(app);
   });
 });
