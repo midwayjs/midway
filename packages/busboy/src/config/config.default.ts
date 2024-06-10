b4a5cd3c95b2eb41cf5b4a596d3d46ccd777e8b6
Harry Chen <czy88840616@gmail.com>
2024-06-10 15:33:26 +0800

    fix: build

--- packages/busboy/src/config/config.default.ts
+++ packages/busboy/src/config/config.default.ts
@@ -7,8 +7,7 @@ export const upload: UploadOptions = {
   mode: 'file',
   fileSize: '10mb',
   whitelist: uploadWhiteList,
-  tmpdir: join(tmpdir(), 'midway-upload-files'),
+  tmpdir: join(tmpdir(), 'midway-busboy-files'),
   cleanTimeout: 5 * 60 * 1000,
   base64: false,
-  enable: true,
 };
