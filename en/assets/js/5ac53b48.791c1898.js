2a300bc627db8141b1c3073ce94e9f711441be10
czy88840616 <czy88840616@users.noreply.github.com>
2024-07-26 03:40:04 +0000

    Deploy website - based on 9edcf6fd9e85f81bb83e8967c17cd1cb1f164020

---
+++ en/assets/js/5ac53b48.791c1898.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[11715],{73852:e=>{e.exports=JSON.parse('{"permalink":"/en/changelog","page":1,"postsPerPage":20,"totalPages":21,"totalCount":408,"nextPage":"/en/changelog/page/2","blogDescription":"Keep yourself up-to-date about new features in every release","blogTitle":"Midway changelog"}')}}]);
