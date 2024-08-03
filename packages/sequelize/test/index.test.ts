44ee58faf8b7ca932488a67bfd7efce6bfb68541
Harry Chen <czy88840616@gmail.com>
2024-08-03 12:28:35 +0800

    fix: remove authenticate when create data source (#3993)
    * fix: remove authenticate when create data source
    
    * test: add case
    
    * chore: set validateConnection true
    
    (cherry picked from commit 7dec40352110244edba11b05ac327a040d45fd93)

--- packages/sequelize/test/index.test.ts
+++ packages/sequelize/test/index.test.ts
@@ -1,8 +1,9 @@
-import { createApp, close } from '@midwayjs/mock';
+import { createApp, close, createLightApp } from '@midwayjs/mock';
 import { Framework, IMidwayKoaApplication } from '@midwayjs/koa';
 import { join } from 'path';
 import { existsSync, unlinkSync } from 'fs';
 import { UserService } from './fixtures/sequelize-new/src/service/user';
+import * as sequelize from '../src';
 
 function cleanFile(file) {
   if (existsSync(file)) {
@@ -11,48 +12,106 @@ function cleanFile(file) {
 }
 
 describe('/test/index.test.ts', () => {
-  let app: IMidwayKoaApplication;
-  beforeAll(async () => {
-    cleanFile(join(__dirname, 'fixtures/sequelize-new', 'database.sqlite'));
-    app = await createApp(
-      join(__dirname, 'fixtures', 'sequelize-new'),
-      {},
-      Framework
-    );
+  describe('test sequelize with new decorator', () => {
+    let app: IMidwayKoaApplication;
+    beforeAll(async () => {
+      cleanFile(join(__dirname, 'fixtures/sequelize-new', 'database.sqlite'));
+      app = await createApp(
+        join(__dirname, 'fixtures', 'sequelize-new'),
+        {},
+        Framework
+      );
+    });
+
+    afterAll(async () => {
+      await close(app);
+    });
+    it('list user service ', async () => {
+      const userService: UserService = await app
+        .getApplicationContext()
+        .getAsync(UserService);
+      const result = await userService.list();
+      expect(result.length).toBe(0);
+    });
+
+    it('list hello service ', async () => {
+      const userService: UserService = await app
+        .getApplicationContext()
+        .getAsync(UserService);
+      const result = await userService.listHello();
+      expect(result.length).toBe(0);
+    });
+
+    it('add and delete', async () => {
+      const userService: UserService = await app
+        .getApplicationContext()
+        .getAsync(UserService);
+      await userService.add();
+      let result = await userService.list();
+      expect(result.length).toBe(1);
+
+      result = await userService.listWithModel();
+      expect(result.length).toBe(1);
+
+      await userService.delete();
+      result = await userService.list();
+      expect(result.length).toBe(0);
+    });
   });
 
-  afterAll(async () => {
-    await close(app);
-  });
-  it('list user service ', async () => {
-    const userService: UserService = await app
-      .getApplicationContext()
-      .getAsync(UserService);
-    const result = await userService.list();
-    expect(result.length).toBe(0);
-  });
+  describe('test connection options', () => {
+    it('should test connection fail and create client success', async () => {
+      const app = await createLightApp({
+        imports: [
+          sequelize,
+        ],
+        globalConfig: {
+          sequelize: {
+            dataSource: {
+              default: {
+                database: 'test',
+                username: 'test',
+                password: 'test',
+                dialect: 'mysql',
+                host: '',
+                port: 3306,
+              },
+            },
+            validateConnection: false,
+          },
+        },
+      });
 
-  it('list hello service ', async () => {
-    const userService: UserService = await app
-      .getApplicationContext()
-      .getAsync(UserService);
-    const result = await userService.listHello();
-    expect(result.length).toBe(0);
-  });
+      const manager = await app.getApplicationContext().getAsync(sequelize.SequelizeDataSourceManager);
+      expect(manager.getAllDataSources().size).toBe(1);
+    });
 
-  it('add and delete', async () => {
-    const userService: UserService = await app
-      .getApplicationContext()
-      .getAsync(UserService);
-    await userService.add();
-    let result = await userService.list();
-    expect(result.length).toBe(1);
-
-    result = await userService.listWithModel();
-    expect(result.length).toBe(1);
-
-    await userService.delete();
-    result = await userService.list();
-    expect(result.length).toBe(0);
-  });
+    it('should test connection fail and create client fail', async () => {
+      let err: Error;
+      try {
+        await createLightApp({
+          imports: [
+            sequelize,
+          ],
+          globalConfig: {
+            sequelize: {
+              dataSource: {
+                default: {
+                  database: 'test',
+                  username: 'test',
+                  password: 'test',
+                  dialect: 'mysql',
+                  host: '',
+                  port: 3306,
+                },
+              },
+            },
+          },
+        });
+      } catch (e) {
+        err = e;
+      }
+      expect(err.name).toEqual('MidwayCommonError');
+    });
+  })
 });
