15063c26e3bd9fcb24055f0fc503faf4eacb31e8
Harry Chen <czy88840616@gmail.com>
2024-09-18 20:44:06 +0800

    fix: test

--- packages/busboy/test/express.test.ts
+++ packages/busboy/test/express.test.ts
@@ -61,22 +61,19 @@ describe('test/express.test.ts', function () {
       const pdfPath = join(__dirname, 'fixtures/test.pdf');
       const request = await createHttpRequest(app);
       const stat = statSync(pdfPath);
-      await request.post('/upload')
+      const response = await request.post('/upload')
         .field('name', 'form')
         .field('name2', 'form2')
         .attach('file', pdfPath)
-        .attach('file2', pdfPath)
-        .expect(200)
-        .then(async response => {
-          assert(response.body.files.length === 2);
-          // assert(response.body.files[0].fieldName === 'file');
-          // assert(response.body.files[1].fieldName === 'file2');
-          assert(response.body.files[1].mimeType === 'application/pdf');
-          assert(response.body.fields.name === 'form');
-          assert(response.body.fields.name2 === 'form2');
-          const file1Stat = statSync(response.body.files[0].data);
-          assert(file1Stat.size && file1Stat.size === stat.size);
-        });
+        .attach('file2', pdfPath);
+
+      expect(response.status).toBe(200);
+      expect(response.body.files.length).toBe(2);
+      expect(response.body.files[1].mimeType).toBe('application/pdf');
+      expect(response.body.fields.name).toBe('form');
+      expect(response.body.fields.name2).toBe('form2');
+      const file1Stat = statSync(response.body.files[0].data);
+      expect(file1Stat.size).toBe(stat.size);
     });
 
     it('upload file ignore path', async () => {
