044c5e16d299dfc14cdef5ed593ad73da65f0b31
Harry Chen <czy88840616@gmail.com>
2024-09-23 18:22:42 +0800

    fix: ensure compatibility with Swagger response schema and fix the parsing of enum types (#4083)
    * fix: swagger schema and enum value
    
    * fix: build
    
    * fix: test
    
    (cherry picked from commit 1dc68035c4fef0f39bd735691e0cdde8f920e63a)

--- packages/swagger/src/decorators/api-exclude-controller.decorator.ts
+++ packages/swagger/src/decorators/api-exclude-controller.decorator.ts
@@ -1,7 +1,7 @@
 import { saveClassMetadata } from '@midwayjs/core';
 import { DECORATORS } from '../constants';
 
-export function ApiExcludeController(disable = true): any {
+export function ApiExcludeController(disable = true): ClassDecorator {
   return (target: any) => {
     saveClassMetadata(DECORATORS.API_EXCLUDE_CONTROLLER, { disable }, target);
   };
