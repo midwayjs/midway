66d02dd4ab1219677379f36bd12c10c5324567d7
Harry Chen <czy88840616@gmail.com>
2024-11-30 22:41:05 +0800

    test: add next test

--- packages/nextjs/test-utils.js
+++ packages/nextjs/test-utils.js
@@ -20,33 +20,39 @@ const rootPath = path.resolve('.').replace(/\\/g, '/');
 let pageExtensions = ['ts', 'js'];
 if (fs.existsSync(`${rootPath}/next.config.js`)) {
   try {
-    const { pageExtensions: configExtensions } = loadConfig(PHASE_TEST, rootPath);
+    const { pageExtensions: configExtensions } = loadConfig(
+      PHASE_TEST,
+      rootPath
+    );
     if (configExtensions) {
       pageExtensions = configExtensions;
     }
   } catch (error) {
-    console.warn('could not load pageExtensions config from next, using .ts and .js as default');
+    console.warn(
+      'could not load pageExtensions config from next, using .ts and .js as default'
+    );
   }
 }
 
-const nextPagesDirectory = fs.existsSync(`${rootPath}/pages`) ?
-  `${rootPath}/pages` :
-  `${rootPath}/src/pages`;
+const nextPagesDirectory = fs.existsSync(`${rootPath}/pages`)
+  ? `${rootPath}/pages`
+  : `${rootPath}/src/pages`;
 
 const handlers = glob
   .sync(`${nextPagesDirectory}/api/**/*.+(ts|js)`)
-  .filter((handler) => !handler.includes('.test') && !handler.includes('.spec'));
+  .filter(handler => !handler.includes('.test') && !handler.includes('.spec'));
 
 const mapping = {};
-handlers.forEach((handler) => { // still got the .js
+handlers.forEach(handler => {
+  // still got the .js
   const handlerName = handler.slice(nextPagesDirectory.length, -3);
-  const handlerFunction = require(`${nextPagesDirectory}/${handlerName}`)
+  const handlerFunction = require(`${nextPagesDirectory}/${handlerName}`);
 
   let key = handlerName;
   for (const ext of pageExtensions) {
     if (handler.endsWith(ext)) {
       key = handler.slice(nextPagesDirectory.length, -ext.length - 1);
-      key = key.endsWith("/index") ? key.slice(0, -6) : key;
+      key = key.endsWith('/index') ? key.slice(0, -6) : key;
       break;
     }
   }
@@ -68,7 +74,8 @@ const getDynamicRoute = (routes, url) => {
       const routePath = routeSplit[index];
       const urlPath = urlSplit[index];
 
-      if (routePath.startsWith('[') && routePath.endsWith(']')) { // if its a dynamic sub-path extend the route params
+      if (routePath.startsWith('[') && routePath.endsWith(']')) {
+        // if its a dynamic sub-path extend the route params
         routeParams[routePath.substring(1, routePath.length - 1)] = urlPath;
       } else if (routePath !== urlPath) {
         break; // if the path does not match, check a new route
@@ -77,14 +84,14 @@ const getDynamicRoute = (routes, url) => {
       if (index === routeSplit.length - 1) {
         return {
           route,
-          routeParams
+          routeParams,
         };
       }
     }
   }
-}
+};
 
-const getHandler = (url) => {
+const getHandler = url => {
   const handler = mapping[url];
 
   if (handler) {
@@ -92,8 +99,10 @@ const getHandler = (url) => {
   }
 
   const routes = Object.keys(mapping);
-  const dynamicRoutes = routes.filter(route => route.includes('[') && route.includes(']'));
-  dynamicRoutes.sort((a, b) => b.length - a.length) // Most specific routes take precedence
+  const dynamicRoutes = routes.filter(
+    route => route.includes('[') && route.includes(']')
+  );
+  dynamicRoutes.sort((a, b) => b.length - a.length); // Most specific routes take precedence
 
   const { route, routeParams } = getDynamicRoute(dynamicRoutes, url);
 
@@ -101,12 +110,9 @@ const getHandler = (url) => {
     handler: mapping[route],
     routeParams,
   };
-}
+};
 
-const requestHandler = (
-  request,
-  response,
-) => {
+const requestHandler = (request, response) => {
   try {
     const [url, queryParams] = request.url.split('?');
     const params = new URLSearchParams(queryParams);
@@ -119,11 +125,11 @@ const requestHandler = (
       { ...query, ...routeParams },
       handler,
       undefined,
-      true,
+      true
     );
   } catch (err) {
     const message = `Error handling request in nextjs-http-supertest: ${inspect(
-      err,
+      err
     )}`;
     console.error(message);
     response.writeHead(500, { 'content-type': 'application/json' });
