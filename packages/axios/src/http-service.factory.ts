50ca89bb22bbf91cd78f718bfa33ecefa8573739
142vip.cn <mmdapl@163.com>
2025-01-06 23:15:37 +0800

    refactor: axios component (#4231)
    * refactor: axios组件结构调整，代码优化
    
    * chore: update
    
    ---------
    
    Co-authored-by: 142vip <fairy@2925.com>

---
+++ packages/axios/src/http-service.factory.ts
@@ -0,0 +1,40 @@
+import {
+  Config,
+  Init,
+  Provide,
+  Scope,
+  ScopeEnum,
+  ServiceFactory,
+} from '@midwayjs/core';
+import axios, { AxiosInstance, CreateAxiosDefaults } from 'axios';
+
+@Provide()
+@Scope(ScopeEnum.Singleton)
+export class HttpServiceFactory extends ServiceFactory<AxiosInstance> {
+  @Config('axios')
+  axiosConfig: any;
+
+  @Init()
+  async init() {
+    let axiosConfig = this.axiosConfig;
+    if (!this.axiosConfig['clients']) {
+      axiosConfig = {
+        default: {},
+        clients: {
+          default: this.axiosConfig,
+        },
+      };
+    }
+    await this.initClients(axiosConfig);
+  }
+  public getName(): string {
+    return 'axios';
+  }
+
+  protected async createClient(
+    config: CreateAxiosDefaults,
+    clientName: string
+  ): Promise<AxiosInstance> {
+    return axios.create(config);
+  }
+}
