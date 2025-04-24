731a5ca12378535f733141d344197044613333bc
larry zhuo <larryzhuo@users.noreply.github.com>
2025-04-24 16:36:40 +0800

    feat(rabbitmq): add msg into rabbitmq ctx (#4326)
    * feat: add consume message into rabbitmq request ctx
    
    * fix: interface error
    
    ---------
    
    Co-authored-by: larry <larry@greaterheat.com>
    (cherry picked from commit b47e8afabe7e6875afe806ec678e09ec5b6cb5a9)

--- packages/rabbitmq/src/interface.ts
+++ packages/rabbitmq/src/interface.ts
@@ -11,20 +11,29 @@ import type { ConfirmChannel, Channel, Options as AmqpOptions } from 'amqplib';
 export interface IRabbitMQApplication {
   connect(...args): Promise<void>;
   createChannel(): Promise<Channel | ConfirmChannel>;
-  createConsumer(listenerOptions: RabbitMQListenerOptions, listenerCallback: (msg: ConsumeMessage | null, channel: Channel, channelWrapper) => Promise<void>): Promise<void>;
+  createConsumer(
+    listenerOptions: RabbitMQListenerOptions,
+    listenerCallback: (
+      msg: ConsumeMessage | null,
+      channel: Channel,
+      channelWrapper
+    ) => Promise<void>
+  ): Promise<void>;
   close(): Promise<void>;
 }
 
-export type IMidwayRabbitMQApplication = IMidwayApplication<IMidwayRabbitMQContext> & IRabbitMQApplication;
+export type IMidwayRabbitMQApplication =
+  IMidwayApplication<IMidwayRabbitMQContext> & IRabbitMQApplication;
 
 export interface IRabbitMQExchange {
-  name: string,
-  type: string,
-  options?: Options.AssertExchange
+  name: string;
+  type: string;
+  options?: Options.AssertExchange;
 }
 
-export interface IMidwayRabbitMQConfigurationOptions extends IConfigurationOptions {
-  url: string | Options.Connect,
+export interface IMidwayRabbitMQConfigurationOptions
+  extends IConfigurationOptions {
+  url: string | Options.Connect;
   socketOptions?: any;
   reconnectTime?: number;
   exchanges?: IRabbitMQExchange[];
@@ -32,6 +41,7 @@ export interface IMidwayRabbitMQConfigurationOptions extends IConfigurationOptio
 }
 
 export type IMidwayRabbitMQContext = IMidwayContext<{
+  data: ConsumeMessage;
   channel: Channel;
   queueName: string;
   ack: (data: any) => void;
