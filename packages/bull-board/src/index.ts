1f21e6fcc6befbdb46d06baa70867435a1df61d0
Harry Chen <czy88840616@gmail.com>
2025-06-18 00:17:14 +0800

    fix: bull-board missing export adapter

--- packages/bull-board/src/index.ts
+++ packages/bull-board/src/index.ts
@@ -3,3 +3,6 @@ export * from './board.middleware';
 export * from './board.manager';
 export * from './interface';
 export * as BullBoard from '@bull-board/api';
+export { MidwayAdapter } from './adapter';
+export { BullAdapter } from '@bull-board/api/bullAdapter';
+export { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
