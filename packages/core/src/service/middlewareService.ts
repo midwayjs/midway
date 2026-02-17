a5f8460fdd8b546bb4e520aaa4ac210c0c1f64ea
yuntian001 <479820787@qq.com>
2026-02-17 21:26:39 +0800

    fix: The problem of MaxListenersExceededWarning caused by repeated assignment of middleware body (#4501)
    * fix: 修复view组件render异步执行的时机问题
    
    原来 的写法会导致await render 并不能等待 render函数执行完毕
    
    * Fix render异步返回值还原为void
    
    * fix: busboy Type 'UploadFileInfo' is not generic.
    
    修复类型声明错误
    Type 'UploadFileInfo' is not generic. files?: UploadFileInfo<any>[];
    
    * fix: 修复中间件body重复赋值
    
    当koa的body赋值时会触发，set事件,如果body为同一个 stream
    会导致为stream重复设置finish,导致关闭时重复导演destroy，
    koa 对于stream的destroy 使用了 `destroy `包 多次调用会造成`MaxListenersExceededWarning: Possible EventEmitter memory leak detected. 21 open listeners added to [ReadStream]. MaxListeners is 20. Use emitter.setMaxListeners() to increase limit`警告

--- packages/core/src/service/middlewareService.ts
+++ packages/core/src/service/middlewareService.ts
@@ -120,7 +120,7 @@ export class MidwayMiddlewareService<T, R, N = unknown> {
                * 2、如果 result 有值（非 undefined），则不管什么情况，都会覆盖当前 body，注意，这里有可能赋值 null，导致 status 为 204，会在中间件处进行修正
                * 3、如果 result 没值，且 ctx.body 已经赋值，则向 result 赋值
                */
-              if (result !== undefined) {
+              if (result !== undefined && result !== context['body']) {
                 context['body'] = result;
               } else if (context['body'] !== undefined) {
                 result = context['body'];
