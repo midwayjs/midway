89362f47e8c021ecdbe168683b984c251dc93831
Harry Chen <czy88840616@gmail.com>
2026-02-20 17:15:54 +0800

    test: remove moduleNameMapper from react vue and web-bridge

--- packages/vue/jest.config.js
+++ packages/vue/jest.config.js
@@ -2,10 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  moduleNameMapper: {
-    '^@midwayjs/web-bridge$': '<rootDir>/../web-bridge/src/index.ts',
-    '^@midwayjs/vue$': '<rootDir>/src',
-  },
   coveragePathIgnorePatterns: ['<rootDir>/test/', '<rootDir>/dist/'],
   coverageProvider: 'v8',
 };
