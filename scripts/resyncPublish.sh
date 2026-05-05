861139bcaaf2b86f0ed1be05e73bde500a62f522
Harry Chen <czy88840616@gmail.com>
2026-05-05 22:56:55 +0800

    fix: sync scripts

--- scripts/resyncPublish.sh
+++ scripts/resyncPublish.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-pkgs=`find packages -maxdepth 1 -mindepth 1`
-cwd=`pwd`
-for pkg in $pkgs
-do
-    cd $cwd
-    if [ -e "${pkg}/package.json" ]
-    then
-      cd $pkg
-      echo ">>>>>> Publishing"
-      echo ">>>>>>" $pkg
-      npm publish
-    fi
-done
-cd $cwd
+set -e
+
+pnpm publish -r --filter "./packages/*" --no-git-checks
