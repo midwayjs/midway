1e65bfa567469874e9d8f6af5cb682e4812c1884
Harry Chen <czy88840616@gmail.com>
2026-08-24 23:33:44 +0800

    fix: make npm publish version selection non-interactive

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,14 +7,23 @@ pnpm -w run build:skill-midway
 git add .
 
 RELEASE_TYPE="$1"
+VERSION_BUMP="$2"
+
+version() {
+  if [ -n "$VERSION_BUMP" ]; then
+    lerna version "$VERSION_BUMP" --yes "$@"
+  else
+    lerna version --yes "$@"
+  fi
+}
 
 case "$RELEASE_TYPE" in
   "beta")
-    SKIP_VERSION_SCRIPTS=true lerna version --yes --force-publish=*
+    SKIP_VERSION_SCRIPTS=true version --force-publish=*
     lerna publish from-git --yes --dist-tag beta
     ;;
   "next")
-    SKIP_VERSION_SCRIPTS=true lerna version --yes
+    SKIP_VERSION_SCRIPTS=true version
     lerna publish from-git --yes --dist-tag next
     ;;
   "canary")
@@ -23,7 +32,7 @@ case "$RELEASE_TYPE" in
     ;;
   *)
     # release (default) - 只有这个会执行完整的 version 脚本
-    lerna version --yes
+    version
     lerna publish from-git --yes
     ;;
 esac
