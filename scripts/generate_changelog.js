41092905831aedadf097e076c88ba430f47b06d3
Harry Chen <czy88840616@gmail.com>
2026-08-25 21:29:03 +0800

    fix: create github release after publishing

--- scripts/generate_changelog.js
+++ scripts/generate_changelog.js
@@ -4,11 +4,6 @@ const { join } = require('path');
 
 const currentVersion = require('../lerna.json').version;
 
-if (!process.env.GITHUB_AUTH) {
-  console.error('Must provide GITHUB_AUTH');
-  process.exit(1);
-}
-
 try {
   const changelogContent = execSync(
     `npx lerna-changelog --nextVersion=v${currentVersion}`
@@ -23,19 +18,6 @@ try {
       '\n\n' + changelogContent + '\n\n'
     );
     writeFileSync(changelogFile, originContent);
-
-    console.log('start create release for version: ', currentVersion);
-
-    const url = `
-    curl \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${process.env.GITHUB_AUTH}" \
-    https://api.github.com/repos/midwayjs/midway/releases \
-    -d '{"tag_name":"v${currentVersion}","target_commitish":"main","name":"${currentVersion}","body":"${changelogContent}","draft":false,"prerelease":false,"generate_release_notes":false}'
-    `;
-
-    execSync(url).toString();
   } else {
     console.log('version not generate and skip changelog');
     process.exit(1);
